beff6fef0d873ecdae4b1d9717a9919994af6a7a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-01 12:50:28 -0400

    make sure to use layer when defining styles

--- packages/react/src/text/Text.css.ts
+++ packages/react/src/text/Text.css.ts
@@ -1,5 +1,11 @@
 import { style } from "@vanilla-extract/css";
 
+import { layers } from "../styles";
+
 export const base = style({
-  fontFamily: "InterVariable, system-ui, sans-serif",
+  "@layer": {
+    [layers.axiom]: {
+      fontFamily: "InterVariable, system-ui, sans-serif",
+    },
+  },
 });
