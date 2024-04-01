672ea5f3f234a11ac2f2f7689fc6d8ef71c188f3
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-01 12:22:37 -0400

    fix and simplify css layers
    outer layer takes precendence over nester layers - so reset should be nested
    inside the top level axiom layer

--- packages/react/src/styles/layers.css.ts
+++ packages/react/src/styles/layers.css.ts
@@ -1,5 +1,4 @@
 import { globalLayer, layer } from "@vanilla-extract/css";
 
-const axiom = globalLayer("optimizely-axiom");
+export const axiom = globalLayer("optimizely-axiom");
 export const reset = layer({ parent: axiom }, "reset");
-export const app = layer({ parent: axiom }, "app");
