b8e55e0284c337913490296bc8c3b59e6af8f426
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-09 15:04:23 -0400

    replace danger with success and remove fg color

--- apps/docs/demos/colors-usage/App.tsx
+++ apps/docs/demos/colors-usage/App.tsx
@@ -2,7 +2,7 @@ import { Box } from "@optiaxiom/react";
 
 export function App() {
   return (
-    <Box background="bg.danger" color="fg.danger" padding="md">
+    <Box background="bg.success" padding="md">
       Setting background and text colors
     </Box>
   );
