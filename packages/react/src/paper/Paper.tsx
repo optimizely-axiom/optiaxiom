8d57f2107ed7ffb14d0e36cc337f446b4355c93a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-17 16:14:05 -0400

    add slate colors and use in paper
    along with transparent and currentColor

--- packages/react/src/paper/Paper.tsx
+++ packages/react/src/paper/Paper.tsx
@@ -19,7 +19,7 @@ export const Paper = forwardRef<HTMLParagraphElement, PaperProps>(
   ({ className, shadow = "sm", ...props }, ref) => {
     return (
       <Box
-        background={{ base: "white", dark: "dark.500" }}
+        background={{ base: "white", dark: "slate.800" }}
         borderRadius="sm"
         className={clsx(className, styles.sprinkles({ boxShadow: shadow }))}
         ref={ref}
