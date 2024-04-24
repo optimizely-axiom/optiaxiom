ca65956933b18e051f8b67237923464271fcf68e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-24 13:14:07 -0400

    fix eslint monorepo config for IDE
    remove top level dummy tsconfig.json and run oas-lint for workspace in addition
    to individual packages
    
    and in oas-lint check for eslint.config.js and tsconfig.json to conditionally
    run tasks
    
    prettier and eslint will only run in workspace once for all code and tsc will
    run for individual projects

--- packages/shared/bin/lint.sh
+++ packages/shared/bin/lint.sh
@@ -1,4 +1,8 @@
 e_flag="--fix" p_flag="-w"
 [ "$1" = "--no-fix" ] && e_flag= p_flag="-c"
-prettier --cache --log-level silent $p_flag .
-concurrently --raw "tsc --noEmit" "eslint --cache --quiet $e_flag ."
+if [ -f eslint.config.js ]; then
+  prettier --cache --log-level silent $p_flag .
+  eslint --cache --quiet $e_flag .
+else
+  tsc --noEmit
+fi
