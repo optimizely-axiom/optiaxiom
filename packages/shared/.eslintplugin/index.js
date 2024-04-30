5714599920d3f3b9c17c338043af35840a7be44f
Rafi Adnan <rafi.adnan@optimizely.com>
2024-04-30 15:51:32 -0400

    rename sprinkles conventions eslint rule to be more generic
    so we can collect more rules inside

--- packages/shared/.eslintplugin/index.js
+++ packages/shared/.eslintplugin/index.js
@@ -1,11 +1,11 @@
 import noGlobalStyles from "./no-global-styles.js";
 import preferTestingLibrary from "./prefer-testing-library.js";
-import sprinklesConstArray from "./sprinkles-const-array.js";
+import sprinklesConventions from "./sprinkles-conventions.js";
 
 export default {
   rules: {
     "no-global-styles": noGlobalStyles,
     "prefer-testing-library": preferTestingLibrary,
-    "sprinkles-const-array": sprinklesConstArray,
+    "sprinkles-conventions": sprinklesConventions,
   },
 };
