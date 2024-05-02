2081c08dcf8e01ea3d5ea9f309996a3c67a284a7
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-02 17:13:17 -0400

    re-use styled system for docs hero page

--- apps/docs/pages/HeroPage.tsx
+++ apps/docs/pages/HeroPage.tsx
@@ -1,31 +1,39 @@
-import { Heading, Stack, Text } from "@optiaxiom/react";
+import { Box, Heading, Stack, Text } from "@optiaxiom/react";
 import Link from "next/link";
 
 import styles from "./HeroPage.module.css";
 
 export function HeroPage() {
   return (
-    <Stack
-      alignItems="center"
-      className={styles.wrapper}
-      gap="xl"
-      justifyContent="center"
-    >
+    <Stack alignItems="center" h="full" justifyContent="center">
       <Heading
         fontSize={["4xl", "5xl", "6xl"]}
         fontWeight="800"
         level="1"
         tracking="tight"
       >
-        <span className={styles.highlight}>Axiom</span> Design System
+        <Box asChild color="aqua.500">
+          <span>Axiom</span>
+        </Box>{" "}
+        Design System
       </Heading>
 
-      <Text className={styles.body}>
+      <Text fontSize="2xl" mb="24">
         Axiom is the technical implementation of the Optimizely Design System.
       </Text>
 
       <Link href="/getting-started">
-        <button className={styles.button}>Get started</button>
+        <Box
+          asChild
+          bg="aqua.200"
+          fontSize="2xl"
+          fontWeight="600"
+          px="32"
+          py="16"
+          rounded="md"
+        >
+          <button className={styles.button}>Get started</button>
+        </Box>
       </Link>
     </Stack>
   );
