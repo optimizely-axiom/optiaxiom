645797eb0d0c3aa5d4a8fb9b4eed8cca515b0158
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-06 11:58:40 -0400

    map cols sprinkle prop to grid-template-columns css property
    without using intermediate css var to simplify usage

--- packages/react/src/grid/Grid.tsx
+++ packages/react/src/grid/Grid.tsx
@@ -6,16 +6,7 @@ type GridProps = ComponentPropsWithRef<typeof Box>;
 
 export const Grid = forwardRef<HTMLDivElement, GridProps>(
   ({ cols = "1", gap = "md", ...props }, ref) => {
-    return (
-      <Box
-        cols={cols}
-        display="grid"
-        gap={gap}
-        gridTemplateColumns="cols"
-        ref={ref}
-        {...props}
-      />
-    );
+    return <Box cols={cols} display="grid" gap={gap} ref={ref} {...props} />;
   },
 );
 
