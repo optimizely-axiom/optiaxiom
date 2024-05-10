edebf67d00b0abeddf2f3d63172b7b943a87f751
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-10 16:36:57 -0400

    use stripes to explicitly denote margin and padding

--- apps/docs/demos/sizing/size/reset-usage/App.tsx
+++ apps/docs/demos/sizing/size/reset-usage/App.tsx
@@ -1,13 +1,11 @@
-import { Box, Stack } from "@optiaxiom/react";
+import { Box } from "@optiaxiom/react";
 
 import { Container } from "../Container";
 
 export function App() {
   return (
-    <Stack>
-      <Container>
-        <Box size="auto">size=auto</Box>
-      </Container>
-    </Stack>
+    <Container>
+      <Box size="auto">size=auto</Box>
+    </Container>
   );
 }
