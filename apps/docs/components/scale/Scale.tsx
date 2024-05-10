38903ab2b32bac4a1881db5d3b7dc24f7dfead4e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-10 16:29:32 -0400

    limit scale table height via maxH

--- apps/docs/components/scale/Scale.tsx
+++ apps/docs/components/scale/Scale.tsx
@@ -20,45 +20,47 @@ export const Scale = ({
   valueLabel?: string;
   values: Record<string, string>;
 }) => (
-  <Table>
-    <thead>
-      <tr>
-        <Th w="80">{keyLabel}</Th>
-        <Th w="128">{valueLabel}</Th>
-        {!hidePixels && <Th w="128">Pixels</Th>}
-        {!hidePreview && (
-          <Box asChild display={["none", "table-cell"]}>
-            <Th />
-          </Box>
-        )}
-      </tr>
-    </thead>
-    <tbody>
-      {Object.entries(values)
-        .sort(([a], [b]) => {
-          const aMatch = a.match(/^([0-9.]+)$/);
-          const aNum = aMatch === null ? NaN : parseFloat(aMatch[1]);
-          const bMatch = b.match(/^([0-9.]+)$/);
-          const bNum = bMatch === null ? NaN : parseFloat(bMatch[1]);
-          if (isNaN(aNum) && isNaN(bNum)) return 0;
-          if (isNaN(aNum)) return 1;
-          if (isNaN(bNum)) return -1;
-          return aNum - bNum;
-        })
-        .map(([name, size]) => (
-          <Tr key={name}>
-            <Td>{name}</Td>
-            <Td>{size}</Td>
-            {!hidePixels && <Td>{px(size)}</Td>}
-            {!hidePreview && (
-              <Box asChild display={["none", "table-cell"]}>
-                <Td>
-                  <Box bg="purple.500" h="16" style={{ width: size }} />
-                </Td>
-              </Box>
-            )}
-          </Tr>
-        ))}
-    </tbody>
-  </Table>
+  <Box maxH="sm" overflow="auto">
+    <Table>
+      <thead>
+        <tr>
+          <Th w="80">{keyLabel}</Th>
+          <Th w="128">{valueLabel}</Th>
+          {!hidePixels && <Th w="128">Pixels</Th>}
+          {!hidePreview && (
+            <Box asChild display={["none", "table-cell"]}>
+              <Th />
+            </Box>
+          )}
+        </tr>
+      </thead>
+      <tbody>
+        {Object.entries(values)
+          .sort(([a], [b]) => {
+            const aMatch = a.match(/^([0-9.]+)$/);
+            const aNum = aMatch === null ? NaN : parseFloat(aMatch[1]);
+            const bMatch = b.match(/^([0-9.]+)$/);
+            const bNum = bMatch === null ? NaN : parseFloat(bMatch[1]);
+            if (isNaN(aNum) && isNaN(bNum)) return 0;
+            if (isNaN(aNum)) return 1;
+            if (isNaN(bNum)) return -1;
+            return aNum - bNum;
+          })
+          .map(([name, size]) => (
+            <Tr key={name}>
+              <Td>{name}</Td>
+              <Td>{size}</Td>
+              {!hidePixels && <Td>{px(size)}</Td>}
+              {!hidePreview && (
+                <Box asChild display={["none", "table-cell"]}>
+                  <Td>
+                    <Box bg="purple.500" h="16" style={{ width: size }} />
+                  </Td>
+                </Box>
+              )}
+            </Tr>
+          ))}
+      </tbody>
+    </Table>
+  </Box>
 );
