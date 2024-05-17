f313d87567ee1b494146de14921fc0deeaaa2528
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-16 23:13:42 -0400

    only omit values that actually exist
    to prevent inlining all possible keys

--- packages/react/src/utils/ExtendProps.ts
+++ packages/react/src/utils/ExtendProps.ts
@@ -1,3 +1,6 @@
-export type ExtendProps<T1, T2, T3 = unknown> = Omit<T1, keyof T2 | keyof T3> &
-  Omit<T2, keyof T3> &
+export type ExtendProps<T1, T2, T3 = unknown> = Omit<
+  T1,
+  keyof T1 & (keyof T2 | keyof T3)
+> &
+  Omit<T2, keyof T2 & keyof T3> &
   T3;
