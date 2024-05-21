66726e76c690acb9cbcb05fa316c4c55098d7716
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-21 14:12:25 -0400

    add more docs for basic styled props usage

---
+++ apps/docs/demos/styles-sx/usage/App.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <Box sx={{ bg: "bg.success", color: "text.success", p: "md" }}>
+      Using sx prop
+    </Box>
+  );
+}
