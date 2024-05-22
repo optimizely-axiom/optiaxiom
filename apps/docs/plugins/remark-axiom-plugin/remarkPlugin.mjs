c0561950f1e7cf3e723be2d0226d2bda049aec31
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-22 17:39:22 -0400

    document color tokens for light/dark mode

--- apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
+++ apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
@@ -1,4 +1,5 @@
 import { transformCode } from "./transformCode.mjs";
+import { transformColorTokens } from "./transformColorTokens.mjs";
 import { transformDemos } from "./transformDemos.mjs";
 import { transformLayout } from "./transformLayout.mjs";
 import { transformPropsTable } from "./transformPropsTable.mjs";
@@ -6,6 +7,7 @@ import { transformPropsTable } from "./transformPropsTable.mjs";
 export function remarkPlugin() {
   return (tree, file) => {
     transformCode(tree, file);
+    transformColorTokens(tree, file);
     transformDemos(tree, file);
     transformLayout(tree, file);
     transformPropsTable(tree, file);
---
+++ packages/react/src/tokens/Colors.doc.ts
@@ -0,0 +1,9 @@
+import { Colors, ColorsDark } from "./Colors";
+
+export function ColorsDoc(_props: Colors) {
+  return null;
+}
+
+export function ColorsDarkDoc(_props: ColorsDark) {
+  return null;
+}
