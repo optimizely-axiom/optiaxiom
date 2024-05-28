8f84b851da74c48e51c0c5d545043cb57ec8a9e8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-28 18:55:26 -0400

    add docs for color style prop

---
+++ apps/docs/demos/color/Item.tsx
@@ -0,0 +1,21 @@
+import { Box, Flex } from "@optiaxiom/react";
+import { type ComponentPropsWithRef, type ReactElement } from "react";
+
+export const Item = ({
+  children,
+}: {
+  children: ReactElement<ComponentPropsWithRef<typeof Box>>;
+}) => (
+  <Flex alignItems="center" gap="8">
+    <Box
+      asChild
+      fontFamily="sans"
+      fontSize="lg"
+      fontWeight="500"
+      textAlign="center"
+      {...children.props}
+    >
+      <button>{children.props.children}</button>
+    </Box>
+  </Flex>
+);
---
+++ apps/docs/demos/color/usage/App.tsx
@@ -0,0 +1,11 @@
+import { Box } from "@optiaxiom/react";
+
+import { Canvas } from "../Canvas";
+
+export function App() {
+  return (
+    <Canvas>
+      <Box color="fg.brand">The quick brown fox jumps over the lazy dog.</Box>
+    </Canvas>
+  );
+}
