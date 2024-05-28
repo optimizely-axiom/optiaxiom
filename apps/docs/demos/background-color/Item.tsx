3531ea424ae128e5e6198574696ec13418bf5677
Rafi Adnan <rafi.adnan@optimizely.com>
2024-05-28 18:49:18 -0400

    add docs for background-color style prop

---
+++ apps/docs/demos/background-color/Item.tsx
@@ -0,0 +1,25 @@
+import { Box, Flex } from "@optiaxiom/react";
+import { type ComponentPropsWithRef, type ReactElement } from "react";
+
+export const Item = ({
+  children,
+}: {
+  children: ReactElement<ComponentPropsWithRef<typeof Box>>;
+}) => (
+  <Flex alignItems="center" gap="8">
+    <Box
+      asChild
+      color="fg.default.inverse"
+      fontFamily="sans"
+      fontSize="md"
+      fontWeight="500"
+      px="16"
+      py="8"
+      rounded="md"
+      textAlign="center"
+      {...children.props}
+    >
+      <button>{children.props.children}</button>
+    </Box>
+  </Flex>
+);
