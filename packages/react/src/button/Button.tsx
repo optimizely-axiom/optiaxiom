427d07ef44467b1811d12e9a6465bfe9a16bbe51
Jafar Saifullah <abujafar.saifullah@optimizely.com>
2024-05-30 15:25:24 +0600

    update Button Component

--- packages/react/src/button/Button.tsx
+++ packages/react/src/button/Button.tsx
@@ -15,55 +15,89 @@ import { Flex } from "../flex";
 import { Text } from "../text";
 import { type Recipe, recipe } from "./Button.recipe";
 
+const presets = {
+  basic: { colorScheme: "secondary", variant: "solid" },
+  "basic-link": { colorScheme: "secondary", variant: "link" },
+  danger: { colorScheme: "danger", variant: "solid" },
+  "danger-outline": { colorScheme: "danger", variant: "outline" },
+  plain: { colorScheme: "secondary", variant: "ghost" },
+  primary: { colorScheme: "primary", variant: "solid" },
+} satisfies Record<string, Recipe>;
+
 type ButtonProps = ExtendProps<
   ComponentPropsWithRef<"button">,
   ComponentPropsWithRef<typeof Flex>,
   {
     asChild?: boolean;
     children?: ReactNode;
+    isLoading?: boolean;
     leftSection?: ReactNode;
+    preset?: keyof typeof presets;
     rightSection?: ReactNode;
   } & Recipe
 >;
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ asChild, children, leftSection, rightSection, ...props }, ref) => {
+  (
+    {
+      asChild,
+      children,
+      colorScheme,
+      disabled,
+      isLoading,
+      leftSection,
+      onClick,
+      preset = "basic",
+      rightSection,
+      variant,
+      ...props
+    },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
     children =
       asChild && isValidElement(children) ? (
         cloneElement(
           children,
           undefined,
-          <Text fontSize="inherit" fontWeight="600">
-            {children.props.children}
-          </Text>,
+          <Text fontSize="inherit">{children.props.children}</Text>,
         )
       ) : (
-        <Text fontSize="inherit" fontWeight="600">
-          {children}
-        </Text>
+        <Text fontSize="inherit">{children}</Text>
       );
 
     const innerRef = useRef<HTMLButtonElement>(null);
     const composedRef = useComposedRefs(ref, innerRef);
 
+    const presetProps = presets[preset];
+    const finalColorScheme = colorScheme ?? presetProps.colorScheme;
+    const finalVariant = variant ?? presetProps.variant;
+
     return (
       <Flex
         asChild
         cursor="pointer"
+        data-disabled={disabled || isLoading}
         display="inline-flex"
         flexDirection="row"
         gap="xs"
+        onClick={disabled || isLoading ? undefined : onClick}
         overflow="hidden"
         position="relative"
         rounded="sm"
         transition="colors"
-        {...recipe(props)}
+        {...recipe({
+          ...props,
+          colorScheme: finalColorScheme,
+          variant: finalVariant,
+        })}
       >
         <Comp ref={composedRef}>
-          {leftSection}
-          <Slottable>{children}</Slottable>
-          {rightSection}
+          <>
+            {leftSection}
+            <Slottable>{children}</Slottable>
+            {rightSection}
+          </>
         </Comp>
       </Flex>
     );
