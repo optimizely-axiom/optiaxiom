67d0cb5ce55876db89c4f802a096c298766eccbc
toha993 <mutasim.toha@optimizely.com>
2024-05-30 18:16:22 +0600

    Fix props for story

--- apps/storybook/src/forms/FormGroup.stories.tsx
+++ apps/storybook/src/forms/FormGroup.stories.tsx
@@ -15,32 +15,30 @@ type Story = StoryObj<typeof FormGroup>;
 export const InputStyle: Story = {
   render: () => (
     <Flex flexDirection="column" gap="xl">
-      <FormGroup label="Form label" note="You have to submit the files">
+      <FormGroup label="Form label">
         <Input
           leftSection={<IconCalendar display="block" height="16" width="20" />}
           placeholder="With Icon"
         />
       </FormGroup>
 
-      <FormGroup label="Form label" note="You have to submit the files">
+      <FormGroup label="Form label">
         <Input placeholder="Without Icon" />
       </FormGroup>
 
-      <FormGroup label="Form label" note="You have to submit the files">
-        <Input data-disabled placeholder="Disabled" />
+      <FormGroup isDisabled={true} label="Form label">
+        <Input placeholder="Disabled" />
       </FormGroup>
 
-      <FormGroup label="Form label" note="You have to submit the files">
-        <Input data-error placeholder="With error" />
+      <FormGroup
+        isInvalid={true}
+        label="Form label"
+        note="You have to submit the files"
+      >
+        <Input placeholder="With error" />
       </FormGroup>
 
       <FormGroup label="Form botton">
-        <Button justifyContent="center" mb="2">
-          Click me
-        </Button>
-        <Button justifyContent="center" mb="2">
-          Click me
-        </Button>
         <Button justifyContent="center" mb="2">
           Click me
         </Button>
