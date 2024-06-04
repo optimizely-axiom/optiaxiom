e5a69740a5f6cfa941347d314b52c62494888eec
Bishwajit <bishwajit0020@gmail.com>
2024-06-04 14:47:39 +0600

    (ProgressBar) Use separate recipes for ProgressBar Root and Indicator

--- packages/react/src/progress-bar/ProgressBar.recipe.ts
+++ packages/react/src/progress-bar/ProgressBar.recipe.ts
@@ -1,22 +1,33 @@
 import { createRecipe } from "../recipes";
 
-export const recipe = createRecipe({
+export const progressRootRecipe = createRecipe({
   base: {
-    bg: "white",
     border: "1",
     overflow: "hidden",
-    position: "relative",
   },
-
-  defaultVariants: {
-    size: "md",
-  },
-
   variants: {
     size: {
-      md: { h: "6", w: "1/2" },
+      default: { h: "6", w: "1/2" },
+    },
+    variant: {
+      default: {
+        bg: "white",
+      },
     },
   },
 });
 
-export type Recipe = Parameters<typeof recipe>[0];
+export const progressIndicatorRecipe = createRecipe({
+  variants: {
+    variant: {
+      default: {
+        bg: "bg.brand.solid",
+        h: "full",
+        rounded: "md",
+        transition: "all",
+      },
+    },
+  },
+});
+
+export type Recipe = Parameters<typeof progressRootRecipe>[0];
