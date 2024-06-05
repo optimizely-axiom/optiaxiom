11fd8de229cf2ac7868313b3d22fc8ef7817c309
toha993 <mutasim.toha@optimizely.com>
2024-06-05 15:28:57 +0600

    Add useId utils

--- packages/react/src/form-field/FormField.tsx
+++ packages/react/src/form-field/FormField.tsx
@@ -9,34 +9,58 @@ import type { ExtendProps } from "../utils/ExtendProps";
 
 import { Box } from "../box";
 import { Text } from "../text";
+import { useId } from "../utils/useId";
 type FormGroupProps = ExtendProps<
   ComponentPropsWithRef<typeof Box>,
   {
     children: ReactElement;
+    description?: string;
+    error?: string;
+    id?: string;
     isDisabled?: boolean;
     isInvalid?: boolean;
     label?: string;
-    note?: string;
     required?: boolean;
   }
 >;
 
 export const FormField = forwardRef<HTMLDivElement, FormGroupProps>(
-  ({ children, isDisabled, isInvalid, label, note, ...props }, ref) => {
+  (
+    {
+      children,
+      description,
+      error,
+      id: _id,
+      isDisabled,
+      isInvalid,
+      label,
+      required,
+      ...props
+    },
+    ref,
+  ) => {
+    const id = useId(_id);
     return (
       <Box display="flex" flexDirection="column" maxW="sm" ref={ref} {...props}>
         {label && (
-          <Text as="label" color={isInvalid ? "border.error" : "fg.secondary"}>
-            {label}
+          <Text asChild color={isInvalid ? "border.error" : "fg.secondary"}>
+            <label htmlFor={id}>{label}</label>
           </Text>
         )}
         {cloneElement(children, {
+          id,
           ["isDisabled"]: isDisabled,
           ["isInvalid"]: isInvalid,
+          required,
         })}
-        {note && (
-          <Text as="label" color={isInvalid ? "border.error" : "fg.secondary"}>
-            {note}
+        {description && (
+          <Text as="p" color={"fg.secondary"}>
+            {description}
+          </Text>
+        )}
+        {error && isInvalid && (
+          <Text as="p" color={"border.error"}>
+            {error}
           </Text>
         )}
       </Box>
