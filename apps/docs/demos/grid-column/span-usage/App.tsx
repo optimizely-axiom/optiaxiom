759249a787d887c3447b5a6fd0eb37f70927e4a8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-05 10:57:12 -0400

    make use of grids explicit in the demo

--- apps/docs/demos/grid-column/span-usage/App.tsx
+++ apps/docs/demos/grid-column/span-usage/App.tsx
@@ -1,17 +1,19 @@
-import { Box } from "@optiaxiom/react";
+import { Box, Grid } from "@optiaxiom/react";
 
 import { Canvas } from "../Canvas";
 
 export function App() {
   return (
     <Canvas>
-      <Box>01</Box>
-      <Box>02</Box>
-      <Box>03</Box>
-      <Box colSpan="2">04</Box>
-      <Box>05</Box>
-      <Box>06</Box>
-      <Box colSpan="2">07</Box>
+      <Grid cols="3">
+        <Box>01</Box>
+        <Box>02</Box>
+        <Box>03</Box>
+        <Box colSpan="2">04</Box>
+        <Box>05</Box>
+        <Box>06</Box>
+        <Box colSpan="2">07</Box>
+      </Grid>
     </Canvas>
   );
 }
