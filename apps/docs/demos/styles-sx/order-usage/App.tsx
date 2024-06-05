a5ce0aa81979b5e2faf1dd87f1a014a60d8a5037
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-05 10:57:27 -0400

    remove docs for sprinkles that have been removed

--- apps/docs/demos/styles-sx/order-usage/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@optiaxiom/react";
-
-export function App() {
-  return (
-    <Text fontWeight="700" sx={{ fontWeight: "400" }}>
-      This text will NOT be bold.
-    </Text>
-  );
-}
--- apps/docs/demos/styles-sx/usage/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@optiaxiom/react";
-
-export function App() {
-  return (
-    <Box sx={{ bg: "bg.success", color: "fg.success", p: "md" }}>
-      Using sx prop
-    </Box>
-  );
-}
