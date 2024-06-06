012cfb75bf093c8b4b1f650a93d3ae9fe23a1654
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-06 14:51:14 -0400

    add lineClamp support to Text component

---
+++ apps/docs/demos/text/line-clamp-usage/App.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <Text lineClamp="2">
+      Lorem Ipsum is a placeholder text commonly used in the design and printing
+      industries. Despite its widespread use, the origins of Lorem Ipsum are
+      somewhat mysterious. Several theories exist about who may have invented
+      the text, but no one knows.
+    </Text>
+  );
+}
