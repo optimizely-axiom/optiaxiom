893cdf4a6198d1546c7b41c8215ed76e614da13e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-06 13:22:56 -0400

    add truncate ability to text component
    and extend Heading from Text to allow inheriting all its abilities

---
+++ apps/docs/demos/text/truncate-usage/App.tsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <Box w="192">
+      <Text truncate>The quick brown fox jumps over the lazy dog.</Text>
+    </Box>
+  );
+}
