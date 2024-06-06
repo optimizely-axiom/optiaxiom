2af1f02b36d228d1ecbe09fb3484f41b588b04b9
Bishwajit <bishwajit0020@gmail.com>
2024-06-06 19:13:53 +0600

    (ProgressBar) Allow passing width to progress bar

--- packages/react/src/progress-bar/ProgressBar.tsx
+++ packages/react/src/progress-bar/ProgressBar.tsx
@@ -2,23 +2,27 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 import { type ComponentPropsWithRef, type ElementRef, forwardRef } from "react";
 
 import { Box } from "../box";
+import { type ExtendProps } from "../utils";
 
-type ProgressBarProps = ComponentPropsWithRef<typeof ProgressPrimitive.Root>;
+type ProgressBarProps = ExtendProps<
+  ComponentPropsWithRef<typeof ProgressPrimitive.Root>,
+  Pick<ComponentPropsWithRef<typeof Box>, "w">
+>;
 
 export const ProgressBar = forwardRef<
   ElementRef<typeof ProgressPrimitive.Root>,
   ProgressBarProps
->((props, ref) => {
+>(({ w = "2/3", ...props }, ref) => {
   const widthPercentage = ((props.value ?? 0) / (props.max ?? 100)) * 100;
 
   return (
-    <Box asChild border="1" h="6" overflow="hidden" w="1/2">
+    <Box asChild border="1" h="6" overflow="hidden" w={w}>
       <ProgressPrimitive.Root ref={ref} {...props}>
         <Box
           asChild
           bg="bg.brand.solid"
           h="full"
-          rounded="md"
+          rounded="lg"
           style={{ width: `${widthPercentage}%` }}
           transition="all"
         >
