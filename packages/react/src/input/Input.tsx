5d2d3d883a5ba63f7a9d4591735503c3604b8a5e
toha993 <mutasim.toha@optimizely.com>
2024-06-06 23:04:55 +0600

    Remove sprinkle based recipes and update style

--- packages/react/src/input/Input.tsx
+++ packages/react/src/input/Input.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { type ComponentPropsWithRef, forwardRef } from "react";
+import { type ComponentPropsWithRef, type ReactNode, forwardRef } from "react";
 
 import { Box } from "../box";
 import { extractSprinkles } from "../sprinkles";
@@ -9,16 +9,25 @@ import * as styles from "./Input.css";
 type InputProps = ExtendProps<
   ComponentPropsWithRef<"input">,
   ComponentPropsWithRef<typeof Box>,
-  { isDisabled?: boolean; isInvalid?: boolean } & styles.InputVariants
+  {
+    isDisabled?: boolean;
+    isInvalid?: boolean;
+    leftSection?: ReactNode;
+    rightSection?: ReactNode;
+  } & styles.WrapperVariants
 >;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   (
     {
       className,
+      defaultValue,
+      id,
       isDisabled,
       isInvalid,
+      leftSection,
       placeholder,
+      rightSection,
       size = "md",
       type,
       variant = "default",
@@ -31,19 +40,43 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       <Box
         aria-disabled={isDisabled}
         aria-invalid={isInvalid}
-        asChild
         className={clsx(
-          styles.input({
+          styles.wrapper({
             size,
             variant,
           }),
-          className,
         )}
         data-disabled={isDisabled}
         data-invalid={isInvalid}
         {...sprinkleProps}
       >
-        <input placeholder={placeholder} ref={ref} type={type} {...restProps} />
+        {leftSection && (
+          <Box flex="none" mr="8">
+            {leftSection}
+          </Box>
+        )}
+        <Box
+          asChild
+          className={clsx(
+            styles.input({
+              variant,
+            }),
+          )}
+        >
+          <input
+            defaultValue={defaultValue}
+            id={id}
+            placeholder={placeholder}
+            ref={ref}
+            type={type}
+            {...restProps}
+          />
+        </Box>
+        {rightSection && (
+          <Box flex="none" ml="8">
+            {rightSection}
+          </Box>
+        )}
       </Box>
     );
   },
