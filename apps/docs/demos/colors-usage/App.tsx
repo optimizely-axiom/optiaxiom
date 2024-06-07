c86acb62c3d535c20f30c90029bc003be8dca1e2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-07 12:38:37 -0400

    move component specific demos to own folders

--- apps/docs/demos/colors-usage/App.tsx
+++
@@ -1,9 +0,0 @@
-import { Box } from "@optiaxiom/react";
-
-export function App() {
-  return (
-    <Box bg="bg.success" color="fg.success" p="md">
-      Setting background and text colors
-    </Box>
-  );
-}
