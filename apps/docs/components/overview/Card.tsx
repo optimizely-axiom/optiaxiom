1c13950b1fb9296c169f69842d4b94f1ddf8cc73
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-11 16:52:52 -0400

    properly center demos inside overview cards
    need to override for items such as progress that have no fixed size

--- apps/docs/components/overview/Card.tsx
+++ apps/docs/components/overview/Card.tsx
@@ -1,28 +1,30 @@
-import type { ReactNode } from "react";
+import type { ComponentPropsWithRef, ReactNode } from "react";
 
-import { Grid } from "@optiaxiom/react";
+import { Box, Flex } from "@optiaxiom/react";
 import { Card as NextraCard } from "nextra-theme-docs";
 
 export function Card({
   children,
   href,
   title,
+  ...props
 }: {
   children: ReactNode;
   href: string;
   title: string;
-}) {
+} & ComponentPropsWithRef<typeof Flex>) {
   return (
     <NextraCard href={href} icon={null} image title={title}>
-      <Grid
+      <Flex
         bg="surface"
         className="nx-pointer-events-none"
+        flexDirection="row"
         h="128"
-        placeItems="center"
+        justifyContent="center"
         w="full"
       >
-        {children}
-      </Grid>
+        <Box {...props}>{children}</Box>
+      </Flex>
     </NextraCard>
   );
 }
