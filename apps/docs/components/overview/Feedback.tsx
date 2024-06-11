5b24ce6bc6a99ca616ac21120fb759f0bfe17597
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-11 16:53:33 -0400

    add progress docs

--- apps/docs/components/overview/Feedback.tsx
+++ apps/docs/components/overview/Feedback.tsx
@@ -1,4 +1,4 @@
-import { Skeleton } from "@optiaxiom/react";
+import { Progress, Skeleton } from "@optiaxiom/react";
 import { Cards } from "nextra-theme-docs";
 
 import { Card } from "./Card";
@@ -6,6 +6,9 @@ import { Card } from "./Card";
 export function Feedback() {
   return (
     <Cards>
+      <Card flex="1" href="/components/progress" mx="32" title="Progress">
+        <Progress value={50} />
+      </Card>
       <Card href="/components/skeleton" title="Skeleton">
         <Skeleton h="24" w="80" />
       </Card>
