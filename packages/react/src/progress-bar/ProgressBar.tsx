16cdd0e18e72686b8f1c56aadb6eaa0804bb5116
Bishwajit <bishwajit0020@gmail.com>
2024-06-11 23:13:23 +0600

    (ProgressBar) Don't show progress indicator when data-state is indeterminate

--- packages/react/src/progress-bar/ProgressBar.tsx
+++ packages/react/src/progress-bar/ProgressBar.tsx
@@ -13,24 +13,32 @@ export const ProgressBar = forwardRef<
   ElementRef<typeof ProgressPrimitive.Root>,
   ProgressBarProps
 >((props, ref) => {
-  const widthPercentage = ((props.value ?? 0) / (props.max ?? 100)) * 100;
+  const widthPercentage =
+    ((props.value ?? 0) / (props.max ?? DEFAULT_MAX)) * 100;
+  const isValidValue =
+    props.value &&
+    props.value >= 0 &&
+    props.value <= (props.max ?? DEFAULT_MAX);
 
   return (
     <Box asChild h="6" rounded="lg" {...props}>
       <ProgressPrimitive.Root ref={ref}>
-        <Box
-          asChild
-          bg="bg.brand.solid"
-          h="full"
-          rounded="lg"
-          style={{ width: `${widthPercentage}%` }}
-          transition="all"
-        >
-          <ProgressPrimitive.Indicator />
-        </Box>
+        {isValidValue && (
+          <Box
+            asChild
+            bg="bg.brand.solid"
+            h="full"
+            rounded="lg"
+            style={{ width: `${widthPercentage}%` }}
+            transition="all"
+          >
+            <ProgressPrimitive.Indicator />
+          </Box>
+        )}
       </ProgressPrimitive.Root>
     </Box>
   );
 });
 
+const DEFAULT_MAX = 100;
 ProgressBar.displayName = "@optiaxiom/react/ProgressBar";
