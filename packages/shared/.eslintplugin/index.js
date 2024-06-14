94bdf3a8f86b59890d141f8373e5d88cc5b6103c
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-14 13:21:03 -0400

    prevent useless clsx calls

--- packages/shared/.eslintplugin/index.js
+++ packages/shared/.eslintplugin/index.js
@@ -1,5 +1,6 @@
 import cssConventions from "./css-conventions.js";
 import noGlobalStyles from "./no-global-styles.js";
+import noUselessClsx from "./no-useless-clsx.js";
 import preferStylesImport from "./prefer-styles-import.js";
 import preferTestingLibrary from "./prefer-testing-library.js";
 import sprinklesConventions from "./sprinkles-conventions.js";
@@ -8,6 +9,7 @@ export default {
   rules: {
     "css-conventions": cssConventions,
     "no-global-styles": noGlobalStyles,
+    "no-useless-clsx": noUselessClsx,
     "prefer-styles-import": preferStylesImport,
     "prefer-testing-library": preferTestingLibrary,
     "sprinkles-conventions": sprinklesConventions,
