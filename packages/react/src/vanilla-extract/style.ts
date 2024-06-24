f75e4066a3a109cdf25d2545056bcee24321ce46
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-24 10:44:15 -0400

    use separate layer for component styles
    and push it below sprinkle layer so sprinkles can always overwrite component
    styles

--- packages/react/src/vanilla-extract/style.ts
+++ packages/react/src/vanilla-extract/style.ts
@@ -6,7 +6,7 @@ export const style = (...args: Parameters<typeof veStyle>) =>
   veStyle(
     (Array.isArray(args[0]) ? args[0] : [args[0]]).map((arg) =>
       typeof arg === "object" && !Array.isArray(arg)
-        ? { "@layer": { [layers.axiom]: arg } }
+        ? { "@layer": { [layers.components]: arg } }
         : arg,
     ),
     args[1],
