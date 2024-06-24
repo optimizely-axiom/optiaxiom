1a4003a1b82da04c11d9da9b8b759bee36cf9163
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-24 10:44:15 -0400

    use separate layer for component styles
    and push it below sprinkle layer so sprinkles can always overwrite component
    styles

--- packages/react/src/styles/layers.css.ts
+++ packages/react/src/styles/layers.css.ts
@@ -3,3 +3,4 @@ import { globalLayer, layer } from "@vanilla-extract/css";
 
 export const axiom = globalLayer("optiaxiom");
 export const reset = layer({ parent: axiom }, "reset");
+export const components = layer({ parent: axiom }, "components");
