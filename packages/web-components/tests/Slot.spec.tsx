6e5a51a5c94dd47073d7bee4b8a93deb3f3282f2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-25 13:15:29 -0400

    add browser based unit testing for web components

---
+++ packages/web-components/tests/Slot.spec.tsx
@@ -0,0 +1,22 @@
+import { Button } from "@optiaxiom/web-components/Button";
+import { describe, expect, it } from "vitest";
+
+import { render, screen, withinShadowRoot } from "../vitest.rtl";
+
+describe("Component slots", () => {
+  function setup(overrides = {}) {
+    return render(
+      <Button preset="primary" {...overrides}>
+        <i slot="rightSection">icon</i>
+        Primary
+      </Button>,
+    );
+  }
+
+  it("should render properly", async () => {
+    setup();
+    expect(
+      withinShadowRoot(screen.getByText("Primary")).getByRole("button"),
+    ).toHaveTextContent("Primaryicon");
+  });
+});
