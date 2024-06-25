05e3263dae4862bbb91eb5cacdcc3e3972f97c8f
Rafi Adnan <rafi.adnan@optimizely.com>
2024-06-25 13:35:42 -0400

    replace framer-motion with simpler custom alternative
    framer-motion is not really needed and the version that supports react <18 is
    really old and also causes issues with unit tests

---
+++ packages/react/src/animate-presence/AnimatePresence.tsx
@@ -0,0 +1,51 @@
+import {
+  type ReactElement,
+  type RefObject,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+
+import { PresenceContext } from "./PresenceContext";
+
+export const AnimatePresence = ({
+  children,
+}: {
+  children?: ReactElement | false;
+}) => {
+  const [exiting, setExiting] = useState<Map<RefObject<undefined>, boolean>>(
+    new Map(),
+  );
+  const lastChildren = useRef(children);
+  if (children) {
+    lastChildren.current = children;
+  }
+
+  const isPresent = !!children;
+  const onExitComplete = useCallback((id: RefObject<undefined>) => {
+    setExiting((state) => {
+      state.delete(id);
+      return new Map(state.entries());
+    });
+  }, []);
+  const register = useCallback((id: RefObject<undefined>) => {
+    setExiting((state) => {
+      state.set(id, true);
+      return new Map(state.entries());
+    });
+  }, []);
+
+  useEffect(() => {
+    if (!isPresent && [...exiting.values()].some(Boolean)) {
+      lastChildren.current = false;
+    }
+  }, [exiting, isPresent]);
+
+  return (
+    <PresenceContext.Provider value={{ isPresent, onExitComplete, register }}>
+      {children ||
+        ([...exiting.values()].some(Boolean) ? lastChildren.current : null)}
+    </PresenceContext.Provider>
+  );
+};
