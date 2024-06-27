e7d3b94dc71aabbfd9d00def6e399c4fd72d8859
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-06-27 16:14:02 +0600

    pass props to button children

--- packages/react/src/button-group/ButtonGroup.tsx
+++ packages/react/src/button-group/ButtonGroup.tsx
@@ -2,35 +2,66 @@ import {
   Children,
   type ComponentPropsWithRef,
   type ReactNode,
+  cloneElement,
   forwardRef,
+  isValidElement,
 } from "react";
 
 import type { ExtendProps } from "../utils";
 
 import { Box } from "../box";
+import { Button } from "../button/Button";
 import { Flex } from "../flex";
 import * as styles from "./ButtonGroup.css";
 
 type ButtonGroupProps = ExtendProps<
+  ComponentPropsWithRef<typeof Button>,
   ComponentPropsWithRef<typeof Flex>,
   {
     children: ReactNode;
+    disabled?: boolean;
   } & styles.ButtonGroupVariants
 >;
 
 export const ButtonGroup = forwardRef<HTMLDivElement, ButtonGroupProps>(
   (
-    { children, className, gap = "0", orientation = "horizontal", ...props },
+    {
+      children,
+      className,
+      colorScheme,
+      disabled,
+      gap = "0",
+      orientation = "horizontal",
+      preset,
+      size,
+      variant,
+      ...props
+    },
     ref,
   ) => {
-    const mappedChildren = Children.map(children, (child) => (
-      <Box
-        asChild
-        className={styles.button({ orientation, spacing: gap !== "0" })}
-      >
-        {child}
-      </Box>
-    ));
+    const mappedChildren = Children.map(children, (child) => {
+      if (isValidElement(child)) {
+        return (
+          <Box
+            asChild
+            className={styles.button({ orientation, spacing: gap !== "0" })}
+          >
+            {cloneElement(
+              child as React.ReactElement<ComponentPropsWithRef<typeof Button>>,
+              {
+                colorScheme: child.props.colorScheme || colorScheme,
+                disabled: child.props.disabled || disabled,
+                preset: child.props.preset || preset,
+                size: child.props.size || size,
+                variant: child.props.variant || variant,
+              },
+            )}
+          </Box>
+        );
+      }
+      return child;
+    });
+
     return (
       <Flex
         className={className}
