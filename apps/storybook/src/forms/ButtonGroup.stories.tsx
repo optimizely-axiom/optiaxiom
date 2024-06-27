2dec55e727599c00fb0a300d5fcdb2aeab218b50
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-06-27 21:57:16 +0600

    add Button Group stories

---
+++ apps/storybook/src/forms/ButtonGroup.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button, ButtonGroup } from "@optiaxiom/react";
+import { IconArrowRight, IconDownload, IconPhoto } from "@tabler/icons-react";
+
+const meta: Meta<typeof ButtonGroup> = {};
+
+export default meta;
+
+type Story = StoryObj<typeof ButtonGroup>;
+
+export const Horizontal: Story = {
+  render: () => (
+    <ButtonGroup>
+      <Button leftSection={<IconPhoto />}>Gallery</Button>
+      <Button rightSection={<IconDownload />}>Download</Button>
+      <Button leftSection={<IconPhoto />} rightSection={<IconArrowRight />}>
+        Enter Gallery
+      </Button>
+    </ButtonGroup>
+  ),
+};
+export const Vertical: Story = {
+  render: () => (
+    <ButtonGroup orientation="vertical">
+      <Button leftSection={<IconPhoto />}>Gallery</Button>
+      <Button rightSection={<IconDownload />}>Download</Button>
+      <Button leftSection={<IconPhoto />} rightSection={<IconArrowRight />}>
+        Enter Gallery
+      </Button>
+    </ButtonGroup>
+  ),
+};
