36751893bd959b779f4b4751c6f1f8e5f86ebfcd
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-01 13:36:06 -0400

    split eslint rules into separate files
    for easier management, readability, and in case we need to disable particular
    rules

--- packages/shared/.eslintplugin/prefer-recipe-export.js
+++ packages/shared/.eslintplugin/prefer-recipe-export.js
@@ -16,24 +16,6 @@ export default ESLintUtils.RuleCreator.withoutDocs({
       },
 
       /**
-       * @type {import('@typescript-eslint/utils').TSESLint.RuleListener['Property']}
-       */
-      'CallExpression:matches([callee.name="recipe"], [callee.name="style"]) Property[key.name="selectors"] > ObjectExpression > Property':
-        (prop) => {
-          const node = prop.key;
-          if (node.type !== "Literal" || !node.raw.includes("[aria-")) {
-            return;
-          }
-
-          const expected = node.raw.replaceAll("aria-", "data-");
-          context.report({
-            fix: (fixer) => fixer.replaceText(node, expected),
-            messageId: "aria",
-            node,
-          });
-        },
-
-      /**
        * @type {import('@typescript-eslint/utils').TSESLint.RuleListener['CallExpression']}
        */
       'ExportNamedDeclaration > VariableDeclaration > VariableDeclarator > CallExpression:matches([callee.name="style"], [callee.name="styleVariants"])':
@@ -68,7 +50,6 @@ export default ESLintUtils.RuleCreator.withoutDocs({
   meta: {
     fixable: "code",
     messages: {
-      aria: "Please use data-* attributes instead of aria-* attributes when writing CSS selectors.",
       preferRecipe: `Please replace {{method}}() with recipe() as top-level export.
 
 Our convention is to always expose recipes rather than directly exposing raw styles.`,
