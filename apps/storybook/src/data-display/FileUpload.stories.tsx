05cd4f5e99c24525e671269579bcc30a5f9fcf22
Bishwajit <bishwajit0020@gmail.com>
2024-07-03 12:01:55 +0600

    Add FileUpload primary story

--- apps/storybook/src/data-display/FileUpload.stories.tsx
+++ apps/storybook/src/data-display/FileUpload.stories.tsx
@@ -20,37 +20,59 @@ type Story = StoryObj<typeof FileUpload>;
 export const Primary: Story = {
   render: () => {
     return (
-      <FileUpload maxFiles={5}>
-        <FileUploadLabel>
-          <p>File Upload</p>
-        </FileUploadLabel>
-        <Box asChild border="2" h="full" w="full">
-          <FileUploadDropzone>Drag your files here</FileUploadDropzone>
-        </Box>
-        <FileUploadContext>
-          {({ acceptedFiles, rejectedFiles }) => {
-            return (
-              <Flex flexDirection="column">
-                <Box>
-                  <p> Accepted Files </p>
-                  {acceptedFiles.map((file) => (
-                    <p key={file.name}> {file.name}</p>
-                  ))}
-                </Box>
-                <Box>
-                  <p> Rejected Files </p>
-                  {rejectedFiles.map((fileRejection) => (
-                    <p key={fileRejection.file.name}>
-                      {" "}
-                      {fileRejection.file.name}
-                    </p>
-                  ))}
-                </Box>
-              </Flex>
-            );
-          }}
-        </FileUploadContext>
-      </FileUpload>
+      <Flex
+        alignItems="center"
+        asChild
+        bg="bg.brand.subtle"
+        border="1"
+        justifyContent="center"
+        rounded="lg"
+        w="320"
+      >
+        <FileUpload maxFiles={5}>
+          <FileUploadLabel>
+            <p>File Upload</p>
+          </FileUploadLabel>
+          <Flex
+            alignItems="center"
+            asChild
+            border="1"
+            h="320"
+            justifyContent="center"
+            rounded="lg"
+            w="full"
+          >
+            <FileUploadDropzone>
+              <Box border="1" p="10" rounded="sm">
+                Drag your files here
+              </Box>
+            </FileUploadDropzone>
+          </Flex>
+          <FileUploadContext>
+            {({ acceptedFiles, rejectedFiles }) => {
+              return (
+                <Flex alignItems="center" flexDirection="column">
+                  <Box>
+                    <p> Accepted Files </p>
+                    {acceptedFiles.map((file) => (
+                      <p key={file.name}> {file.name}</p>
+                    ))}
+                  </Box>
+                  <Box>
+                    <p> Rejected Files </p>
+                    {rejectedFiles.map((fileRejection) => (
+                      <p key={fileRejection.file.name}>
+                        {" "}
+                        {fileRejection.file.name}
+                      </p>
+                    ))}
+                  </Box>
+                </Flex>
+              );
+            }}
+          </FileUploadContext>
+        </FileUpload>
+      </Flex>
     );
   },
 };
