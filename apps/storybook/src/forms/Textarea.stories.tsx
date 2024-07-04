4a99fb4159b27847f18679de2af6a02565b18c58
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-04 13:20:14 +0600

    update stories

---
+++ apps/storybook/src/forms/Textarea.stories.tsx
@@ -0,0 +1,89 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Button, Flex, Text, Textarea } from "@optiaxiom/react";
+
+const meta: Meta<typeof Textarea> = {
+  component: Textarea,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Textarea>;
+
+export const Default: Story = {
+  args: {
+    placeholder: "Enter text....",
+  },
+};
+
+export const WithTopSection: Story = {
+  args: {
+    placeholder: "Enter text....",
+    topSection: <Text>Top Section</Text>,
+  },
+};
+
+export const WithBottomSection: Story = {
+  args: {
+    bottomSection: <Text>Bottom Section</Text>,
+    placeholder: "Enter text....",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    placeholder: "Enter text....",
+  },
+};
+
+export const Readonly: Story = {
+  args: {
+    placeholder: "Enter text....",
+    readOnly: true,
+  },
+};
+
+export const NoResize: Story = {
+  args: {
+    placeholder: "Enter text....",
+    resize: "none",
+  },
+};
+
+export const AutoResizeable: Story = {
+  args: {
+    autoResize: true,
+    placeholder: "Enter text....",
+  },
+};
+
+export const Custom: Story = {
+  args: {
+    autoResize: true,
+    bottomSection: (
+      <Flex
+        style={{
+          alignItems: "stretch",
+          justifyContent: "center",
+        }}
+      >
+        <Button preset="primary">Bottom Section</Button>
+      </Flex>
+    ),
+    placeholder: "Write your text",
+    topSection: <Text>Top Section</Text>,
+  },
+};
+
+export const TypesOfTextarea: Story = {
+  render: () => (
+    <Flex>
+      <Textarea disabled={false} placeholder="Default Placeholder" />
+      <Textarea defaultValue="Disabled with value" disabled />
+      <Textarea disabled placeholder="Disabled placeholder" />
+      <Textarea error placeholder="Error state" />
+      <Textarea defaultValue="Error with value" error />
+    </Flex>
+  ),
+};
