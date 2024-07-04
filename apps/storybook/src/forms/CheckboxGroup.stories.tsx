47bc09a667c9aa82b26f7eaafdbcd3cb70e43595
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-04 09:07:07 +0600

    update stories of Checkbox Group Component

--- apps/storybook/src/forms/CheckboxGroup.stories.tsx
+++ apps/storybook/src/forms/CheckboxGroup.stories.tsx
@@ -4,6 +4,7 @@ import { CheckboxGroup } from "@optiaxiom/react";
 
 const meta: Meta<typeof CheckboxGroup> = {
   component: CheckboxGroup,
+  title: "CheckboxGroup",
 };
 
 export default meta;
@@ -12,6 +13,58 @@ type Story = StoryObj<typeof CheckboxGroup>;
 
 export const Basic: Story = {
   args: {
-    label: "Test",
+    checkboxItems: [
+      { id: "item1", label: "Item 1" },
+      { id: "item2", label: "Item 2" },
+      { id: "item3", label: "Item 3" },
+    ],
+    label: "Select All",
+  },
+};
+
+export const WithHelperText: Story = {
+  args: {
+    checkboxItems: [
+      { helperText: "Helper Text 1", id: "item1", label: "Item 1" },
+      { helperText: "Helper Text 2", id: "item2", label: "Item 2" },
+      { helperText: "Helper Text 3", id: "item3", label: "Item 3" },
+    ],
+    helperText: "This is a helper text",
+    label: "Select All",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    checkboxItems: [
+      { id: "item1", label: "Item 1" },
+      { id: "item2", label: "Item 2" },
+      { id: "item3", label: "Item 3" },
+    ],
+    disabled: true,
+    label: "Select All",
+  },
+};
+
+export const Readonly: Story = {
+  args: {
+    checkboxItems: [
+      { id: "item1", label: "Item 1" },
+      { id: "item2", label: "Item 2" },
+      { id: "item3", label: "Item 3" },
+    ],
+    label: "Select All",
+    readonly: true,
+  },
+};
+
+export const MixedStates: Story = {
+  args: {
+    checkboxItems: [
+      { defaultChecked: true, id: "item1", label: "Item 1" },
+      { id: "item2", label: "Item 2" },
+      { defaultChecked: "indeterminate", id: "item3", label: "Item 3" },
+    ],
+    label: "Select All",
   },
 };
