4cb43056ae07b3b475701d8d8bf5229e48274e2f
toha993 <mutasim.toha@optimizely.com>
2024-07-04 18:58:11 +0600

    Update story

--- apps/storybook/src/navigation/Tabs.stories.tsx
+++ apps/storybook/src/navigation/Tabs.stories.tsx
@@ -7,7 +7,6 @@ import {
   TabsList,
   TabsTrigger,
 } from "@optiaxiom/react";
-import { expect, userEvent, within } from "@storybook/test";
 
 const meta: Meta<typeof Tabs> = {
   component: Tabs,
@@ -18,36 +17,6 @@ export default meta;
 type Story = StoryObj<typeof Tabs>;
 
 export const Primary: Story = {
-  play: async ({ canvasElement }) => {
-    const canvas = within(canvasElement);
-
-    await expect(
-      canvas.getByRole("tab", { name: "First" }),
-    ).toBeInTheDocument();
-    await expect(
-      canvas.getByRole("tab", { name: "Second" }),
-    ).toBeInTheDocument();
-    await expect(
-      canvas.getByRole("tab", { name: "Third" }),
-    ).toBeInTheDocument();
-    await expect(canvas.getByRole("tab", { name: "First" })).toHaveAttribute(
-      "aria-selected",
-      "true",
-    );
-    await expect(canvas.getByText("This is first content")).toBeVisible();
-    await userEvent.click(canvas.getByRole("tab", { name: "Second" }));
-    await expect(canvas.getByRole("tab", { name: "Second" })).toHaveAttribute(
-      "aria-selected",
-      "true",
-    );
-    await expect(canvas.getByText("This is second content")).toBeVisible();
-    await userEvent.click(canvas.getByRole("tab", { name: "Third" }));
-    await expect(canvas.getByRole("tab", { name: "Third" })).toHaveAttribute(
-      "aria-selected",
-      "true",
-    );
-    await expect(canvas.getByText("This is third content")).toBeVisible();
-  },
   render: () => (
     <Tabs defaultValue="first">
       <TabsList>
@@ -70,7 +39,7 @@ export const Primary: Story = {
 
 export const CustomContent: Story = {
   render: () => (
-    <Tabs defaultValue="tab1" flexDirection="column">
+    <Tabs defaultValue="tab1">
       <TabsList>
         <TabsTrigger value="tab1">Account</TabsTrigger>
         <TabsTrigger disabled value="tab2">
