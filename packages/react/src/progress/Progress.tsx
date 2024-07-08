8b0098a303e550dfc2b4ebcd039953e5ad8cf9c6
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-08 16:52:07 -0400

    explicitly use HTMLDivElement
    instead of inferring it from ProgressPrimitive.Root which becomes self
    referential

--- packages/react/src/progress/Progress.tsx
+++ packages/react/src/progress/Progress.tsx
@@ -1,5 +1,5 @@
 import * as ProgressPrimitive from "@radix-ui/react-progress";
-import { type ComponentPropsWithRef, type ElementRef, forwardRef } from "react";
+import { type ComponentPropsWithRef, forwardRef } from "react";
 
 import { Box } from "../box";
 import { type ExtendProps } from "../utils";
@@ -9,37 +9,36 @@ type ProgressProps = ExtendProps<
   ComponentPropsWithRef<typeof Box>
 >;
 
-export const Progress = forwardRef<
-  ElementRef<typeof ProgressPrimitive.Root>,
-  ProgressProps
->((props, ref) => {
-  const widthPercentage =
-    ((props.value ?? 0) / (props.max ?? DEFAULT_MAX)) * 100;
-  const isValidValue =
-    typeof props.value !== "undefined" &&
-    props.value !== null &&
-    props.value >= 0 &&
-    props.value <= (props.max ?? DEFAULT_MAX);
+export const Progress = forwardRef<HTMLDivElement, ProgressProps>(
+  (props, ref) => {
+    const widthPercentage =
+      ((props.value ?? 0) / (props.max ?? DEFAULT_MAX)) * 100;
+    const isValidValue =
+      typeof props.value !== "undefined" &&
+      props.value !== null &&
+      props.value >= 0 &&
+      props.value <= (props.max ?? DEFAULT_MAX);
 
-  return (
-    <Box asChild bg="border.disabled" h="6" rounded="lg" {...props}>
-      <ProgressPrimitive.Root ref={ref}>
-        {isValidValue && (
-          <Box
-            asChild
-            bg="bg.brand.solid"
-            h="full"
-            rounded="lg"
-            style={{ width: `${widthPercentage}%` }}
-            transition="all"
-          >
-            <ProgressPrimitive.Indicator />
-          </Box>
-        )}
-      </ProgressPrimitive.Root>
-    </Box>
-  );
-});
+    return (
+      <Box asChild bg="border.disabled" h="6" rounded="lg" {...props}>
+        <ProgressPrimitive.Root ref={ref}>
+          {isValidValue && (
+            <Box
+              asChild
+              bg="bg.brand.solid"
+              h="full"
+              rounded="lg"
+              style={{ width: `${widthPercentage}%` }}
+              transition="all"
+            >
+              <ProgressPrimitive.Indicator />
+            </Box>
+          )}
+        </ProgressPrimitive.Root>
+      </Box>
+    );
+  },
+);
 
 const DEFAULT_MAX = 100;
 Progress.displayName = "@optiaxiom/react/Progress";
