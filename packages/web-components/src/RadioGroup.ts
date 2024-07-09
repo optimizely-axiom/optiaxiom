5c77bf43a29d926488eedf724ee12e0aa09c7a46
toha993 <mutasim.toha@optimizely.com>
2024-07-09 16:16:28 +0600

    Add web component

---
+++ packages/web-components/src/RadioGroup.ts
@@ -0,0 +1,17 @@
+import { RadioGroup as RadioGroupComponent } from "@optiaxiom/react";
+
+import type { ComponentAttributes } from "./ComponentAttributes";
+
+import { register } from "./register";
+
+export const RadioGroup = "ax-radio-group";
+register(RadioGroup, RadioGroupComponent);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      [RadioGroup]: ComponentAttributes<typeof RadioGroupComponent>;
+    }
+  }
+}
