d40376f5a81dcc24f2b467f954013391303ca7a2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-11 14:03:45 -0400

    allow both controlled and uncontrolled usage of tooltips

--- apps/storybook/src/overlays/Tooltip.stories.tsx
+++ apps/storybook/src/overlays/Tooltip.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Flex, Text, Tooltip } from "@optiaxiom/react";
+import { Flex, Switch, Text, Tooltip } from "@optiaxiom/react";
 import {
   expect,
   screen,
@@ -8,6 +8,7 @@ import {
   waitForElementToBeRemoved,
   within,
 } from "@storybook/test";
+import { useState } from "react";
 
 export default {
   component: Tooltip,
@@ -32,6 +33,60 @@ export const Primary: Story = {
     await waitForElementToBeRemoved(
       screen.queryByRole("tooltip", { name: "Add to library" }),
     );
+  },
+};
+
+export const Controlled: Story = {
+  args: {
+    children: <button>Hover</button>,
+    content: "Add to library",
+    defaultOpen: true,
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+
+    await userEvent.hover(canvas.getByRole("button"));
+    await expect(
+      await screen.findByRole("tooltip", { name: "Add to library" }),
+    ).toBeInTheDocument();
+    await userEvent.click(document.body);
+    await waitForElementToBeRemoved(
+      screen.queryByRole("tooltip", { name: "Add to library" }),
+    );
+
+    await userEvent.click(canvas.getByRole("switch"));
+    await userEvent.hover(canvas.getByRole("button"));
+    await expect(
+      screen.queryByRole("tooltip", { name: "Add to library" }),
+    ).not.toBeInTheDocument();
+  },
+  render: function SampleStory() {
+    const [enabled, setEnabled] = useState(true);
+    const [open, setOpen] = useState(false);
+
+    return (
+      <Flex flexDirection="row">
+        <Switch checked={enabled} onCheckedChange={setEnabled}>
+          Enable tooltip
+        </Switch>
+
+        <Tooltip
+          content="Add to library"
+          onOpenChange={(flag) => enabled && setOpen(flag)}
+          open={open}
+        >
+          <button>Hover</button>
+        </Tooltip>
+      </Flex>
+    );
+  },
+};
+
+export const DefaultOpen: Story = {
+  args: {
+    children: <button>Hover</button>,
+    content: "Add to library",
+    defaultOpen: true,
   },
 };
 
