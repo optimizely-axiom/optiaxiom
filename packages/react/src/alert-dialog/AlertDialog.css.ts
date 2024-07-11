c70432f7baf8c7510e50d21e5c515159332071c1
toha993 <mutasim.toha@optimizely.com>
2024-07-11 16:47:44 +0600

    Update styling

--- packages/react/src/alert-dialog/AlertDialog.css.ts
+++ packages/react/src/alert-dialog/AlertDialog.css.ts
@@ -3,6 +3,11 @@ import { type RecipeVariants, recipe, style } from "../vanilla-extract";
 
 export const content = recipe({
   base: [
+    {
+      bg: "white",
+      rounded: "lg",
+      shadow: "md",
+    },
     style({
       position: "absolute",
     }),
@@ -18,6 +23,12 @@ export const content = recipe({
 
 export const overlay = recipe({
   base: [
+    {
+      alignItems: "center",
+      bg: "dark.200",
+      h: "full",
+      w: "full",
+    },
     style({
       position: "absolute",
     }),
@@ -26,8 +37,41 @@ export const overlay = recipe({
 
 export const footer = recipe({
   base: [
+    {
+      flexDirection: "row",
+      gap: "md",
+      justifyContent: "end",
+      px: "24",
+      py: "20",
+    },
     style({
       borderTop: `1px solid ${theme.colors["border.secondary"]}`,
+    }),
+  ],
+});
+
+export const title = recipe({
+  base: [
+    {
+      fontSize: "2xl",
+      fontWeight: "600",
+      pb: "16",
+      pt: "24",
+      px: "24",
+    },
+  ],
+});
+
+export const description = recipe({
+  base: [
+    {
+      fontSize: "md",
+      overflow: "auto",
+      px: "24",
+      py: "16",
+    },
+    style({
+      maxHeight: "50dvh",
     }),
   ],
 });
