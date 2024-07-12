f8c89ad3f2ac16af69c97c69d0df65f9f23d54e1
toha993 <mutasim.toha@optimizely.com>
2024-07-12 16:38:15 +0600

    Update styling

--- packages/react/src/badge/Badge.tsx
+++ packages/react/src/badge/Badge.tsx
@@ -6,30 +6,46 @@ import { Flex } from "../flex";
 import { Text } from "../text";
 import * as styles from "./Badge.css";
 
+const appearances = {
+  danger: { colorScheme: "danger", variant: "accent" },
+  info: { colorScheme: "info", variant: "accent" },
+  neutral: { colorScheme: "neutral", variant: "accent" },
+  primary: { colorScheme: "primary", variant: "subtle" },
+  success: { colorScheme: "success", variant: "subtle" },
+  warning: { colorScheme: "warning", variant: "subtle" },
+} satisfies Record<string, styles.BadgeVariants>;
+
 type BadgeProps = ExtendProps<
   ComponentPropsWithRef<typeof Flex>,
   {
+    appearance?: keyof typeof appearances;
     children: ReactNode;
-    type?:
-      | "danger"
-      | "default"
-      | "primary"
-      | "secondary"
-      | "success"
-      | "warning";
-  }
+  } & styles.BadgeVariants
 >;
 
 export const Badge = forwardRef<HTMLDivElement, BadgeProps>(
-  ({ children, className, type = "default", ...props }, ref) => {
+  (
+    {
+      appearance = "neutral",
+      children,
+      className,
+      colorScheme,
+      variant,
+      ...props
+    },
+    ref,
+  ) => {
+    const presetProps = appearances[appearance];
+    const finalColorScheme = colorScheme ?? presetProps.colorScheme;
+    const finalVariant = variant ?? presetProps.variant;
     return (
       <Flex
-        px="8"
-        py="4"
-        rounded="md"
-        {...props}
-        {...styles.badge({ type }, className)}
+        {...styles.badge(
+          { colorScheme: finalColorScheme, variant: finalVariant },
+          className,
+        )}
         ref={ref}
+        {...props}
       >
         <Text as="span" fontSize="inherit">
           {children}
