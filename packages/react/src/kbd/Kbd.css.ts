58d1e6a5cb472ab9bb383e5276555cc92e864e4f
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-12 13:43:25 -0400

    inline sprinkle props in jsx
    since we're not using any custom css

--- packages/react/src/kbd/Kbd.css.ts
+++ packages/react/src/kbd/Kbd.css.ts
@@ -1,20 +1,5 @@
 import { recipe, style } from "../vanilla-extract";
 
-export const kbd = recipe({
-  base: [
-    {
-      alignItems: "center",
-      border: "1",
-      borderB: "2",
-      display: "inline-flex",
-      flexDirection: "row",
-      fontWeight: "600",
-      gap: "4",
-      whiteSpace: "nowrap",
-    },
-  ],
-});
-
 export const keys = recipe({
   base: style({
     fontSize: "1.2em",
