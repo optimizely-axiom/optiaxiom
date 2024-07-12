056b6eba31e2613b62366d3645d190a3ff812dc8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-12 17:43:35 -0400

    document button group component

---
+++ apps/docs/demos/button/group-usage/App.tsx
@@ -0,0 +1,11 @@
+import { Button, ButtonGroup } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <ButtonGroup>
+      <Button>First</Button>
+      <Button>Second</Button>
+      <Button>Third</Button>
+    </ButtonGroup>
+  );
+}
