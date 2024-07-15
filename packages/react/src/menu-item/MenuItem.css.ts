e74c0530f64b0d3f9d92845fe478abd87d5ec505
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-15 15:33:33 +0600

    add props & styling to menu items

--- packages/react/src/menu-item/MenuItem.css.ts
+++ packages/react/src/menu-item/MenuItem.css.ts
@@ -1,9 +1,20 @@
 import { recipe } from "../vanilla-extract";
 
+export const wrapper = recipe({
+  base: [
+    {
+      gap: "xs",
+    },
+  ],
+});
+
 export const item = recipe({
   base: [
     {
-      p: "xs",
+      fontSize: "md",
+      // border: "1",
+      px: "md",
+      py: "10",
     },
   ],
 });
