3842950cbde31e93da241ba4a5556948a204d58c
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-16 11:22:02 +0600

    remove iconUnchecked option

--- packages/react/src/checkbox/icons-svg/IconIndeterminate.tsx
+++ packages/react/src/checkbox/icons-svg/IconIndeterminate.tsx
@@ -11,7 +11,7 @@ export const IconIndeterminate = (props: ComponentPropsWithRef<"svg">) => {
     >
       <path
         d="M2.85714 3.19995C2.35714 3.19995 2 3.56662 2 3.99995C2 4.46662 2.35714 4.79995 2.85714 4.79995H9.14286C9.60714 4.79995 10 4.46662 10 3.99995C10 3.56662 9.60714 3.19995 9.14286 3.19995H2.85714Z"
-        fill="white"
+        fill="currentColor"
       />
     </svg>
   );
