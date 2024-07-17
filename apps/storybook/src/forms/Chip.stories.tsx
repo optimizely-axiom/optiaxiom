704186ceb7c69aea620ca2203b97afb7f334d156
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-17 10:30:19 +0600

    update stories

--- apps/storybook/src/forms/Chip.stories.tsx
+++ apps/storybook/src/forms/Chip.stories.tsx
@@ -1,97 +1,53 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Chip, Flex } from "@optiaxiom/react";
-import { IconChevronDown } from "@tabler/icons-react";
+import { Avatar, Chip, Flex } from "@optiaxiom/react";
 
 export default {
-  argTypes: {
-    onClick: { action: "click" },
-  },
   component: Chip,
 } as Meta<typeof Chip>;
 
 type Story = StoryObj<typeof Chip>;
 
-const Variants: Story = {
-  render: (args) => (
-    <Flex flexDirection="column" gap="sm">
-      {variants.map(([variant, label]) => (
-        <Flex flexDirection="row" gap="sm" key={variant}>
-          {sizes.map((size) => (
-            <Chip {...args} key={size} size={size} variant={variant}>
-              {label} - {size}
-            </Chip>
-          ))}
-          <Chip disabled {...args} variant={variant}>
-            Disabled
-          </Chip>
-        </Flex>
-      ))}
-    </Flex>
-  ),
-};
-
-const sizes = ["sm", "md", "lg"] as const;
-
-const variants = [
-  ["solid", "Solid"],
-  ["outline", "Outline"],
-] as const;
-
-export const Default: Story = {
-  ...Variants,
+export const Basic: Story = {
   args: {
+    children: "Hello",
     colorScheme: "neutral",
   },
 };
 
-export const Primary: Story = {
-  ...Variants,
-  args: {
-    colorScheme: "primary",
-  },
-};
-
-export const Danger: Story = {
-  ...Variants,
-  args: {
-    colorScheme: "danger",
-    onPressedChange: () => {},
-  },
-};
-
-export const Link: Story = {
-  args: {
-    asChild: true,
-    children: <a href="/">Sample Link</a>,
-  },
-};
-
-export const StandaloneIcon: Story = {
-  render: (args) => (
-    <Flex flexDirection="row">
-      <Chip {...args} size="sm" startDecorator={<IconChevronDown />} />
-      <Chip {...args} size="md" startDecorator={<IconChevronDown />} />
-      <Chip {...args} size="lg" startDecorator={<IconChevronDown />} />
-    </Flex>
-  ),
-};
-export const Icons: Story = {
+export const DifferentSizes: Story = {
   args: {
     children: "Chip",
   },
   render: (args) => (
     <Flex>
       <Flex flexDirection="row">
-        <Chip {...args} size="sm" startDecorator={<IconChevronDown />} />
-        <Chip {...args} size="md" startDecorator={<IconChevronDown />} />
-        <Chip {...args} size="lg" startDecorator={<IconChevronDown />} />
+        <Chip {...args} size="sm" />
+        <Chip {...args} size="md" />
+        <Chip {...args} size="lg" />
       </Flex>
       <Flex flexDirection="row">
-        <Chip {...args} size="sm" startDecorator={<IconChevronDown />} />
-        <Chip {...args} size="md" startDecorator={<IconChevronDown />} />
-        <Chip {...args} size="lg" startDecorator={<IconChevronDown />} />
+        <Chip {...args} size="sm" />
+        <Chip {...args} size="md" />
+        <Chip {...args} size="lg" />
       </Flex>
     </Flex>
   ),
+};
+
+export const WithAvatar: Story = {
+  args: {
+    children: "Hello",
+    colorScheme: "neutral",
+    onPressedChange: () => {},
+    startDecorator: <Avatar name="Jamie" size="sm" />,
+  },
+};
+
+export const WithCloseButton: Story = {
+  args: {
+    children: "Hello",
+    colorScheme: "neutral",
+    onPressedChange: () => {},
+  },
 };
