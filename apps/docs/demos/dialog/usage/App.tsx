f3a337e46f1a093d6503af6ab74d26427c32efa9
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-17 17:34:11 -0400

    add basic dialog docs

---
+++ apps/docs/demos/dialog/usage/App.tsx
@@ -0,0 +1,35 @@
+import {
+  Button,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+  DialogTitle,
+  Text,
+} from "@optiaxiom/react";
+import { type ComponentPropsWithRef, useState } from "react";
+
+export function App({
+  size = "md",
+}: Pick<ComponentPropsWithRef<typeof Dialog>, "size">) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setOpen(true)}>Open Dialog</Button>
+
+      <Dialog onClose={() => setOpen(false)} open={open} size={size}>
+        <DialogTitle>Modal Title</DialogTitle>
+
+        <DialogBody>
+          <Text>This is the modal body</Text>
+        </DialogBody>
+
+        <DialogFooter>
+          <Button appearance="primary" onClick={() => setOpen(false)}>
+            Close
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
+  );
+}
