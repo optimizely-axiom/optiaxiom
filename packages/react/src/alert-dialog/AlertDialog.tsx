9809389272e7868fb5bf46ac2dbbd00902d93b8c
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-17 18:36:28 -0400

    add alert-dialog animations

--- packages/react/src/alert-dialog/AlertDialog.tsx
+++ packages/react/src/alert-dialog/AlertDialog.tsx
@@ -1,10 +1,12 @@
 import * as RadixAlertDialog from "@radix-ui/react-alert-dialog";
 import { type ComponentPropsWithRef, type ReactNode, forwardRef } from "react";
 
+import { AnimatePresence } from "../animate-presence";
 import { Box, type BoxProps } from "../box";
 import { Button } from "../button";
 import { Flex } from "../flex";
 import { Paper } from "../paper";
+import { Transition } from "../transition";
 import * as styles from "./AlertDialog.css";
 
 type AlertDialogProps = BoxProps<
@@ -38,37 +40,46 @@ export const AlertDialog = forwardRef<HTMLDivElement, AlertDialogProps>(
   ) => {
     return (
       <RadixAlertDialog.Root open={open} {...props}>
-        <RadixAlertDialog.Portal>
-          <Box asChild {...styles.overlay()}>
-            <RadixAlertDialog.Overlay />
-          </Box>
+        <AnimatePresence>
+          {open && (
+            <RadixAlertDialog.Portal forceMount>
+              <Transition>
+                <Box asChild {...styles.overlay()}>
+                  <RadixAlertDialog.Overlay />
+                </Box>
+              </Transition>
 
-          <Paper asChild {...styles.content({ size })}>
-            <RadixAlertDialog.Content ref={ref}>
-              <Box asChild {...styles.title()}>
-                <RadixAlertDialog.Title>{title}</RadixAlertDialog.Title>
-              </Box>
+              <Transition type="fade-down">
+                <Paper asChild {...styles.content({ size })}>
+                  <RadixAlertDialog.Content ref={ref}>
+                    <Box asChild {...styles.title()}>
+                      <RadixAlertDialog.Title>{title}</RadixAlertDialog.Title>
+                    </Box>
 
-              <Box asChild {...styles.description()}>
-                <RadixAlertDialog.Description>
-                  {children}
-                </RadixAlertDialog.Description>
-              </Box>
-              <Flex {...styles.footer()}>
-                <RadixAlertDialog.Cancel asChild>
-                  <Button appearance="secondary" onClick={onCancel}>
-                    {cancel}
-                  </Button>
-                </RadixAlertDialog.Cancel>
-                <RadixAlertDialog.Action asChild>
-                  <Button appearance={appearance} onClick={onAction}>
-                    {action}
-                  </Button>
-                </RadixAlertDialog.Action>
-              </Flex>
-            </RadixAlertDialog.Content>
-          </Paper>
-        </RadixAlertDialog.Portal>
+                    <Box asChild {...styles.description()}>
+                      <RadixAlertDialog.Description>
+                        {children}
+                      </RadixAlertDialog.Description>
+                    </Box>
+
+                    <Flex {...styles.footer()}>
+                      <RadixAlertDialog.Cancel asChild>
+                        <Button appearance="secondary" onClick={onCancel}>
+                          {cancel}
+                        </Button>
+                      </RadixAlertDialog.Cancel>
+                      <RadixAlertDialog.Action asChild>
+                        <Button appearance={appearance} onClick={onAction}>
+                          {action}
+                        </Button>
+                      </RadixAlertDialog.Action>
+                    </Flex>
+                  </RadixAlertDialog.Content>
+                </Paper>
+              </Transition>
+            </RadixAlertDialog.Portal>
+          )}
+        </AnimatePresence>
       </RadixAlertDialog.Root>
     );
   },
