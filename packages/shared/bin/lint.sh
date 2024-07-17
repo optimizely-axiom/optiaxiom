777e46f67e27c2343e869051c605cd56b0848dc0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-17 19:09:13 -0400

    show prettier logs when running lint in no-fix mode

--- packages/shared/bin/lint.sh
+++ packages/shared/bin/lint.sh
@@ -1,9 +1,9 @@
 set -e
 
-e_flag="--fix" p_flag="-w"
+e_flag="--fix" p_flag="--log-level silent -w"
 [ "$1" = "--no-fix" ] && e_flag= p_flag="-c"
 if [ -f eslint.config.js ]; then
-  prettier --cache --log-level silent $p_flag .
+  prettier --cache $p_flag .
   eslint --cache --quiet $e_flag .
 else
   tsc --noEmit
