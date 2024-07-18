981b1a4461b2245cdd632f2eb26cb1f635bab53d
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-18 18:38:25 -0400

    document all dialog component props
    and include messaging about extending box props within props-table

--- apps/docs/plugins/remark-axiom-plugin/transformPropsTable.mjs
+++ apps/docs/plugins/remark-axiom-plugin/transformPropsTable.mjs
@@ -32,6 +32,8 @@ export function transformPropsTable(tree) {
       const componentRaw = node.attributes.find(
         (attr) => attr.name === "component",
       ).value;
+      const extendsComponent =
+        node.attributes.find((attr) => attr.name === "extends")?.value ?? "Box";
       const component = componentRaw.value ?? componentRaw;
       const doc = docs.find(
         (doc) => doc.displayName === `@optiaxiom/react/${component}`,
@@ -45,6 +47,21 @@ export function transformPropsTable(tree) {
         (d) => d.filePath === doc.filePath.replace(".tsx", ".css.ts"),
       );
 
+      const props = Object.entries(doc.props)
+        .sort(([a], [b]) => a.localeCompare(b))
+        .filter(([, prop]) =>
+          prop.parent
+            ? !prop.parent.fileName.includes("@types/react")
+            : prop.declarations.find(
+                (decl) => decl.fileName === doc.filePath,
+              ) ||
+              styles.find((style) => Object.hasOwn(style.props, prop.name)) ||
+              (component === "Box" &&
+                Object.hasOwn(sprinkles?.props ?? {}, prop.name)),
+        )
+        .filter(([, prop]) => prop.type.name !== "never")
+        .map(([, prop]) => prop);
+
       const tree = fromMarkdown(
         [
           needsImport && `import { Box } from "@optiaxiom/react";`,
@@ -54,58 +71,53 @@ export function transformPropsTable(tree) {
           "",
           `### \`${component}\` component props`,
           "",
-          "<Table>",
-          "  <thead>",
-          "    <tr>",
-          '      <Th asChild style={{ width: "25%" }}>',
-          "        Name",
-          "      </Th>",
-          '      <Th asChild style={{ width: "75%" }}>',
-          "        Type",
-          "      </Th>",
-          "    </tr>",
-          "  </thead>",
-          "  <tbody>",
-          ...Object.entries(doc.props)
-            .sort(([a], [b]) => a.localeCompare(b))
-            .filter(([, prop]) =>
-              prop.parent
-                ? !prop.parent.fileName.includes("@types/react")
-                : prop.declarations.find(
-                    (decl) => decl.fileName === doc.filePath,
-                  ) ||
-                  styles.find((style) =>
-                    Object.hasOwn(style.props, prop.name),
-                  ) ||
-                  (component === "Box" &&
-                    Object.hasOwn(sprinkles?.props ?? {}, prop.name)),
-            )
-            .filter(([, prop]) => prop.type.name !== "never")
-            .flatMap(([, prop]) => [
-              "<Tr>",
-              '  <Td whiteSpace="nowrap">',
-              `    {<Box color="fg.accent.magenta" fontFamily="mono">${prop.name}${prop.required ? "*" : ""}</Box>}`,
-              "  </Td>",
-              "  <Td>",
-              [
-                `<PropType
-                  component="${component}"
-                  prop={${JSON.stringify(prop)}}
-                  sprinkle={${JSON.stringify(sprinkles?.props[prop.name])}}
-                />`,
-                prop.description
-                  .replaceAll(/{@link ([^\s}]+)(?:\s([^}]+))}/g, "[$2]($1)")
-                  .replaceAll(/{@link ([^}]+)}/g, "[$1]($1)")
-                  .replaceAll("@see", "\n\n"),
+          `Supports all [\`${extendsComponent}\`](/components/${kebabCase(extendsComponent)}/props) props` +
+            (props.length > 0 ? " in addition to its own" : "") +
+            ".",
+          ...(props.length > 0
+            ? [
+                "",
+                "<Table>",
+                "  <thead>",
+                "    <tr>",
+                '      <Th asChild style={{ width: "25%" }}>',
+                "        Name",
+                "      </Th>",
+                '      <Th asChild style={{ width: "75%" }}>',
+                "        Type",
+                "      </Th>",
+                "    </tr>",
+                "  </thead>",
+                "  <tbody>",
+                ...props.flatMap((prop) => [
+                  "<Tr>",
+                  '  <Td whiteSpace="nowrap">',
+                  `    {<Box color="fg.accent.magenta" fontFamily="mono">${prop.name}${prop.required ? "*" : ""}</Box>}`,
+                  "  </Td>",
+                  "  <Td>",
+                  [
+                    `<PropType
+                    component="${component}"
+                    prop={${JSON.stringify(prop)}}
+                    sprinkle={${JSON.stringify(sprinkles?.props[prop.name])}}
+                  />`,
+                    prop.description
+                      .replaceAll(/{@link ([^\s}]+)(?:\s([^}]+))}/g, "[$2]($1)")
+                      .replaceAll(/{@link ([^}]+)}/g, "[$1]($1)")
+                      .replaceAll("@see", "\n\n"),
+                  ]
+                    .filter(Boolean)
+                    .join("\n\n"),
+                  "  </Td>",
+                  "</Tr>",
+                ]),
+                "  </tbody>",
+                "</Table>",
               ]
-                .filter(Boolean)
-                .join("\n\n"),
-              "  </Td>",
-              "</Tr>",
-            ]),
-          "  </tbody>",
-          "</Table>",
-        ].join("\n"),
+            : []),
+        ]
+          .filter((line) => line !== false)
+          .join("\n"),
         {
           extensions: [mdxjs()],
           mdastExtensions: [mdxFromMarkdown()],
@@ -121,4 +133,8 @@ export function transformPropsTable(tree) {
       return index + tree.children.length;
     },
   );
+}
+
+function kebabCase(str) {
+  return str.replaceAll(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
 }
