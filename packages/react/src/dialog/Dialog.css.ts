c6bda75f79c580cd1d35555572ad7b1330a48b33
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-18 18:50:53 -0400

    bump size small modal size to 400px
    and limit the width with a fixed 24px offset on each side

--- packages/react/src/dialog/Dialog.css.ts
+++ packages/react/src/dialog/Dialog.css.ts
@@ -9,7 +9,7 @@ export const content = recipe({
     },
     style({
       left: "50%",
-      maxWidth: "90dvw",
+      maxWidth: "calc(100dvw - 2 * 24px)",
       position: "fixed",
       top: "50%",
       translate: "-50% -50%",
@@ -18,7 +18,7 @@ export const content = recipe({
   variants: {
     size: {
       sm: style({
-        width: "375px",
+        width: "400px",
       }),
       md: style({
         width: "600px",
