c22b4ca8da549db34ac421992730e65c29eb7681
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-24 15:30:23 -0400

    remove extra grouping of rules
    this was needed before to prevent the sort lint plugin from complaining
    
    but now we've configured sort lint plugin to allow blank lines to group object
    properties

--- packages/react/src/box/Box.css.ts
+++ packages/react/src/box/Box.css.ts
@@ -21,118 +21,96 @@ export const box = recipe({
             },
           },
 
-          ...{
-            /**
-             * 1. Add the correct height in Firefox.
-             * 2. Correct the inheritance of border color in Firefox. (https://bugzilla.mozilla.org/show_bug.cgi?id=190655)
-             * 3. Ensure horizontal rules are visible by default.
-             */
-            "&:is(hr)": {
-              borderTopWidth: "1px" /* 3 */,
-              color: "inherit" /* 2 */,
-              height: 0 /* 1 */,
-            },
+          /**
+           * 1. Add the correct height in Firefox.
+           * 2. Correct the inheritance of border color in Firefox. (https://bugzilla.mozilla.org/show_bug.cgi?id=190655)
+           * 3. Ensure horizontal rules are visible by default.
+           */
+          "&:is(hr)": {
+            borderTopWidth: "1px" /* 3 */,
+            color: "inherit" /* 2 */,
+            height: 0 /* 1 */,
           },
 
-          ...{
-            /**
-             * Remove the default font size and weight for headings.
-             */
-            "&:is(h1, h2, h3, h4, h5, h6)": {
-              fontSize: "inherit",
-              fontWeight: "inherit",
-            },
+          /**
+           * Remove the default font size and weight for headings.
+           */
+          "&:is(h1, h2, h3, h4, h5, h6)": {
+            fontSize: "inherit",
+            fontWeight: "inherit",
           },
 
-          ...{
-            /**
-             * Add the correct font weight in Edge and Safari.
-             */
-            "&:is(b, strong)": {
-              fontWeight: "bolder",
-            },
+          /**
+           * Add the correct font weight in Edge and Safari.
+           */
+          "&:is(b, strong)": {
+            fontWeight: "bolder",
           },
 
-          ...{
-            /**
-             * 1. Use the user's configured `mono` font-family by default.
-             * 2. Use the user's configured `mono` font-feature-settings by default.
-             * 4. Correct the odd `em` font sizing in all browsers.
-             */
-            "&:is(code, kbd, samp, pre)": {
-              fontFamily: theme.fontFamily.mono /* 1 */,
-              fontFeatureSettings: "normal" /* 2 */,
-              fontSize: "1em" /* 4 */,
-            },
+          /**
+           * 1. Use the user's configured `mono` font-family by default.
+           * 2. Use the user's configured `mono` font-feature-settings by default.
+           * 4. Correct the odd `em` font sizing in all browsers.
+           */
+          "&:is(code, kbd, samp, pre)": {
+            fontFamily: theme.fontFamily.mono /* 1 */,
+            fontFeatureSettings: "normal" /* 2 */,
+            fontSize: "1em" /* 4 */,
           },
 
-          ...{
-            /**
-             * 1. Change the font styles in all browsers.
-             */
-            "&:is(button, input, optgroup, select, textarea)": {
-              color: "inherit" /* 1 */,
-              fontSize: "100%" /* 1 */,
-              letterSpacing: "inherit" /* 1 */,
-            },
+          /**
+           * 1. Change the font styles in all browsers.
+           */
+          "&:is(button, input, optgroup, select, textarea)": {
+            color: "inherit" /* 1 */,
+            fontSize: "100%" /* 1 */,
+            letterSpacing: "inherit" /* 1 */,
           },
 
-          ...{
-            /**
-             * 1. Correct the inability to style clickable types in iOS and Safari.
-             * 2. Remove default button styles.
-             */
-            "&:is(button, input:where([type=button], [type=reset], [type=submit]))":
-              {
-                WebkitAppearance: "button",
-                backgroundColor: "transparent" /* 2 */,
-                backgroundImage: "none" /* 2 */,
-              },
+          /**
+           * 1. Correct the inability to style clickable types in iOS and Safari.
+           * 2. Remove default button styles.
+           */
+          "&:is(button, input:where([type=button], [type=reset], [type=submit]))":
+            {
+              WebkitAppearance: "button",
+              backgroundColor: "transparent" /* 2 */,
+              backgroundImage: "none" /* 2 */,
+            },
+
+          /**
+           * 1. Correct the odd appearance in Chrome and Safari.
+           * 2. Correct the outline style in Safari.
+           */
+          "&:is([type=search])": {
+            WebkitAppearance: "textfield" /* 1 */,
+            outlineOffset: "-2px" /* 2 */,
           },
 
-          ...{
-            /**
-             * 1. Correct the odd appearance in Chrome and Safari.
-             * 2. Correct the outline style in Safari.
-             */
-            "&:is([type=search])": {
-              WebkitAppearance: "textfield" /* 1 */,
-              outlineOffset: "-2px" /* 2 */,
-            },
+          /**
+           * 1. Correct the inability to style clickable types in iOS and Safari.
+           * 2. Change font properties to `inherit` in Safari.
+           */
+          "&::-webkit-file-upload-button": {
+            WebkitAppearance: "button" /* 1 */,
+            font: "inherit" /* 2 */,
           },
 
-          ...{
-            /**
-             * 1. Correct the inability to style clickable types in iOS and Safari.
-             * 2. Change font properties to `inherit` in Safari.
-             */
-            "&::-webkit-file-upload-button": {
-              WebkitAppearance: "button" /* 1 */,
-              font: "inherit" /* 2 */,
-            },
+          "&:is(ol, ul, menu)": {
+            listStyle: "none",
           },
 
-          ...{
-            "&:is(ol, ul, menu)": {
-              listStyle: "none",
-            },
+          "&:is(textarea)": {
+            resize: "vertical",
           },
 
-          ...{
-            "&:is(textarea)": {
-              resize: "vertical",
-            },
-          },
-
-          ...{
-            /**
-             * 1. Reset the default placeholder opacity in Firefox. (https://github.com/tailwindlabs/tailwindcss/issues/3300)
-             * 2. Set the default placeholder color to the user's configured gray 400 color.
-             */
-            "&:is(input, textarea)::placeholder": {
-              color: theme.colors["neutral.600"] /* 2 */,
-              opacity: 1 /* 1 */,
-            },
+          /**
+           * 1. Reset the default placeholder opacity in Firefox. (https://github.com/tailwindlabs/tailwindcss/issues/3300)
+           * 2. Set the default placeholder color to the user's configured gray 400 color.
+           */
+          "&:is(input, textarea)::placeholder": {
+            color: theme.colors["neutral.600"] /* 2 */,
+            opacity: 1 /* 1 */,
           },
 
           ...{
@@ -144,42 +122,34 @@ export const box = recipe({
             },
           },
 
-          ...{
-            /**
-             * Make sure disabled buttons don't get the pointer cursor.
-             */
-            "&:is(:disabled)": {
-              cursor: "default",
-            },
+          /**
+           * Make sure disabled buttons don't get the pointer cursor.
+           */
+          "&:is(:disabled)": {
+            cursor: "default",
           },
 
-          ...{
-            "&::-webkit-search-decoration": {
-              WebkitAppearance: "none",
-            },
-
-            "&::-webkit-search-cancel-button": {
-              WebkitAppearance: "none",
-            },
+          "&::-webkit-search-decoration": {
+            WebkitAppearance: "none",
           },
 
-          ...{
-            /**
-             * Make replaced elements `display: block` by default. (https://github.com/mozdevs/cssremedy/issues/14)
-             */
-            "&:is(img, svg, video, canvas, audio, iframe, embed, object)": {
-              display: "block",
-            },
+          "&::-webkit-search-cancel-button": {
+            WebkitAppearance: "none",
           },
 
-          ...{
-            /**
-             * Constrain images and videos to the parent width and preserve their intrinsic aspect ratio. (https://github.com/mozdevs/cssremedy/issues/14)
-             */
-            "&:is(img, video)": {
-              height: "auto",
-              maxWidth: "100%",
-            },
+          /**
+           * Make replaced elements `display: block` by default. (https://github.com/mozdevs/cssremedy/issues/14)
+           */
+          "&:is(img, svg, video, canvas, audio, iframe, embed, object)": {
+            display: "block",
+          },
+
+          /**
+           * Constrain images and videos to the parent width and preserve their intrinsic aspect ratio. (https://github.com/mozdevs/cssremedy/issues/14)
+           */
+          "&:is(img, video)": {
+            height: "auto",
+            maxWidth: "100%",
           },
 
           "&:is(select)": {
