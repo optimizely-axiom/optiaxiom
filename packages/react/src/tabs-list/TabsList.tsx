50d0ca6b9812f5434d315d7013f82bcfd4cdf637
toha993 <mutasim.toha@optimizely.com>
2024-07-25 00:49:34 +0600

    Update extend props

--- packages/react/src/tabs-list/TabsList.tsx
+++ packages/react/src/tabs-list/TabsList.tsx
@@ -1,17 +1,17 @@
 import * as RadixTabs from "@radix-ui/react-tabs";
 import { type ComponentPropsWithRef, forwardRef } from "react";
 
-import type { ExtendProps } from "../utils";
+import type { BoxProps } from "../box";
 
 import { Flex } from "../flex";
 import * as styles from "./TabsList.css";
 
-type TabsProps = ExtendProps<
-  ComponentPropsWithRef<typeof Flex>,
-  ComponentPropsWithRef<typeof RadixTabs.TabsList>
+type TabsListProps = BoxProps<
+  "div",
+  ComponentPropsWithRef<typeof RadixTabs.List>
 >;
 
-export const TabsList = forwardRef<HTMLDivElement, TabsProps>(
+export const TabsList = forwardRef<HTMLDivElement, TabsListProps>(
   ({ children, ...props }, ref) => {
     return (
       <Flex asChild {...styles.list()}>
--- packages/react/src/tabs/Tabs.tsx
+++ packages/react/src/tabs/Tabs.tsx
@@ -1,14 +1,11 @@
 import * as RadixTabs from "@radix-ui/react-tabs";
 import { type ComponentPropsWithRef, forwardRef } from "react";
 
-import type { ExtendProps } from "../utils";
+import type { BoxProps } from "../box";
 
 import { Flex } from "../flex";
 
-type TabsProps = ExtendProps<
-  ComponentPropsWithRef<typeof Flex>,
-  ComponentPropsWithRef<typeof RadixTabs.Root>
->;
+type TabsProps = BoxProps<"div", ComponentPropsWithRef<typeof RadixTabs.Root>>;
 
 export const Tabs = forwardRef<HTMLDivElement, TabsProps>(
   ({ children, ...props }, ref) => {
