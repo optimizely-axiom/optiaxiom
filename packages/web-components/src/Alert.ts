0df2bb6d9a85ddcc92ae9cc51ae3322747dbc14e
toha993 <mutasim.toha@optimizely.com>
2024-07-25 03:33:33 +0600

    Add alert web component

---
+++ packages/web-components/src/Alert.ts
@@ -0,0 +1,17 @@
+import { Alert as AlertComponent } from "@optiaxiom/react";
+
+import type { ComponentAttributes } from "./ComponentAttributes";
+
+import { register } from "../register";
+
+export const Alert = "ax-alert";
+register(Alert, AlertComponent);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      [Alert]: ComponentAttributes<typeof AlertComponent>;
+    }
+  }
+}
