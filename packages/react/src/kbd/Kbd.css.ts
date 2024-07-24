73ff60f24564c5d4a42133454cec4b963bf7d4f2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-24 16:26:52 -0400

    remove line-height since it's not needed

--- packages/react/src/kbd/Kbd.css.ts
+++ packages/react/src/kbd/Kbd.css.ts
@@ -3,7 +3,6 @@ import { recipe, style } from "../vanilla-extract";
 export const keys = recipe({
   base: style({
     fontSize: "1.2em",
-    lineHeight: "1",
     textDecoration: "none",
   }),
 });
