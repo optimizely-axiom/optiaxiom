d3723aadafd9a6c2e987f888c0b92057326f21f1
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-25 14:42:40 -0400

    refactor indicator
    - simplify stories and make stories consistent with badge
    - add disabled prop to remove badge
    - remove so many overrides of badge and use as-is

--- packages/react/src/indicator/Indicator.tsx
+++ packages/react/src/indicator/Indicator.tsx
@@ -1,42 +1,49 @@
 import { type ReactNode, forwardRef } from "react";
 
-import type { BoxProps } from "../box";
-
 import { Badge } from "../badge";
+import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
 import * as styles from "./Indicator.css";
 
 type IndicatorProps = BoxProps<
   typeof Badge,
   {
+    align?: "end" | "start";
     content: ReactNode;
-  } & styles.IndicatorVariants
+    disabled?: boolean;
+  }
 >;
 
 export const Indicator = forwardRef<HTMLDivElement, IndicatorProps>(
   (
     {
+      align = "start",
+      asChild,
       children,
       className,
-      colorScheme = "neutral",
+      colorScheme,
       content,
-      position = "top-right",
-      variant = "solid",
+      disabled,
+      variant,
       ...props
     },
     ref,
   ) => {
     return (
-      <Flex {...styles.wrapper()}>
-        <Badge
-          colorScheme={colorScheme}
-          ref={ref}
-          variant={variant}
-          {...styles.indicator({ position }, className)}
-          {...props}
-        >
-          {content}
-        </Badge>
+      <Flex ref={ref} {...styles.indicator({}, className)} {...props}>
+        {!disabled && (
+          <Box {...styles.floating({ align })}>
+            <Badge
+              asChild={asChild}
+              colorScheme={colorScheme}
+              variant={variant}
+              {...styles.badge()}
+            >
+              {content}
+            </Badge>
+          </Box>
+        )}
+
         {children}
       </Flex>
     );
