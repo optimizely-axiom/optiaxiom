15018d08381ef1ca23ec77394cdd66b7c69e9c58
Rafi Adnan <rafi.adnan@optimizely.com>
2024-07-25 20:14:01 -0400

    add toast web components

---
+++ packages/web-components/src/components/ToastAction.ts
@@ -0,0 +1,17 @@
+import { ToastAction as ToastActionComponent } from "@optiaxiom/react";
+
+import type { ComponentAttributes } from "./ComponentAttributes";
+
+import { register } from "../register";
+
+export const ToastAction = "ax-toast-action";
+register(ToastAction, ToastActionComponent);
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      [ToastAction]: ComponentAttributes<typeof ToastActionComponent>;
+    }
+  }
+}
