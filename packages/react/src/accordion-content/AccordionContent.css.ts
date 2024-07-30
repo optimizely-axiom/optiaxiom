db102b9398764e2eb79ff13605754955bfd5949f
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-30 16:58:50 +0600

    add animation for slideUp and slideDown

--- packages/react/src/accordion-content/AccordionContent.css.ts
+++ packages/react/src/accordion-content/AccordionContent.css.ts
@@ -1,5 +1,37 @@
-import { recipe } from "../vanilla-extract";
+import { keyframes, recipe, style } from "../vanilla-extract";
 
+const slideDown = keyframes({
+  from: {
+    height: 0,
+  },
+  to: {
+    height: "var(--radix-accordion-content-height)",
+  },
+});
+const slideUp = keyframes({
+  from: {
+    height: "var(--radix-accordion-content-height)",
+  },
+  to: {
+    height: 0,
+  },
+});
 export const wrapper = recipe({
-  base: [],
+  base: [
+    {
+      overflow: "visible",
+      w: "320",
+    },
+    style({
+      minWidth: "var(--radix-accordion-content-width)",
+      selectors: {
+        '&[data-state="closed"]': {
+          animation: `${slideUp} 300ms ease-out`,
+        },
+        '&[data-state="open"]': {
+          animation: `${slideDown} 300ms ease-out`,
+        },
+      },
+    }),
+  ],
 });
