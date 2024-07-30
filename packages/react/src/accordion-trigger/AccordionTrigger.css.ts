5acc1e175276def9dea26f6b52805f6a536293b9
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-07-30 15:43:23 +0600

    add chevron icon for trigger

--- packages/react/src/accordion-trigger/AccordionTrigger.css.ts
+++ packages/react/src/accordion-trigger/AccordionTrigger.css.ts
@@ -1,5 +1,18 @@
-import { recipe } from "../vanilla-extract";
+import { recipe, style } from "../vanilla-extract";
+
+const marker = style({});
 
 export const wrapper = recipe({
-  base: [],
+  base: [marker],
+});
+export const icon = recipe({
+  base: [
+    style({
+      selectors: {
+        [`${marker}[data-state=open] &`]: {
+          transform: "rotate(180deg)",
+        },
+      },
+    }),
+  ],
 });
