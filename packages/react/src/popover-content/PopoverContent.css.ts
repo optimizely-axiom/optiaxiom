9e89af5dd8a445f5f723be71c18da10a11ad3584
toha993 <mutasim.toha@optimizely.com>
2024-08-02 02:38:00 +0600

    Update styling

--- packages/react/src/popover-content/PopoverContent.css.ts
+++ packages/react/src/popover-content/PopoverContent.css.ts
@@ -1,24 +1,4 @@
-import { keyframes, recipe, style } from "../vanilla-extract";
-
-const slideUpAndFade = keyframes({
-  "0%": { opacity: 0, transform: "translateY(2px)" },
-  "100%": { opacity: 1, transform: "translateY(0)" },
-});
-
-const slideRightAndFade = keyframes({
-  "0%": { opacity: 0, transform: "translateX(-2px)" },
-  "100%": { opacity: 1, transform: "translateX(0)" },
-});
-
-const slideDownAndFade = keyframes({
-  "0%": { opacity: 0, transform: "translateY(-2px)" },
-  "100%": { opacity: 1, transform: "translateY(0)" },
-});
-
-const slideLeftAndFade = keyframes({
-  "0%": { opacity: 0, transform: "translateX(2px)" },
-  "100%": { opacity: 1, transform: "translateX(0)" },
-});
+import { recipe, style } from "../vanilla-extract";
 
 export const content = recipe({
   base: [
@@ -31,28 +11,10 @@ export const content = recipe({
       p: "sm",
       rounded: "lg",
       shadow: "md",
+      z: "popover",
     },
     style({
       minWidth: "var(--radix-popover-trigger-width)",
-
-      animationDuration: "400ms",
-      animationTimingFunction: "cubic-bezier(0.16, 1, 0.3, 1)",
-      willChange: "transform, opacity",
-
-      selectors: {
-        '&[data-state="open"]&[data-side="bottom"]': {
-          animationName: slideUpAndFade,
-        },
-        '&[data-state="open"]&[data-side="left"]': {
-          animationName: slideRightAndFade,
-        },
-        '&[data-state="open"]&[data-side="right"]': {
-          animationName: slideLeftAndFade,
-        },
-        '&[data-state="open"]&[data-side="top"]': {
-          animationName: slideDownAndFade,
-        },
-      },
     }),
   ],
 });
