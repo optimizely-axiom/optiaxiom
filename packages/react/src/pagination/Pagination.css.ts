fe2a64b0eea6b3265b455a28f3277b20eca9b576
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-03 21:12:47 +0600

    refactor

--- packages/react/src/pagination/Pagination.css.ts
+++ packages/react/src/pagination/Pagination.css.ts
@@ -1,5 +1,23 @@
 import { recipe } from "../vanilla-extract";
 
 export const wrapper = recipe({
-  base: [{ flexDirection: "row" }],
+  base: [
+    {
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "row",
+      justifyContent: "center",
+    },
+  ],
+});
+
+export const paginationList = recipe({
+  base: [
+    {
+      alignItems: "center",
+      display: "flex",
+      flexDirection: "row",
+      gap: "xs",
+    },
+  ],
 });
