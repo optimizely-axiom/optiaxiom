7237fe3a3639d47be9880860156c0ec60a2dafd4
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-09 10:13:42 +0600

    add precedingSeparator prop

--- packages/react/src/breadcrumb-item/BreadcrumbItem.tsx
+++ packages/react/src/breadcrumb-item/BreadcrumbItem.tsx
@@ -5,25 +5,40 @@ import { Text } from "../text";
 import * as styles from "./BreadcrumbItem.css";
 
 export type BreadcrumbItemProps = {
+  followingSeparator?: boolean;
   href: string;
   isEllipsis?: boolean;
-  isLast?: boolean;
   label: string;
+  onClick?: () => void;
+  precedingSeparator?: boolean;
   separator?: React.ReactNode;
 };
 
 export const BreadcrumbItem: React.FC<BreadcrumbItemProps> = ({
+  followingSeparator = false,
   href,
-
-  isLast = false,
+  isEllipsis = false,
   label,
+  onClick,
+  precedingSeparator = true,
   separator = ">",
 }) => (
   <li {...styles.breadcrumbItem()}>
-    <Text as="span">
-      <Link href={href}>{label}</Link>
-    </Text>
-    {!isLast && (
+    {precedingSeparator && (
+      <Text as="span" {...styles.separator()}>
+        {separator}
+      </Text>
+    )}
+    {href === "#" && isEllipsis ? (
+      <Text as="span" onClick={onClick}>
+        {label}
+      </Text>
+    ) : (
+      <Text as="span">
+        <Link href={href}>{label}</Link>
+      </Text>
+    )}
+    {followingSeparator && (
       <Text as="span" {...styles.separator()}>
         {separator}
       </Text>
