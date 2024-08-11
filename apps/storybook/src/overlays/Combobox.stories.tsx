0c9d79901020d9f586d620d93d21b1750300d0f7
toha993 <mutasim.toha@optimizely.com>
2024-08-11 15:09:22 +0600

    Remove premium image source

--- apps/storybook/src/overlays/Combobox.stories.tsx
+++ apps/storybook/src/overlays/Combobox.stories.tsx
@@ -165,7 +165,6 @@ const users = [
     email: "david.nguyen@example.com",
     id: "DN",
     name: "David Nguyen",
-    src: "https://plus.unsplash.com/premium_photo-1671656349322-41de944d259b?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
 ];
 
