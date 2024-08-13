6bc3d37b4360c6eed871d9ff7a8fffabcd9f876c
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-13 16:21:38 +0600

    add styles to NavItems

--- packages/react/src/global-nav-list/GlobalNavList.tsx
+++ packages/react/src/global-nav-list/GlobalNavList.tsx
@@ -1,9 +1,23 @@
 import { Box, type BoxProps } from "../box";
-
+import { Flex } from "../flex";
+import { GlobalNavItem } from "../global-nav-item";
+import { IconAngleLeft } from "../icons/IconAngleLeft";
+import * as styles from "./GlobalNavList.css";
 export type GlobalNavListProps = BoxProps<"div">;
 
-export const GlobalNavList = ({ children }: GlobalNavListProps) => {
-  return <Box>{children}</Box>;
+export const GlobalNavList = ({
+  children,
+  className,
+  ...props
+}: GlobalNavListProps) => {
+  return (
+    <Flex {...styles.list({}, className)} {...props}>
+      <Box flex="1">{children}</Box>
+      <GlobalNavItem mt="auto" startDecorator={<IconAngleLeft />}>
+        Collapse
+      </GlobalNavItem>
+    </Flex>
+  );
 };
 
 export default GlobalNavList;
