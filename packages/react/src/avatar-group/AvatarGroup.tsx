47fd5df9ad6db775a4cc3eda569a2461ec2521d5
toha993 <mutasim.toha@optimizely.com>
2024-08-14 03:07:57 +0600

    Add hover card for overflowed children

--- packages/react/src/avatar-group/AvatarGroup.tsx
+++ packages/react/src/avatar-group/AvatarGroup.tsx
@@ -8,6 +8,9 @@ import {
 import { Avatar } from "../avatar";
 import { AvatarContext } from "../avatar-context/AvatarContext";
 import { Box, type BoxProps } from "../box";
+import { HoverCard } from "../hover-card";
+import { HoverCardContent } from "../hover-card-content";
+import { HoverCardTrigger } from "../hover-card-trigger";
 import * as styles from "./AvatarGroup.css";
 
 type AvatarGroupProps = BoxProps<
@@ -32,9 +35,20 @@ export const AvatarGroup = forwardRef<HTMLDivElement, AvatarGroupProps>(
         <Box ref={ref} {...styles.avatarGroup({}, className)} {...props}>
           {visibleChildren}
           {overflowChildren.length > 0 && (
-            <Avatar colorScheme="gray" size={size}>
-              +{overflowChildren.length}
-            </Avatar>
+            <HoverCard>
+              <HoverCardTrigger asChild>
+                <Avatar colorScheme="gray" size={size}>
+                  +{overflowChildren.length}
+                </Avatar>
+              </HoverCardTrigger>
+              <HoverCardContent flexDirection="row">
+                {overflowChildren.map((child, index) => (
+                  <Box asChild key={index}>
+                    {child}
+                  </Box>
+                ))}
+              </HoverCardContent>
+            </HoverCard>
           )}
         </Box>
       </AvatarContext.Provider>
