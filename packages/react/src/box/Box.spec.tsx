9a091be6b9362bffe3755fbf717e2d8c95fd03b8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-08-14 17:46:33 -0400

    add tests for sprinkle prop merging in slots

--- packages/react/src/box/Box.spec.tsx
+++ packages/react/src/box/Box.spec.tsx
@@ -8,4 +8,21 @@ describe("Box component", () => {
     render(<Box>This is a box</Box>);
     expect(screen.getByText(/This is a box/i)).toBeInTheDocument();
   });
+
+  it("should merge sprinkle props properly", () => {
+    render(
+      <Box asChild mx="4">
+        <Box asChild mx="2">
+          <Box mx="0">This is a box</Box>
+        </Box>
+      </Box>,
+    );
+    expect(screen.getByText(/This is a box/i)).not.toContainHTML(
+      "marginLeft_4",
+    );
+    expect(screen.getByText(/This is a box/i)).not.toContainHTML(
+      "marginLeft_2",
+    );
+    expect(screen.getByText(/This is a box/i)).toContainHTML("marginLeft_0");
+  });
 });
