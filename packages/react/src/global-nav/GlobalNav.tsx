6d5edd610b94d744eef24408dbffc94c02727567
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-14 22:32:43 +0600

    add GlobalNavContext

--- packages/react/src/global-nav/GlobalNav.tsx
+++ packages/react/src/global-nav/GlobalNav.tsx
@@ -1,10 +1,11 @@
-import { type ReactNode, forwardRef } from "react";
+import { forwardRef, useState } from "react";
+import { type ReactNode } from "react";
 
 import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
+import { GlobalNavContext } from "../global-nav-context";
 import { extractSprinkles } from "../sprinkles";
 import * as styles from "./GlobalNav.css";
-
 type GlobalNavProps = BoxProps<
   "nav",
   {
@@ -16,20 +17,25 @@ export const GlobalNav = forwardRef<HTMLElement, GlobalNavProps>(
   ({ children, className, endDecorator, ...props }, ref) => {
     const { restProps, sprinkleProps } = extractSprinkles(props);
 
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const toggleCollapsed = () => setIsCollapsed((prev) => !prev);
+
     return (
-      <Flex {...styles.wrapper()}>
-        <Box asChild {...styles.nav({}, className)} {...sprinkleProps}>
-          <nav
-            aria-label="Global Navigation"
-            ref={ref}
-            role="navigation"
-            {...restProps}
-          >
-            {children}
-          </nav>
-        </Box>
-        {endDecorator}
-      </Flex>
+      <GlobalNavContext.Provider value={{ isCollapsed, toggleCollapsed }}>
+        <Flex {...styles.wrapper()}>
+          <Box asChild {...styles.nav({}, className)} {...sprinkleProps}>
+            <nav
+              aria-label="Global Navigation"
+              ref={ref}
+              role="navigation"
+              {...restProps}
+            >
+              {children}
+            </nav>
+          </Box>
+          {endDecorator}
+        </Flex>
+      </GlobalNavContext.Provider>
     );
   },
 );
