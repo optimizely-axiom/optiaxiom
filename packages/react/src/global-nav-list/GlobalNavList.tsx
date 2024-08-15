cf449b51cafee6463a4a86f6ea551cd462a1766a
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-15 12:08:48 +0600

    add toggleCollapse functionality

--- packages/react/src/global-nav-list/GlobalNavList.tsx
+++ packages/react/src/global-nav-list/GlobalNavList.tsx
@@ -1,5 +1,6 @@
 import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
+import { useGlobalNav } from "../global-nav-context";
 import { GlobalNavItem } from "../global-nav-item";
 import { IconAngleLeft } from "../icons/IconAngleLeft";
 import * as styles from "./GlobalNavList.css";
@@ -10,10 +11,15 @@ export const GlobalNavList = ({
   className,
   ...props
 }: GlobalNavListProps) => {
+  const { toggleCollapsed } = useGlobalNav();
   return (
     <Flex {...styles.list({}, className)} {...props}>
       <Box flex="1">{children}</Box>
-      <GlobalNavItem mt="auto" startDecorator={<IconAngleLeft />}>
+      <GlobalNavItem
+        mt="auto"
+        onClick={toggleCollapsed}
+        startDecorator={<IconAngleLeft />}
+      >
         Collapse
       </GlobalNavItem>
     </Flex>
