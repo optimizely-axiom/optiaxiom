14baff14afe86e401cace62e07e835496734399c
toha993 <mutasim.toha@optimizely.com>
2024-08-15 17:17:47 +0600

    Add with items

--- packages/react/src/combobox-content/ComboboxContent.tsx
+++ packages/react/src/combobox-content/ComboboxContent.tsx
@@ -8,6 +8,7 @@ import {
 import { Box, type BoxProps } from "../box";
 import { ComboboxContext } from "../combobox-context";
 import { Command } from "../command";
+import { CommandCheckboxItem } from "../command-checkbox-item";
 import { CommandEmpty } from "../command-empty";
 import { CommandInput } from "../command-input";
 import { CommandItem } from "../command-item";
@@ -31,7 +32,35 @@ export const ComboboxContent = forwardRef<HTMLDivElement, ComboboxContentProps>(
     if (!context)
       throw new Error("ComboboxContent must be used within a Combobox");
 
-    const { items, onSelect, setOpen, setValue, value } = context;
+    const { items, mode, onSelect, setOpen, setValue, value } = context;
+
+    const handleSelect = (currentValue: string) => {
+      if (mode === "single") {
+        setValue(currentValue === value ? "" : currentValue);
+        setOpen(false);
+      } else if (mode === "multiple") {
+        const valueArray = value ? value.split(",") : [];
+        let newValue: string;
+        if (valueArray.includes(currentValue)) {
+          newValue = valueArray.filter((v) => v !== currentValue).join(",");
+        } else {
+          newValue = [...valueArray, currentValue].join(",");
+        }
+        setValue(newValue);
+      }
+      if (onSelect) {
+        onSelect(currentValue);
+      }
+    };
+
+    const isSelected = (itemValue: string) => {
+      if (mode === "single") {
+        return value === itemValue;
+      } else if (mode === "multiple") {
+        return value?.split(",").includes(itemValue);
+      }
+      return false;
+    };
 
     return (
       <PopoverContent asChild p="0" ref={ref} {...props}>
@@ -49,37 +78,39 @@ export const ComboboxContent = forwardRef<HTMLDivElement, ComboboxContentProps>(
               </Flex>
             </CommandEmpty>
             <CommandList maxH="sm" mt="4" overflow="auto">
-              {items?.map((item) => (
-                <CommandItem
-                  key={item.value}
-                  keywords={[item.value, item.label]}
-                  onSelect={(currentValue) => {
-                    setValue(currentValue === value ? "" : currentValue);
-                    setOpen(false);
-                    if (onSelect) {
-                      onSelect(currentValue);
-                    }
-                  }}
-                  tabIndex={0}
-                  value={item.value}
-                >
-                  <Box
-                    display="flex"
-                    flexDirection="row"
-                    justifyContent="space-between"
-                    w="full"
+              {items?.map((item) =>
+                mode === "multiple" ? (
+                  <CommandCheckboxItem
+                    checked={isSelected(item.value)}
+                    key={item.value}
+                    onCheckedChange={() => handleSelect(item.value)}
+                    value={item.value}
                   >
                     {item.label}
-                    {value == item.value ? (
-                      <Box color="black" mr="2">
-                        <IconCheck />
-                      </Box>
-                    ) : (
-                      <></>
-                    )}
-                  </Box>
-                </CommandItem>
-              ))}
+                  </CommandCheckboxItem>
+                ) : (
+                  <CommandItem
+                    key={item.value}
+                    onSelect={() => handleSelect(item.value)}
+                    value={item.value}
+                  >
+                    <Box
+                      alignItems="center"
+                      display="flex"
+                      flexDirection="row"
+                      justifyContent="space-between"
+                      w="full"
+                    >
+                      {item.label}
+                      {isSelected(item.value) && (
+                        <Box color="black" mr="2">
+                          <IconCheck />
+                        </Box>
+                      )}
+                    </Box>
+                  </CommandItem>
+                ),
+              )}
             </CommandList>
           </Command>
         )}
--- packages/react/src/combobox-context/ComboboxContext.ts
+++ packages/react/src/combobox-context/ComboboxContext.ts
@@ -1,12 +1,14 @@
 import { createContext } from "react";
 
 export type Item = {
+  disabled?: boolean;
   label: string;
   value: string;
 };
 
 export type ComboboxContextType = {
   items?: Item[];
+  mode?: "multiple" | "single";
   onSelect?: (value: string) => void;
   open?: boolean;
   setOpen: (open: boolean) => void;
