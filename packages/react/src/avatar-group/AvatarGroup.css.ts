64fee341738bbf5581af8689446f105ecb7bc287
toha993 <mutasim.toha@optimizely.com>
2024-08-15 23:18:47 +0600

    Remove pointer style

--- packages/react/src/avatar-group/AvatarGroup.css.ts
+++ packages/react/src/avatar-group/AvatarGroup.css.ts
@@ -1,17 +1,9 @@
-import { recipe, style } from "../vanilla-extract";
+import { recipe } from "../vanilla-extract";
 
 export const avatarGroup = recipe({
   base: [
     {
       display: "flex",
     },
-  ],
-});
-
-export const pointer = recipe({
-  base: [
-    style({
-      cursor: "pointer",
-    }),
   ],
 });
