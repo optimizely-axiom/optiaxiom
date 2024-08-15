3989f7b7c79a270af4b508184a86b5d6346f539a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-08-15 14:22:26 -0400

    add keepOpenOnActivation to HoverCard
    for mobile support

--- packages/react/src/field/Field.tsx
+++ packages/react/src/field/Field.tsx
@@ -70,7 +70,7 @@ export const Field = forwardRef<HTMLDivElement, FieldProps>(
             </Text>
 
             {info && (
-              <HoverCard>
+              <HoverCard keepOpenOnActivation>
                 <HoverCardTrigger asChild>
                   <Button
                     aria-label="Information hover"
