a814a12b001d8516caec58198b15b773d8217e65
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-08-22 22:05:04 +0600

    add chevron position & fix as per design

--- packages/react/src/accordion-item/AccordionItem.tsx
+++ packages/react/src/accordion-item/AccordionItem.tsx
@@ -1,16 +1,22 @@
 import * as RadixAccordion from "@radix-ui/react-accordion";
 import { forwardRef } from "react";
 
-import { Box, type BoxProps } from "../box";
+import { type BoxProps } from "../box";
+import { Flex } from "../flex";
+import { extractSprinkles } from "../sprinkles";
 
 type AccordionItemProps = BoxProps<typeof RadixAccordion.Item>;
 
 export const AccordionItem = forwardRef<HTMLDivElement, AccordionItemProps>(
   ({ children, value, ...props }, ref) => {
+    const { restProps, sprinkleProps } = extractSprinkles(props);
+
     return (
-      <Box asChild borderT="1" ref={ref} {...props}>
-        <RadixAccordion.Item value={value}>{children}</RadixAccordion.Item>
-      </Box>
+      <Flex asChild gap="4" ref={ref} {...sprinkleProps}>
+        <RadixAccordion.Item value={value} {...restProps}>
+          {children}
+        </RadixAccordion.Item>
+      </Flex>
     );
   },
 );
--- packages/react/src/accordion-trigger/AccordionTrigger.tsx
+++ packages/react/src/accordion-trigger/AccordionTrigger.tsx
@@ -4,12 +4,14 @@ import { type ReactNode, forwardRef } from "react";
 import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
 import { IconAngleDown } from "../icons/IconAngleDown";
+import { IconAngleRight } from "../icons/IconAngleRight";
 import { extractSprinkles } from "../sprinkles";
 import * as styles from "./AccordionTrigger.css";
 
 type AccordionTriggerProps = BoxProps<
   typeof RadixAccordion.Trigger,
   {
+    chevron?: "end" | "start";
     endDecorator?: ReactNode;
   }
 >;
@@ -17,27 +19,23 @@ type AccordionTriggerProps = BoxProps<
 export const AccordionTrigger = forwardRef<
   HTMLButtonElement,
   AccordionTriggerProps
->(({ children, endDecorator, ...props }, ref) => {
+>(({ chevron = "start", children, endDecorator, ...props }, ref) => {
   const { restProps, sprinkleProps } = extractSprinkles(props);
 
   return (
-    <Flex
-      asChild
-      color="fg.default"
-      flexDirection="row"
-      fontSize="lg"
-      fontWeight="600"
-      py="sm"
-      textAlign="start"
-      w="full"
-      {...sprinkleProps}
-    >
+    <Flex asChild {...styles.trigger()} {...sprinkleProps}>
       <RadixAccordion.Trigger ref={ref} {...restProps}>
+        {chevron === "start" && (
+          <Box asChild {...styles.icon({ chevron })}>
+            {endDecorator ?? <IconAngleRight />}
+          </Box>
+        )}
         <Box flex="1">{children}</Box>
-
-        <Box asChild {...styles.icon()}>
-          {endDecorator ?? <IconAngleDown />}
-        </Box>
+        {chevron === "end" && (
+          <Box asChild {...styles.icon({ chevron })}>
+            {endDecorator ?? <IconAngleDown />}
+          </Box>
+        )}
       </RadixAccordion.Trigger>
     </Flex>
   );
