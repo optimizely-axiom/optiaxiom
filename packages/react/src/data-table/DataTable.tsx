98cfd81857b980a3d0e44d43c8317092f6244126
toha993 <mutasim.toha@optimizely.com>
2024-08-25 18:36:06 +0600

    Add filtered icon and column pinning

--- packages/react/src/data-table/DataTable.tsx
+++ packages/react/src/data-table/DataTable.tsx
@@ -1,13 +1,18 @@
 import {
   type ColumnDef,
+  type SortingState,
   flexRender,
   getCoreRowModel,
   getPaginationRowModel,
+  getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+import { assignInlineVars } from "@vanilla-extract/dynamic";
 import React from "react";
 
 import { Box } from "../box";
+import { Button } from "../button";
+import { IconSort } from "../icons/IconSort";
 import { Pagination } from "../pagination";
 import { Table } from "../table";
 import { TableBody } from "../table-body";
@@ -15,6 +20,7 @@ import { TableCell } from "../table-cell";
 import { TableHead } from "../table-head";
 import { TableHeader } from "../table-header";
 import { TableRow } from "../table-row";
+import { Tooltip } from "../tooltip";
 import * as styles from "./DataTable.css";
 
 interface DataTableProps<TData, TValue> {
@@ -30,6 +36,8 @@ export const DataTable = <TData, TValue>({
   data,
   pinnedColumns = [],
 }: DataTableProps<TData, TValue>) => {
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+
   const [{ pageIndex, pageSize }, setPagination] = React.useState({
     pageIndex: 0,
     pageSize: 10,
@@ -45,78 +53,114 @@ export const DataTable = <TData, TValue>({
     data,
     getCoreRowModel: getCoreRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
     onPaginationChange: setPagination,
+    onSortingChange: setSorting,
     state: {
       columnPinning: { left: pinnedColumns },
       pagination,
+      sorting,
     },
   });
 
   const totalPages = table.getPageCount();
 
+  let offset = 0;
+  const offsets = Object.fromEntries(
+    table
+      .getAllColumns()
+      .flatMap((column) => [
+        [column.id, (offset += column.getSize()) - column.getSize()],
+      ]),
+  );
+
   return (
     <Box alignItems="center" display="flex" flexDirection="column">
-      <Box style={{ overflowX: "auto", position: "relative", width: "100%" }}>
-        <Table>
-          <TableHeader>
-            {table.getHeaderGroups().map((headerGroup) => (
-              <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => {
-                  const isPinned = pinnedColumns.includes(header.column.id);
-                  return (
-                    <TableHead
-                      {...styles.tableHead({
-                        pinned: isPinned ? "left" : undefined,
-                      })}
-                      colSpan={header.colSpan}
-                      key={header.id}
-                      style={{
-                        left: isPinned
-                          ? `${header.getStart("left")}px`
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead
+                    key={header.id}
+                    {...styles.tableHead({
+                      pinned: header.column.getIsPinned() ? true : undefined,
+                    })}
+                    style={{
+                      ...assignInlineVars({
+                        [styles.cellOffset]: header.column.getIsPinned()
+                          ? `${offsets[header.column.id]}px`
                           : undefined,
-                        width: `${header.getSize()}px`,
-                      }}
-                    >
-                      {flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                    </TableHead>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {table.getRowModel().rows.map((row) => (
-              <TableRow key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  const isPinned = pinnedColumns.includes(cell.column.id);
-                  return (
-                    <TableCell
-                      {...styles.tableCell({
-                        pinned: isPinned ? "left" : undefined,
-                      })}
-                      key={cell.id}
-                      style={{
-                        left: isPinned
-                          ? `${cell.column.getStart("left")}px`
+                        [styles.columnWidth]: `${header.getSize()}px`,
+                      }),
+                    }}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext(),
+                    )}
+                    {header.column.columnDef.enableSorting && (
+                      <Tooltip
+                        content={
+                          header.column.getCanSort()
+                            ? header.column.getNextSortingOrder() === "asc"
+                              ? "Sort ascending"
+                              : header.column.getNextSortingOrder() === "desc"
+                                ? "Sort descending"
+                                : "Clear sort"
+                            : undefined
+                        }
+                      >
+                        <Button
+                          border="0"
+                          icon={
+                            <IconSort
+                              sortDirection={
+                                header.column.getIsSorted() as
+                                  | "asc"
+                                  | "desc"
+                                  | false
+                              }
+                            />
+                          }
+                          onClick={() => header.column.toggleSorting()}
+                        />
+                      </Tooltip>
+                    )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows.map((row) => (
+            <TableRow key={row.id}>
+              {row.getVisibleCells().map((cell) => {
+                return (
+                  <TableCell
+                    {...styles.tableCell({
+                      pinned: cell.column.getIsPinned() ? true : undefined,
+                    })}
+                    key={cell.id}
+                    style={{
+                      ...assignInlineVars({
+                        [styles.cellOffset]: cell.column.getIsPinned()
+                          ? `${offsets[cell.column.id]}px`
                           : undefined,
-                        width: `${cell.column.getSize()}px`,
-                      }}
-                    >
-                      {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext(),
-                      )}
-                    </TableCell>
-                  );
-                })}
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Box>
+                        [styles.columnWidth]: `${cell.column.getSize()}px`,
+                      }),
+                    }}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
 
       <Box mt="8">
         <Pagination
