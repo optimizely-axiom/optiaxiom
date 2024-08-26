d68a1f687547a0c554b45860840e277c25d613d0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-08-26 11:49:15 -0400

    inline props and changelog in component docs page
    - simplifies navigation so footer links navigate between components
    - simplifies sidebar and reduces clutter
    - reduces confusion between docs page and sub-items like props
    - makes all docs/api related to a component visible in one page

--- apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
+++ apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
@@ -1,7 +1,6 @@
 import { transformCode } from "./transformCode.mjs";
 import { transformColorTokens } from "./transformColorTokens.mjs";
 import { transformDemos } from "./transformDemos.mjs";
-import { transformLayout } from "./transformLayout.mjs";
 import { transformPropsTable } from "./transformPropsTable.mjs";
 
 export function remarkPlugin() {
@@ -9,7 +8,6 @@ export function remarkPlugin() {
     transformCode(tree, file);
     transformColorTokens(tree, file);
     transformDemos(tree, file);
-    transformLayout(tree, file);
     transformPropsTable(tree, file);
   };
 }
