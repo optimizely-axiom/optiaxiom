810d9869ddca7e2defccef424bdb32439c0ed469
toha993 <mutasim.toha@optimizely.com>
2024-08-28 00:47:50 +0600

    Update size for docs

--- apps/docs/demos/avatar/usage/App.tsx
+++ apps/docs/demos/avatar/usage/App.tsx
@@ -5,14 +5,14 @@ export function App() {
     <Flex flexDirection="row" gap="10">
       <Avatar
         name="John Snow"
-        size="xl"
+        size="5xl"
         src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
       >
         JS
       </Avatar>
       <Avatar
         name="Daenerys Targaryen"
-        size="lg"
+        size="xl"
         src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
       >
         DT
