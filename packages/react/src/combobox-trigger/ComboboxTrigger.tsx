cdc184fba28a868d688800edc5fe0c8b8b7d3169
toha993 <mutasim.toha@optimizely.com>
2024-08-28 18:07:35 +0600

    Trigger

--- packages/react/src/combobox-trigger/ComboboxTrigger.tsx
+++ packages/react/src/combobox-trigger/ComboboxTrigger.tsx
@@ -1,26 +1,45 @@
 import { forwardRef } from "react";
 
-import { Button, type ButtonProps } from "../button";
-import { IconAngleDown } from "../icons/IconAngleDown";
+import type { ButtonProps } from "../button";
+
+import { useComboboxContext } from "../combobox-context";
+import { ComboboxMultiTrigger } from "../combobox-multi-trigger";
+import { ComboboxSingleTrigger } from "../combobox-single-trigger";
 import { PopoverTrigger } from "../popover-trigger";
 
-type ComboboxTriggerProps = ButtonProps<typeof PopoverTrigger>;
+type ComboboxTriggerProps = ButtonProps<
+  typeof PopoverTrigger,
+  {
+    maxDisplayedItems?: number;
+    title?: string;
+  }
+>;
 
 export const ComboboxTrigger = forwardRef<
   HTMLButtonElement,
   ComboboxTriggerProps
->(({ asChild, children, ...props }, ref) => {
+>(({ asChild, children, maxDisplayedItems = 2, title, ...props }, ref) => {
+  const { mode } = useComboboxContext("Combobox");
+
   return (
     <PopoverTrigger asChild ref={ref}>
       {asChild ? (
         children
+      ) : mode === "single" ? (
+        <ComboboxSingleTrigger title={title} {...props} />
+      ) : mode === "multiple" ? (
+        <ComboboxMultiTrigger
+          maxDisplayedItems={maxDisplayedItems}
+          title={title}
+          {...props}
+        />
       ) : (
-        <Button icon={<IconAngleDown />} iconPosition="end" {...props}>
-          {children}
-        </Button>
+        children
       )}
     </PopoverTrigger>
   );
 });
 
 ComboboxTrigger.displayName = "@optiaxiom/react/ComboboxTrigger";
+
+export default ComboboxTrigger;
