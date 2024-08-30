1818bb8dc81fe60b47583bae30410073ac97d28a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-08-30 14:33:50 -0400

    add root axiom provider as global wrapper to all tests

--- packages/react/vitest.rtl.ts
+++ packages/react/vitest.rtl.ts
@@ -1,9 +1,19 @@
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { createElement } from "react";
 import { vi } from "vitest";
 
+import { AxiomProvider } from "./src";
+
+const GlobalTestWrapper = ({ children }: { children?: React.ReactNode }) => {
+  return createElement(AxiomProvider, undefined, children);
+};
+
 const customRender = (...args: Parameters<typeof render>) => ({
-  ...render(...args),
+  ...render(args[0], {
+    wrapper: GlobalTestWrapper,
+    ...args[1],
+  }),
   user: userEvent.setup({
     advanceTimers: vi.advanceTimersByTime,
   }),
