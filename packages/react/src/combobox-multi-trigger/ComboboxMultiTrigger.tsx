54f2a1c3d6969b0588203ff9bdd35753f07557ea
toha993 <mutasim.toha@optimizely.com>
2024-08-30 19:34:08 +0600

    Add pill in combobox and remove redundant combobox trigger

--- packages/react/src/combobox-multi-trigger/ComboboxMultiTrigger.tsx
+++ packages/react/src/combobox-multi-trigger/ComboboxMultiTrigger.tsx
@@ -1,17 +1,21 @@
 import { forwardRef } from "react";
 
-import { Badge } from "../badge";
+import type { PopoverTrigger } from "../popover-trigger";
+
 import { Box } from "../box";
 import { Button, type ButtonProps } from "../button";
 import { useComboboxContext } from "../combobox-context";
-import { ComboboxTrigger } from "../combobox-trigger";
 import { Flex } from "../flex";
+import { Pill } from "../pill";
 import { Text } from "../text";
 
-type ComboboxMultiTriggerProps = {
-  maxDisplayedItems?: number;
-  title?: string;
-} & ButtonProps<typeof ComboboxTrigger>;
+type ComboboxMultiTriggerProps = ButtonProps<
+  typeof PopoverTrigger,
+  {
+    maxDisplayedItems?: number;
+    title?: string;
+  }
+>;
 
 export const ComboboxMultiTrigger = forwardRef<
   HTMLButtonElement,
@@ -20,34 +24,32 @@ export const ComboboxMultiTrigger = forwardRef<
   const { mode, value } = useComboboxContext("Combobox");
 
   return (
-    <ComboboxTrigger asChild>
-      <Button alignItems="center" ref={ref} w="full" {...props}>
-        <Box overflow="hidden">
-          {mode === "multiple" && Array.isArray(value) && value.length > 0 ? (
-            <Flex
-              alignItems="center"
-              flexDirection="row"
-              flexWrap="wrap"
-              gap="2"
-              overflow="hidden"
-            >
-              {value.slice(0, maxDisplayedItems).map((item) => (
-                <Box flex="none" key={item}>
-                  <Badge>{item}</Badge>
-                </Box>
-              ))}
-              {value.length > maxDisplayedItems && (
-                <Box flex="none">
-                  <Text>+{value.length - maxDisplayedItems} more</Text>
-                </Box>
-              )}
-            </Flex>
-          ) : (
-            <Text>{title}</Text>
-          )}
-        </Box>
-      </Button>
-    </ComboboxTrigger>
+    <Button alignItems="center" ref={ref} w="full" {...props}>
+      <Box overflow="hidden">
+        {mode === "multiple" && Array.isArray(value) && value.length > 0 ? (
+          <Flex
+            alignItems="center"
+            flexDirection="row"
+            flexWrap="wrap"
+            gap="2"
+            overflow="hidden"
+          >
+            {value.slice(0, maxDisplayedItems).map((item) => (
+              <Box flex="none" key={item}>
+                <Pill asChild>{item}</Pill>
+              </Box>
+            ))}
+            {value.length > maxDisplayedItems && (
+              <Box flex="none">
+                <Text>+{value.length - maxDisplayedItems} more</Text>
+              </Box>
+            )}
+          </Flex>
+        ) : (
+          <Text>{title}</Text>
+        )}
+      </Box>
+    </Button>
   );
 });
 
--- packages/react/src/combobox-single-trigger/ComboboxSingleTrigger.tsx
+++ packages/react/src/combobox-single-trigger/ComboboxSingleTrigger.tsx
@@ -1,11 +1,12 @@
 import { forwardRef } from "react";
 
+import type { PopoverTrigger } from "../popover-trigger";
+
 import { Button, type ButtonProps } from "../button";
 import { useComboboxContext } from "../combobox-context";
-import { ComboboxTrigger } from "../combobox-trigger";
 import { IconAngleDown } from "../icons/IconAngleDown";
 
-type ComboboxSingleTriggerProps = ButtonProps<typeof ComboboxTrigger>;
+type ComboboxSingleTriggerProps = ButtonProps<typeof PopoverTrigger>;
 
 export const ComboboxSingleTrigger = forwardRef<
   HTMLButtonElement,
@@ -13,12 +14,9 @@ export const ComboboxSingleTrigger = forwardRef<
 >(({ title, ...props }, ref) => {
   const { value } = useComboboxContext("Combobox");
   return (
-    <ComboboxTrigger asChild ref={ref} {...props}>
-      <Button>
-        {value || title}
-        <IconAngleDown />
-      </Button>
-    </ComboboxTrigger>
+    <Button endDecorator={<IconAngleDown />} ref={ref} {...props}>
+      {value || title}
+    </Button>
   );
 });
 
