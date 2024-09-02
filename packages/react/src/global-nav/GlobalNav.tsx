56991088163a0ee46fbb6830520794255458e38d
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-02 11:54:51 +0600

    add controlled state for GlobalNav

--- packages/react/src/global-nav/GlobalNav.tsx
+++ packages/react/src/global-nav/GlobalNav.tsx
@@ -1,6 +1,9 @@
 import * as RadixCollapsible from "@radix-ui/react-collapsible";
-import { forwardRef, useState } from "react";
+import { useControllableState } from "@radix-ui/react-use-controllable-state";
+import { type ComponentPropsWithRef, forwardRef } from "react";
 import { type ReactNode } from "react";
+
+import type { ExtendProps } from "../utils";
 
 import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
@@ -9,21 +12,45 @@ import { GlobalNavItem } from "../global-nav-item";
 import { IconCollapse } from "../icons/IconCollapse";
 import { extractSprinkles } from "../sprinkles";
 
-type GlobalNavProps = BoxProps<
-  "nav",
-  {
-    endDecorator?: ReactNode;
-  }
+type GlobalNavProps = ExtendProps<
+  Omit<
+    ComponentPropsWithRef<typeof RadixCollapsible.Root>,
+    "defaultOpen" | "onOpenChange" | "open"
+  >,
+  BoxProps<
+    "nav",
+    {
+      defaultExpanded?: boolean;
+      endDecorator?: ReactNode;
+      expanded?: boolean;
+      onExpandedChange?: (expanded: boolean) => void;
+    }
+  >
 >;
 
 export const GlobalNav = forwardRef<HTMLElement, GlobalNavProps>(
-  ({ children, endDecorator, ...props }, ref) => {
+  (
+    {
+      children,
+      defaultExpanded,
+      endDecorator,
+      expanded: expandedProp,
+      onExpandedChange,
+      ...props
+    },
+    ref,
+  ) => {
     const { restProps, sprinkleProps } = extractSprinkles(props);
-    const [open, setOpen] = useState(false);
+
+    const [expanded, setExpanded] = useControllableState({
+      defaultProp: defaultExpanded,
+      onChange: onExpandedChange,
+      prop: expandedProp,
+    });
 
     return (
-      <GlobalNavContext.Provider value={{ open }}>
-        <RadixCollapsible.Root onOpenChange={() => setOpen(!open)} open={open}>
+      <GlobalNavContext.Provider value={{ expanded }}>
+        <RadixCollapsible.Root onOpenChange={setExpanded} open={expanded}>
           <Flex border="1" pb="md" pt="lg">
             <Box asChild display="flex" {...sprinkleProps}>
               <nav
