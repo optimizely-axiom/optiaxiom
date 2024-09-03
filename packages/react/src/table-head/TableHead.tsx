557fb295ca2b9b0bd1b828ce015812937c8d39c8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-03 16:55:17 -0400

    remove manual colSpan prop now that sprinkles does not conflict

--- packages/react/src/table-head/TableHead.tsx
+++ packages/react/src/table-head/TableHead.tsx
@@ -3,20 +3,16 @@ import { forwardRef } from "react";
 import { Box, type BoxProps } from "../box";
 import * as styles from "./TableHead.css";
 
-type TableHeadProps = BoxProps<
-  "th",
-  {
-    colSpan?: number;
-  }
->;
+type TableHeadProps = BoxProps<"th">;
 
 export const TableHead = forwardRef<HTMLTableCellElement, TableHeadProps>(
   ({ children, className, colSpan, ...props }, ref) => (
-    <Box {...styles.head({}, className)} asChild {...props}>
+    <Box asChild {...styles.head({}, className)} {...props}>
       <th colSpan={colSpan} ref={ref}>
         {children}
       </th>
     </Box>
   ),
 );
+
 TableHead.displayName = "@optiaxiom/react/TableHead";
