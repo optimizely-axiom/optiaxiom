724f2e27746b231739e1aecdbb01cc2bfabbd0c5
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-04 21:42:49 +0600

    wrap breadcrumb-item with li tag

--- packages/react/src/breadcrumb-item/BreadcrumbItem.tsx
+++ packages/react/src/breadcrumb-item/BreadcrumbItem.tsx
@@ -6,8 +6,10 @@ export type BreadcrumbItemProps = ComponentPropsWithoutRef<typeof Link>;
 
 export const BreadcrumbItem = ({ children, ...props }: BreadcrumbItemProps) => {
   return (
-    <Link alignItems="center" display="flex" {...props}>
-      {children}
-    </Link>
+    <li>
+      <Link alignItems="center" display="flex" {...props}>
+        {children}
+      </Link>
+    </li>
   );
 };
