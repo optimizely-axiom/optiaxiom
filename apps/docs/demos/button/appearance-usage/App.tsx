264584e4065e88a49da07797dcce18058a4d21ba
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-04 15:48:00 +0600

    remove props from docs page

--- apps/docs/demos/button/appearance-usage/App.tsx
+++ apps/docs/demos/button/appearance-usage/App.tsx
@@ -4,15 +4,6 @@ import { Button } from "@optiaxiom/react";
 
 export function App({
   appearance = "default",
-  colorScheme,
-  variant,
-}: Pick<
-  ComponentPropsWithRef<typeof Button>,
-  "appearance" | "colorScheme" | "variant"
->) {
-  return (
-    <Button appearance={appearance} colorScheme={colorScheme} variant={variant}>
-      Delete
-    </Button>
-  );
+}: Pick<ComponentPropsWithRef<typeof Button>, "appearance">) {
+  return <Button appearance={appearance}>Delete</Button>;
 }
