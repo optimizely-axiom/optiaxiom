66f29046bd148909e4b4568cfb2c3fc9d436353d
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-05 10:29:18 +0600

    remove useBreadcrumb hook

--- packages/react/src/breadcrumbs/Breadcrumbs.tsx
+++ packages/react/src/breadcrumbs/Breadcrumbs.tsx
@@ -1,27 +1,28 @@
-import { type ReactNode, forwardRef } from "react";
+import { forwardRef } from "react";
 
 import { Box, type BoxProps } from "../box";
 import { extractSprinkles } from "../sprinkles";
-import { useBreadcrumbItems } from "./useBreadcrumbItems";
 
-export type BreadcrumbsProps = BoxProps<
-  "nav",
-  {
-    maxItems?: number;
-    separator?: ReactNode;
-  }
->;
+export type BreadcrumbsProps = BoxProps<"nav">;
+/* 
+  Breadcrumb,
+  BreadcrumbEllipsis,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+*/
 
 export const Breadcrumbs = forwardRef<HTMLElement, BreadcrumbsProps>(
-  ({ children, maxItems, separator = "/", ...props }, ref) => {
+  ({ children, ...props }, ref) => {
     const { restProps, sprinkleProps } = extractSprinkles(props);
-    const visibleItems = useBreadcrumbItems(children, maxItems, separator);
+    // const visibleItems = useBreadcrumbItems(children, maxItems, separator);
 
     return (
       <Box asChild {...sprinkleProps}>
         <nav aria-label="Breadcrumb" ref={ref} {...restProps}>
           <Box alignItems="center" asChild display="flex" gap="xs">
-            <ol>{visibleItems}</ol>
+            <ol>{children}</ol>
           </Box>
         </nav>
       </Box>
