37638c617ff4a6b47caf1d331d47d35b9ee001e0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-05 14:17:45 -0400

    add button loading docs

---
+++ apps/docs/demos/button/loading-usage/App.tsx
@@ -0,0 +1,35 @@
+import { Button, Flex, Switch, Tooltip } from "@optiaxiom/react";
+import { useState } from "react";
+
+export function App() {
+  const [loading, setLoading] = useState(true);
+
+  return (
+    <Flex>
+      <Flex flexDirection="row">
+        <Tooltip content="Loading button demo">
+          <Button appearance="primary" loading={loading}>
+            Primary
+          </Button>
+        </Tooltip>
+
+        <Tooltip content="Loading button demo">
+          <Button loading={loading}>Default</Button>
+        </Tooltip>
+
+        <Tooltip content="Loading button demo">
+          <Button appearance="secondary" loading={loading}>
+            Secondary
+          </Button>
+        </Tooltip>
+      </Flex>
+
+      <Switch
+        checked={loading}
+        onCheckedChange={() => setLoading((loading) => !loading)}
+      >
+        Loading
+      </Switch>
+    </Flex>
+  );
+}
