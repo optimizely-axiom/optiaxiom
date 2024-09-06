3ef0ecb9e6883df930dcb1a3c6eed340f228b565
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-05 21:43:22 -0400

    embed code snippet
    and remove single use remark plugin for transforming code

--- apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
+++ apps/docs/plugins/remark-axiom-plugin/remarkPlugin.mjs
@@ -1,11 +1,9 @@
-import { transformCode } from "./transformCode.mjs";
 import { transformColorTokens } from "./transformColorTokens.mjs";
 import { transformDemos } from "./transformDemos.mjs";
 import { transformPropsTable } from "./transformPropsTable.mjs";
 
 export function remarkPlugin() {
   return (tree, file) => {
-    transformCode(tree, file);
     transformColorTokens(tree, file);
     transformDemos(tree, file);
     transformPropsTable(tree, file);
