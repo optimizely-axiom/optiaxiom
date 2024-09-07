2a4c128acdcb982768e4154ba51c131f5ece031d
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-07 22:39:41 +0600

    renaming to breadcrumb

--- packages/react/src/unstable.ts
+++ packages/react/src/unstable.ts
@@ -1,5 +1,5 @@
+export * from "./breadcrumb";
 export * from "./breadcrumb-item";
-export * from "./breadcrumbs";
 export * from "./combobox";
 export * from "./combobox-content";
 export * from "./combobox-multi-trigger";
