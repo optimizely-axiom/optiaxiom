da2e0c35fc4c13a4fee44886f0c125a1b6ffe0a8
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-11 17:18:23 -0400

    prefix font names with dynamic id
    to prevent collision with existing fonts

--- packages/web-components/src/styles.ts
+++ packages/web-components/src/styles.ts
@@ -1,13 +1,20 @@
 export const sheets: CSSStyleSheet[] = [];
 
+const uuid = "ax" + crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
+
 export function injectGlobalStyle(text: string) {
   const style = document.createElement("style");
   style.appendChild(
     document.createTextNode(
-      text.replaceAll(
-        /url\(["']?([^"')]+)["']?\)/g,
-        (_, m1) => `url("${import.meta.resolve("./" + m1)}")`,
-      ),
+      text
+        .replaceAll(
+          /font-family:\s?(Fira Code Variable|InterVariable+);/g,
+          (_, m1) => `font-family: ${uuid} ${m1};`,
+        )
+        .replaceAll(
+          /url\(["']?([^"')]+)["']?\)/g,
+          (_, m1) => `url("${import.meta.resolve("./" + m1)}")`,
+        ),
     ),
   );
   document.head.append(style);
@@ -15,6 +22,8 @@ export function injectGlobalStyle(text: string) {
 
 export function injectLocalStyle(text: string) {
   const sheet = new CSSStyleSheet();
-  void sheet.replace(text);
+  void sheet.replace(
+    text.replaceAll(/Fira Code Variable|InterVariable/g, (m) => `${uuid} ${m}`),
+  );
   sheets.push(sheet);
 }
