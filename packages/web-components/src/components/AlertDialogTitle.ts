5005a8a7bf38aae33e57c28862c24335a3f1a88e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-11 17:24:09 -0400

    fix AlertDialogTitle web-component export

--- packages/web-components/src/components/AlertDialogTitle.ts
+++ packages/web-components/src/components/AlertDialogTitle.ts
@@ -1,17 +1,17 @@
-import { AlertDialog as AlertDialogComponent } from "@optiaxiom/react";
+import { AlertDialogTitle as AlertDialogTitleComponent } from "@optiaxiom/react";
 
 import type { ComponentAttributes } from "./ComponentAttributes";
 
 import { register } from "../register";
 
-export const AlertDialog = "ax-alert-dialog";
-export default register(AlertDialog, AlertDialogComponent);
+export const AlertDialogTitle = "ax-alert-dialog-title";
+export default register(AlertDialogTitle, AlertDialogTitleComponent);
 
 declare global {
   // eslint-disable-next-line @typescript-eslint/no-namespace
   namespace JSX {
     interface IntrinsicElements {
-      [AlertDialog]: ComponentAttributes<typeof AlertDialogComponent>;
+      [AlertDialogTitle]: ComponentAttributes<typeof AlertDialogTitleComponent>;
     }
   }
 }
