838757bbc691d5512d501b85b993e3b01c66125d
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-12 17:09:56 -0400

    create re-usable MenuContentBase component

--- packages/react/src/dropdown-menu-sub-content/DropdownMenuSubContent.tsx
+++ packages/react/src/dropdown-menu-sub-content/DropdownMenuSubContent.tsx
@@ -1,34 +1,25 @@
 import * as RadixMenu from "@radix-ui/react-dropdown-menu";
 import { forwardRef } from "react";
 
-import { AnimatePresence } from "../animate-presence";
-import { Box, type BoxProps } from "../box";
+import type { BoxProps } from "../box";
+
 import { useDropdownMenuSubContext } from "../dropdown-menu-sub-context";
-import { Transition } from "../transition";
-import * as styles from "./DropdownMenuSubContent.css";
+import { MenuContentBase } from "../menu-content-base";
 
 type MenuSubContentProps = BoxProps<typeof RadixMenu.SubContent>;
 
 export const DropdownMenuSubContent = forwardRef<
   HTMLDivElement,
   MenuSubContentProps
->(({ children, className, sideOffset = 2, ...props }, ref) => {
+>(({ children, sideOffset = 2, ...props }, ref) => {
   const { open } = useDropdownMenuSubContext("DropdownMenuSubContent");
 
   return (
-    <AnimatePresence>
-      {open && (
-        <RadixMenu.Portal forceMount>
-          <Transition duration="sm" type="pop">
-            <Box asChild {...styles.content({}, className)} {...props}>
-              <RadixMenu.SubContent ref={ref} sideOffset={sideOffset}>
-                {children}
-              </RadixMenu.SubContent>
-            </Box>
-          </Transition>
-        </RadixMenu.Portal>
-      )}
-    </AnimatePresence>
+    <MenuContentBase open={open} {...props}>
+      <RadixMenu.SubContent ref={ref} sideOffset={sideOffset}>
+        {children}
+      </RadixMenu.SubContent>
+    </MenuContentBase>
   );
 });
 
