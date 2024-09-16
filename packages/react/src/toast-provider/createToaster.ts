61f017e32c47e40f5553fbcabc81cb8eb99fae58
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-16 11:25:35 -0400

    replace jotai with simpler useSyncExternalStore

--- packages/react/src/toast-provider/createToaster.ts
+++ packages/react/src/toast-provider/createToaster.ts
@@ -1,55 +1,60 @@
 import type { ComponentPropsWithoutRef, ReactElement } from "react";
 
-import { atom, getDefaultStore } from "jotai";
-
 import type { Toast } from "../toast/Toast";
 
 type ToastElement = ReactElement<ComponentPropsWithoutRef<typeof Toast>>;
+type ToastItem = {
+  id: string;
+  open: boolean;
+  toast: ToastElement;
+};
+
+const EMPTY: ToastItem[] = [];
 
 let id = 1;
 const genId = () => {
   return "t" + id++;
 };
 
-type Store = ReturnType<typeof getDefaultStore>;
+export const createToaster = () => {
+  let snapshot: ToastItem[] = [];
 
-export const createToaster = (store: Store = getDefaultStore()) => {
-  const toastsAtom = atom<
-    Array<{
-      id: string;
-      open: boolean;
-      toast: ToastElement;
-    }>
-  >([]);
+  const listeners = new Set<() => void>();
+  const emit = () => {
+    for (const listener of listeners) {
+      listener();
+    }
+  };
 
   return {
-    atom: toastsAtom,
+    store: {
+      getServerSnapshot: () => EMPTY,
+      getSnapshot: () => snapshot,
+      subscribe: (callback: () => void) => {
+        listeners.add(callback);
+        return () => listeners.delete(callback);
+      },
+    },
 
     create: (toast: ToastElement) => {
       const id = genId();
-      store.set(toastsAtom, [
-        ...store.get(toastsAtom),
-        { id, open: true, toast },
-      ]);
+      snapshot = [...snapshot, { id, open: true, toast }];
+
+      emit();
 
       return id;
     },
 
     remove: (id: string) => {
-      store.set(
-        toastsAtom,
-        store
-          .get(toastsAtom)
-          .map((item) => (item.id === id ? { ...item, open: false } : item)),
+      snapshot = snapshot.map((item) =>
+        item.id === id ? { ...item, open: false } : item,
       );
-      setTimeout(
-        () =>
-          store.set(
-            toastsAtom,
-            store.get(toastsAtom).filter((item) => item.id !== id),
-          ),
-        200,
-      );
+      emit();
+
+      setTimeout(() => {
+        snapshot = snapshot.filter((item) => item.id !== id);
+        emit();
+      }, 200);
     },
   };
 };
