d303c377352bf04bd8b2f8d5006009cbff53efff
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-18 16:48:02 -0400

    inline MapLeafNodes since VE does not export it
    so we can see actual values of the tokens instead of useless `var(--${string})`
    types

--- packages/globals/src/theme.css.ts
+++ packages/globals/src/theme.css.ts
@@ -21,7 +21,32 @@ const createThemeContractFromTokens = <
   ) as T;
 };
 
-export const theme = createGlobalThemeContract(
+const createGlobalThemeContractInlineTypes = <T extends NullableTokens>(
+  tokens: T,
+  mapFn: (value: null | string) => string,
+) =>
+  createGlobalThemeContract(tokens, mapFn) as unknown as MapLeafNodes<
+    T,
+    CSSVarFunction
+  >;
+
+export const theme = createGlobalThemeContractInlineTypes(
   createThemeContractFromTokens(tokens, ""),
   (value) => `ax-${value}`,
 );
+
+type CSSVarFunction =
+  | `var(--${string})`
+  | `var(--${string}, ${number | string})`;
+type Primitive = boolean | null | number | string | undefined;
+export type MapLeafNodes<Obj, LeafType> = {
+  [Prop in keyof Obj]: Obj[Prop] extends Primitive
+    ? LeafType
+    : // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      Obj[Prop] extends Record<number | string, any>
+      ? MapLeafNodes<Obj[Prop], LeafType>
+      : never;
+};
+type NullableTokens = {
+  [key: string]: NullableTokens | null | string;
+};
