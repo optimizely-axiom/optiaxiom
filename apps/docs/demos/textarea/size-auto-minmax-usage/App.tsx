0e84d9de41f00c3691702242958a3a810bd3504c
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-19 15:36:52 -0400

    keep maxRows as number type

--- apps/docs/demos/textarea/size-auto-minmax-usage/App.tsx
+++ apps/docs/demos/textarea/size-auto-minmax-usage/App.tsx
@@ -2,6 +2,6 @@ import { Textarea } from "@optiaxiom/react";
 
 export function App() {
   return (
-    <Textarea maxRows="3" placeholder="Enter text..." resize="auto" rows={1} />
+    <Textarea maxRows={3} placeholder="Enter text..." resize="auto" rows={1} />
   );
 }
