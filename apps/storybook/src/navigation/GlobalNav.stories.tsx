3ae9cfa44801651b798035b5f068a325bc44f8e5
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-09-19 19:08:17 +0600

    fix avatar account image

--- apps/storybook/src/navigation/GlobalNav.stories.tsx
+++ apps/storybook/src/navigation/GlobalNav.stories.tsx
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Link } from "@optiaxiom/react";
+import { Avatar, Link } from "@optiaxiom/react";
 import {
   GlobalNav,
   GlobalNavItem,
@@ -28,10 +28,7 @@ const Template: Story["render"] = (args) => (
     addonAfter={
       <GlobalNavProfileMenu
         avatar={
-          <img
-            alt="Sample"
-            src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
-          />
+          <Avatar src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
         }
         name="Rhaenyra Targaryen"
         organization="Optimizely"
