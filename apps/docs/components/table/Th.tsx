d86a2098c5c8b3a4dfd8af9fb8e66f6808af14f4
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-20 12:06:29 -0400

    remove hack to apply rounded corners to table headers
    and replace with a simple overflow=auto rule
    
    otherwise styles get messed up in safari since position=relative has no effect
    on a thead

--- apps/docs/components/table/Th.tsx
+++ apps/docs/components/table/Th.tsx
@@ -2,14 +2,12 @@ import type { ComponentPropsWithRef } from "react";
 
 import { Box, Text } from "@optiaxiom/react";
 
-import styles from "./Th.module.css";
-
 export const Th = ({
   asChild,
   children,
   ...props
 }: ComponentPropsWithRef<typeof Box>) => (
-  <Box asChild bg="bg.neutral" className={styles.th} {...props}>
+  <Box asChild bg="bg.neutral" {...props}>
     <th>
       <Text asChild={asChild} fontWeight="600" p="12">
         {children ?? <>&nbsp;</>}
--- apps/docs/components/table/Thead.tsx
+++ apps/docs/components/table/Thead.tsx
@@ -2,13 +2,11 @@ import type { ComponentPropsWithRef } from "react";
 
 import { Box } from "@optiaxiom/react";
 
-import styles from "./Thead.module.css";
-
 export const Thead = ({
   children,
   ...props
 }: ComponentPropsWithRef<typeof Box>) => (
-  <Box asChild className={styles.thead} {...props}>
+  <Box asChild {...props}>
     <thead>{children}</thead>
   </Box>
 );
