43d15840c1bb0f535a61a0a340021820c4607cff
toha993 <mutasim.toha@optimizely.com>
2024-09-24 21:38:08 +0600

    Add icon

--- packages/react/src/alert-dialog-title/AlertDialogTitle.tsx
+++ packages/react/src/alert-dialog-title/AlertDialogTitle.tsx
@@ -1,9 +1,10 @@
 import * as RadixAlertDialog from "@radix-ui/react-alert-dialog";
 import { forwardRef } from "react";
 
-import type { BoxProps } from "../box";
-
+import { Box, type BoxProps } from "../box";
+import { Flex } from "../flex";
 import { Heading } from "../heading";
+import { IconTriangleExclamation } from "../icons/IconTriangleExclamation";
 
 type AlertDialogTitleProps = BoxProps<typeof RadixAlertDialog.Title>;
 
@@ -12,17 +13,21 @@ export const AlertDialogTitle = forwardRef<
   AlertDialogTitleProps
 >(({ children, ...props }, ref) => {
   return (
-    <Heading
-      asChild
-      fontWeight="500"
-      level="3"
-      p="lg"
-      pb="md"
-      ref={ref}
-      {...props}
-    >
-      <RadixAlertDialog.Title>{children}</RadixAlertDialog.Title>
-    </Heading>
+    <Flex flexDirection="row" gap="md" p="lg" pb="md">
+      <Box asChild color="fg.accent.red" h="16" w="auto">
+        <IconTriangleExclamation />
+      </Box>
+      <Heading
+        asChild
+        color="fg.default"
+        fontWeight="500"
+        level="3"
+        ref={ref}
+        {...props}
+      >
+        <RadixAlertDialog.Title>{children}</RadixAlertDialog.Title>
+      </Heading>
+    </Flex>
   );
 });
 
