8987265b6e2788115041b20ee797f419468e8d4b
toha993 <mutasim.toha@optimizely.com>
2024-09-25 15:54:07 +0600

    Extract sprinkles for title

--- packages/react/src/alert-dialog-title/AlertDialogTitle.tsx
+++ packages/react/src/alert-dialog-title/AlertDialogTitle.tsx
@@ -5,6 +5,7 @@ import { Box, type BoxProps } from "../box";
 import { Flex } from "../flex";
 import { Heading } from "../heading";
 import { IconTriangleExclamation } from "../icons/IconTriangleExclamation";
+import { extractSprinkles } from "../sprinkles";
 
 type AlertDialogTitleProps = BoxProps<typeof RadixAlertDialog.Title>;
 
@@ -12,18 +13,20 @@ export const AlertDialogTitle = forwardRef<
   HTMLHeadingElement,
   AlertDialogTitleProps
 >(({ children, ...props }, ref) => {
+  const { restProps, sprinkleProps } = extractSprinkles(props);
   return (
-    <Flex flexDirection="row" gap="md" p="lg" pb="md">
+    <Flex flexDirection="row" gap="md" p="lg" pb="md" {...sprinkleProps}>
       <Box asChild color="fg.accent.red" h="16" w="auto">
         <IconTriangleExclamation />
       </Box>
+
       <Heading
         asChild
         color="fg.default"
         fontWeight="500"
         level="3"
         ref={ref}
-        {...props}
+        {...restProps}
       >
         <RadixAlertDialog.Title>{children}</RadixAlertDialog.Title>
       </Heading>
