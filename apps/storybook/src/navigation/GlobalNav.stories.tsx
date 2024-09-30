2fd39bd3d1471670c6c582840a12dd2ea3570903
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-30 15:31:44 -0400

    set render at default meta level

--- apps/storybook/src/navigation/GlobalNav.stories.tsx
+++ apps/storybook/src/navigation/GlobalNav.stories.tsx
@@ -19,58 +19,55 @@ import {
 
 export default {
   component: GlobalNav,
+  render: (args) => (
+    <GlobalNav
+      addonAfter={
+        <GlobalNavProfileMenu
+          avatar={
+            <Avatar src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
+          }
+          name="Rhaenyra Targaryen"
+          organization="Optimizely"
+        />
+      }
+      style={{
+        height: "80vh",
+      }}
+      {...args}
+    >
+      <GlobalNavItem addonBefore={<IconBinaryTree />}>Projects</GlobalNavItem>
+      <GlobalNavItem active addonBefore={<IconFlag2 />}>
+        Flags
+      </GlobalNavItem>
+      <GlobalNavItem addonBefore={<IconChartInfographic />}>
+        Reports
+      </GlobalNavItem>
+      <GlobalNavItem addonBefore={<IconUsers />}>Audiences</GlobalNavItem>
+      <GlobalNavItem addonBefore={<IconHistory />}>History</GlobalNavItem>
+      <GlobalNavItem addonBefore={<IconChartLine />}>Events</GlobalNavItem>
+      <GlobalNavItem addonBefore={<IconSettings />}>Settings</GlobalNavItem>
+      <GlobalNavItem
+        addonAfter={<Link external href="https://www.google.com/" />}
+        addonBefore={<IconVocabulary />}
+      >
+        Tutorial
+      </GlobalNavItem>
+    </GlobalNav>
+  ),
 } as Meta<typeof GlobalNav>;
 
 type Story = StoryObj<typeof GlobalNav>;
 
-const Template: Story["render"] = (args) => (
-  <GlobalNav
-    addonAfter={
-      <GlobalNavProfileMenu
-        avatar={
-          <Avatar src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
-        }
-        name="Rhaenyra Targaryen"
-        organization="Optimizely"
-      />
-    }
-    style={{
-      height: "80vh",
-    }}
-    {...args}
-  >
-    <GlobalNavItem addonBefore={<IconBinaryTree />}>Projects</GlobalNavItem>
-    <GlobalNavItem active addonBefore={<IconFlag2 />}>
-      Flags
-    </GlobalNavItem>
-    <GlobalNavItem addonBefore={<IconChartInfographic />}>
-      Reports
-    </GlobalNavItem>
-    <GlobalNavItem addonBefore={<IconUsers />}>Audiences</GlobalNavItem>
-    <GlobalNavItem addonBefore={<IconHistory />}>History</GlobalNavItem>
-    <GlobalNavItem addonBefore={<IconChartLine />}>Events</GlobalNavItem>
-    <GlobalNavItem addonBefore={<IconSettings />}>Settings</GlobalNavItem>
-    <GlobalNavItem
-      addonAfter={<Link external href="https://www.google.com/" />}
-      addonBefore={<IconVocabulary />}
-    >
-      Tutorial
-    </GlobalNavItem>
-  </GlobalNav>
-);
+export const Basic: Story = {};
 
-export const Basic: Story = {
-  render: Template,
-};
 export const NotCollapsible: Story = {
   args: {
     collapsible: false,
   },
-  render: Template,
 };
+
 export const Expanded: Story = {
   args: {
     expanded: true,
   },
-  render: Template,
 };
