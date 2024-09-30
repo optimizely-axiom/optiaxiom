43654ac7d91aec6b70693a7d703a64a046c68817
Rafi Adnan <rafi.adnan@optimizely.com>
2024-09-30 19:10:35 -0400

    rename account item component
    and use Item suffix to clearly denote this is a variation of other nav items

--- apps/storybook/src/navigation/GlobalNav.stories.tsx
+++ apps/storybook/src/navigation/GlobalNav.stories.tsx
@@ -3,8 +3,8 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Avatar, Box, Link } from "@optiaxiom/react";
 import {
   GlobalNav,
+  GlobalNavAccountItem,
   GlobalNavItem,
-  GlobalNavProfileMenu,
 } from "@optiaxiom/react/unstable";
 import {
   IconBinaryTree,
@@ -20,7 +20,7 @@ import {
 export default {
   args: {
     addonAfter: (
-      <GlobalNavProfileMenu
+      <GlobalNavAccountItem
         avatar={
           <Avatar src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80" />
         }
