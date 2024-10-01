aca38fc96ef6d7eeb1fa295f01ba6b7e59b4c863
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-01 11:30:50 +0600

    update component files

--- packages/react/src/toggle-group-item/ToggleGroupItem.tsx
+++ packages/react/src/toggle-group-item/ToggleGroupItem.tsx
@@ -2,19 +2,16 @@ import * as RadixToggleGroup from "@radix-ui/react-toggle-group";
 import { forwardRef } from "react";
 
 import { Button, type ButtonProps } from "../button";
-import { extractSprinkles } from "../sprinkles";
 
 type ToggleGroupItemProps = ButtonProps<typeof RadixToggleGroup.Item>;
 
 export const ToggleGroupItem = forwardRef<
   HTMLButtonElement,
   ToggleGroupItemProps
->(({ children, ...props }, ref) => {
-  const { restProps, sprinkleProps } = extractSprinkles(props);
-
+>(({ children, value, ...props }, ref) => {
   return (
-    <Button appearance="secondary" asChild size="lg" {...sprinkleProps}>
-      <RadixToggleGroup.Item ref={ref} {...restProps}>
+    <Button appearance="secondary" asChild {...props}>
+      <RadixToggleGroup.Item ref={ref} value={value}>
         {children}
       </RadixToggleGroup.Item>
     </Button>
--- packages/react/src/toggle-group/ToggleGroup.tsx
+++ packages/react/src/toggle-group/ToggleGroup.tsx
@@ -1,18 +1,19 @@
 import * as RadixToggleGroup from "@radix-ui/react-toggle-group";
 import { forwardRef } from "react";
 
-import { Box, type BoxProps } from "../box";
+import { type BoxProps } from "../box";
+import { Flex } from "../flex";
 
 type ToggleGroupProps = BoxProps<typeof RadixToggleGroup.Root>;
 
 export const ToggleGroup = forwardRef<HTMLDivElement, ToggleGroupProps>(
   ({ children, type = "single", ...props }, ref) => {
     return (
-      <Box asChild {...props}>
+      <Flex asChild flexDirection="row" gap="0" {...props}>
         <RadixToggleGroup.Root ref={ref} type={type}>
           {children}
         </RadixToggleGroup.Root>
-      </Box>
+      </Flex>
     );
   },
 );
