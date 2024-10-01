dd364ec61c943cddcbc663c2566e0861ac24fbd5
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2024-10-01 11:30:34 +0600

    update story file

--- apps/storybook/src/forms/ToggleGroup.stories.tsx
+++ apps/storybook/src/forms/ToggleGroup.stories.tsx
@@ -1,24 +1,81 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
+import { Flex } from "@optiaxiom/react";
 import { ToggleGroup, ToggleGroupItem } from "@optiaxiom/react/unstable";
+import {
+  IconDeviceImac,
+  IconDeviceMobile,
+  IconDeviceTablet,
+} from "@tabler/icons-react";
 
-export default {
+const deviceItems = [
+  { icon: <IconDeviceImac />, value: "desktop" },
+  { icon: <IconDeviceTablet />, value: "tablet" },
+  { icon: <IconDeviceMobile />, value: "mobile" },
+] as const;
+
+const sizes = ["sm", "md", "lg"] as const;
+const meta: Meta<typeof ToggleGroup> = {
   argTypes: {
     onClick: { action: "click" },
   },
   component: ToggleGroup,
-} as Meta<typeof ToggleGroup>;
+};
+
+export default meta;
 
 type Story = StoryObj<typeof ToggleGroup>;
 
-export const Basic: Story = {
-  render: () => {
-    return (
-      <ToggleGroup type="single">
-        <ToggleGroupItem value="a">A</ToggleGroupItem>
-        <ToggleGroupItem value="b">B</ToggleGroupItem>
-        <ToggleGroupItem value="c">C</ToggleGroupItem>
-      </ToggleGroup>
-    );
+const createToggleGroupStory = (
+  storyArgs: Partial<React.ComponentProps<typeof ToggleGroup>> = {},
+): Story => ({
+  args: {
+    children: deviceItems.map(({ icon, value }) => (
+      <ToggleGroupItem icon={icon} key={value} value={value} />
+    )),
+    ...storyArgs,
   },
+});
+
+export const Basic: Story = createToggleGroupStory({
+  defaultValue: "desktop",
+});
+
+export const Sizes: Story = {
+  render: () => (
+    <Flex>
+      {sizes.map((size) => (
+        <ToggleGroup defaultValue="desktop" key={size} type="single">
+          {deviceItems.map(({ icon, value }) => (
+            <ToggleGroupItem
+              icon={icon}
+              key={value}
+              size={size}
+              value={value}
+            />
+          ))}
+        </ToggleGroup>
+      ))}
+    </Flex>
+  ),
 };
+
+export const Multiple: Story = createToggleGroupStory({
+  defaultValue: ["desktop", "mobile"],
+  type: "multiple",
+});
+
+export const DisabledGroup: Story = createToggleGroupStory({
+  disabled: true,
+});
+
+export const DisabledItem: Story = createToggleGroupStory({
+  children: deviceItems.map(({ icon, value }, index) => (
+    <ToggleGroupItem
+      disabled={index === 0}
+      icon={icon}
+      key={value}
+      value={value}
+    />
+  )),
+});
