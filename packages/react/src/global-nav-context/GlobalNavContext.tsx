69699537dc905e081e1f55ac1c26d14973bc5857
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-01 18:04:03 -0400

    add transitions when toggling side nav

--- packages/react/src/global-nav-context/GlobalNavContext.tsx
+++ packages/react/src/global-nav-context/GlobalNavContext.tsx
@@ -1,6 +1,7 @@
 import { createContext } from "@radix-ui/react-context";
 
 export const [GlobalNavContextProvider, useGlobalNavContext] = createContext<{
+  animations: boolean;
   expanded: boolean | undefined;
   onExpandedChange: (expanded: boolean) => void;
 }>("GlobalNav");
