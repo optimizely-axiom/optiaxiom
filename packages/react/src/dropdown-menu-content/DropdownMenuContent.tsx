892d922c2510c56e3e79e17535d44990c51c19ae
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-02 15:10:24 -0400

    export and re-use MenuContentBaseProps in DropdownMenuContent

--- packages/react/src/dropdown-menu-content/DropdownMenuContent.tsx
+++ packages/react/src/dropdown-menu-content/DropdownMenuContent.tsx
@@ -1,25 +1,32 @@
 import * as RadixMenu from "@radix-ui/react-dropdown-menu";
 import { forwardRef } from "react";
 
-import type { BoxProps } from "../box";
-
 import { useDropdownMenuContext } from "../dropdown-menu-context";
-import { MenuContentBase } from "../menu-content-base";
+import {
+  MenuContentBase,
+  type MenuContentBaseProps,
+} from "../menu-content-base";
 
-type MenuContentProps = BoxProps<typeof RadixMenu.Content>;
+type DropdownMenuContentProps = MenuContentBaseProps<
+  typeof RadixMenu.Content,
+  {
+    open?: never;
+  }
+>;
 
-export const DropdownMenuContent = forwardRef<HTMLDivElement, MenuContentProps>(
-  ({ align = "start", children, sideOffset = 2, ...props }, ref) => {
-    const { open } = useDropdownMenuContext("DropdownMenuContent");
+export const DropdownMenuContent = forwardRef<
+  HTMLDivElement,
+  DropdownMenuContentProps
+>(({ align = "start", children, sideOffset = 2, ...props }, ref) => {
+  const { open } = useDropdownMenuContext("DropdownMenuContent");
 
-    return (
-      <MenuContentBase open={open} {...props}>
-        <RadixMenu.Content align={align} ref={ref} sideOffset={sideOffset}>
-          {children}
-        </RadixMenu.Content>
-      </MenuContentBase>
-    );
-  },
-);
+  return (
+    <MenuContentBase open={open} {...props}>
+      <RadixMenu.Content align={align} ref={ref} sideOffset={sideOffset}>
+        {children}
+      </RadixMenu.Content>
+    </MenuContentBase>
+  );
+});
 
 DropdownMenuContent.displayName = "@optiaxiom/react/DropdownMenuContent";
