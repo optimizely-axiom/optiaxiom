ecec4fbb8c3e1d0b64a243175732cf2f2d44f834
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-02 14:41:28 -0400

    store icon figma config in object format
    so keys are automatically sorted via eslint for easier organization/lookup

--- apps/figma/src/Icon.figma.tsx
+++ apps/figma/src/Icon.figma.tsx
@@ -2,20 +2,20 @@ import figma from "@figma/code-connect";
 // @ts-expect-error -- private package
 import { IconAngleDown, IconCalendar, IconUser } from "@optimizely/axiom-icons";
 
-figma.connect(
-  IconAngleDown,
-  "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10738",
-  { example: () => <IconAngleDown /> },
-);
-
-figma.connect(
-  IconCalendar,
-  "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10317",
-  { example: () => <IconCalendar /> },
-);
-
-figma.connect(
-  IconUser,
-  "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10794",
-  { example: () => <IconUser /> },
-);
+export default {
+  IconAngleDown: figma.connect(
+    IconAngleDown,
+    "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10738",
+    { example: () => <IconAngleDown /> },
+  ),
+  IconCalendar: figma.connect(
+    IconCalendar,
+    "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10317",
+    { example: () => <IconCalendar /> },
+  ),
+  IconUser: figma.connect(
+    IconUser,
+    "https://www.figma.com/design/F1vaZkWrXaCLxM81W5OzkG/Axiom-Icons?node-id=1236-10794",
+    { example: () => <IconUser /> },
+  ),
+};
