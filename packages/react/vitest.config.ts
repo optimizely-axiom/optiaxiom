87c7cabc13e038228d62a4d32cdaa56df4ebadc2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-05 02:31:12 -0400

    fix vanilla extract build time issues for vitest
    rather than defaulting to empty object which messes up the sprinkle types

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,7 +2,7 @@ import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [vanillaExtractPlugin()],
+  plugins: [vanillaExtractPlugin({ unstable_mode: "transform" })],
   test: {
     environment: "happy-dom",
     restoreMocks: true,
