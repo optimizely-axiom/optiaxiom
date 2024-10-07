04b174e72a692ddb0a8a24fcdd7b58069f936b2c
toha993 <mutasim.toha@optimizely.com>
2024-10-07 23:56:29 +0600

    Add icon component

---
+++ packages/react/src/icon/Icon.tsx
@@ -0,0 +1,17 @@
+import { type ComponentPropsWithRef, forwardRef } from "react";
+
+import { Box } from "../box";
+
+type IconProps = ComponentPropsWithRef<typeof Box>;
+
+export const Icon = forwardRef<HTMLDivElement, IconProps>(
+  ({ children, ...props }, ref) => {
+    return (
+      <Box asChild h="16" ref={ref} w="auto" {...props}>
+        {children}
+      </Box>
+    );
+  },
+);
+
+Icon.displayName = "@optiaxiom/react/Icon";
