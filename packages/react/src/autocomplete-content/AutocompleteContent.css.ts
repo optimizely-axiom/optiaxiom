c37c41ece2dcb7c6d555b622b41325dde176cccc
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-07 16:10:35 -0400

    hide popover if it is empty

--- packages/react/src/autocomplete-content/AutocompleteContent.css.ts
+++ packages/react/src/autocomplete-content/AutocompleteContent.css.ts
@@ -1,15 +1,25 @@
 import { recipe, style } from "../vanilla-extract";
 
+const marker = style({});
+
 export const content = recipe({
   base: [
     {
       p: "4",
     },
+    style({
+      selectors: {
+        [`&:has(${marker}:empty)`]: {
+          visibility: "hidden",
+        },
+      },
+    }),
   ],
 });
 
 export const list = recipe({
   base: [
+    marker,
     style({
       overflow: "auto",
       overscrollBehavior: "contain",
