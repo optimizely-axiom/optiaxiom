c93fedaf80b27f2dad0ebbde3972fe2e310914bf
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-14 09:40:18 -0400

    optimize deps

--- apps/storybook/vitest.config.ts
+++ apps/storybook/vitest.config.ts
@@ -2,6 +2,12 @@ import { storybookTest } from "@storybook/experimental-addon-test/vitest-plugin"
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  optimizeDeps: {
+    include: [
+      "@storybook/experimental-addon-test/internal/test-utils",
+      "react/jsx-dev-runtime",
+    ],
+  },
   plugins: [
     storybookTest({
       storybookScript: "pnpm dev --ci",
