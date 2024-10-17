5de004f41e9a8de8a312862fdca42298f78061d4
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-17 16:11:42 -0400

    add basic select docs

---
+++ apps/docs/demos/select/usage/data.ts
@@ -0,0 +1,12 @@
+export const colors = [
+  { color: "#00B8D9", label: "Ocean", value: "ocean" },
+  { color: "#0052CC", isDisabled: true, label: "Blue", value: "blue" },
+  { color: "#5243AA", label: "Purple", value: "purple" },
+  { color: "#FF5630", label: "Red", value: "red" },
+  { color: "#FF8B00", label: "Orange", value: "orange" },
+  { color: "#FFC400", label: "Yellow", value: "yellow" },
+  { color: "#36B37E", label: "Green", value: "green" },
+  { color: "#00875A", label: "Forest", value: "forest" },
+  { color: "#253858", label: "Slate", value: "slate" },
+  { color: "#666666", label: "Silver", value: "silver" },
+];
