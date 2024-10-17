4a83e45f7c1ae74060546a2f31f77bcb1621b9d2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-17 14:55:31 -0400

    add basic autocomplete docs

---
+++ apps/docs/demos/autocomplete/usage/App.tsx
@@ -0,0 +1,48 @@
+import {
+  Autocomplete,
+  AutocompleteContent,
+  AutocompleteEmptyItem,
+  AutocompleteItem,
+  AutocompleteItemIndicator,
+  AutocompleteList,
+  AutocompleteTrigger,
+} from "@optiaxiom/react/unstable";
+import { useState } from "react";
+
+import { colors } from "./data";
+
+export function App() {
+  const [items, setItems] = useState(colors);
+
+  return (
+    <Autocomplete
+      isItemDisabled={(item) => Boolean(item.isDisabled)}
+      items={items}
+      itemToKey={(item) => item?.value}
+      itemToString={(item) => (item ? item.label : "")}
+      onInputValueChange={(inputValue) => {
+        setItems(
+          colors.filter(
+            (color) =>
+              !inputValue ||
+              color.label.toLowerCase().includes(inputValue.toLowerCase()),
+          ),
+        );
+      }}
+    >
+      <AutocompleteTrigger placeholder="Search a color..." w="240" />
+
+      <AutocompleteContent>
+        <AutocompleteList>
+          {(item: (typeof items)[number]) => (
+            <AutocompleteItem>
+              {item.label}
+              <AutocompleteItemIndicator />
+            </AutocompleteItem>
+          )}
+        </AutocompleteList>
+        <AutocompleteEmptyItem>No result found</AutocompleteEmptyItem>
+      </AutocompleteContent>
+    </Autocomplete>
+  );
+}
