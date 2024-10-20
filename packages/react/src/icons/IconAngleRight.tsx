2a332fd150501daac43d4cf5b91e76308a0a70ff
toha993 <mutasim.toha@optimizely.com>
2024-10-20 15:50:31 +0600

    Update angle icons

--- packages/react/src/icons/IconAngleRight.tsx
+++ packages/react/src/icons/IconAngleRight.tsx
@@ -3,12 +3,10 @@ import { withIcon } from "./withIcon";
 export const IconAngleRight = withIcon(
   {
     fill: "none",
-    height: 16,
-    name: "IconAngleLeft",
-    width: 20,
+    name: "IconAngleRight",
   },
   <path
-    d="M12.5982 8.34373L8.09825 13.3437C7.91075 13.5625 7.59825 13.5625 7.41075 13.375C7.192 13.1875 7.192 12.875 7.3795 12.6875L11.567 7.99998L7.3795 3.34373C7.192 3.15623 7.192 2.84373 7.41075 2.65623C7.59825 2.46873 7.91075 2.46873 8.09825 2.68748L12.5982 7.65623C12.7857 7.87498 12.7857 8.15623 12.5982 8.34373Z"
+    d="M7.5 13L12.5 8L7.5 3"
     stroke="currentColor"
     strokeLinecap="round"
     strokeLinejoin="round"
