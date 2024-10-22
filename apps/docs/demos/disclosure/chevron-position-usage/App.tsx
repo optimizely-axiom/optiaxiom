7790c75e07d4f864295b7bad5feb5bc62c90392e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-22 13:17:07 -0400

    add disclosure docs

---
+++ apps/docs/demos/disclosure/chevron-position-usage/App.tsx
@@ -0,0 +1,23 @@
+import type { ComponentPropsWithRef } from "react";
+
+import {
+  Disclosure,
+  DisclosureContent,
+  DisclosureTrigger,
+} from "@optiaxiom/react";
+
+export function App({
+  chevronPosition,
+}: Pick<ComponentPropsWithRef<typeof DisclosureTrigger>, "chevronPosition">) {
+  return (
+    <Disclosure maxW="sm" w="full">
+      <DisclosureTrigger chevronPosition={chevronPosition}>
+        Disclosure label
+      </DisclosureTrigger>
+      <DisclosureContent>
+        Aenean neque dui, lobortis et sem quis, mattis varius nisl. Nulla turpis
+        sapien, venenatis eu pharetra at, ullamcorper sed nibh.
+      </DisclosureContent>
+    </Disclosure>
+  );
+}
---
+++ apps/docs/demos/disclosure/index.ts
@@ -0,0 +1,2 @@
+export { App as ChevronPositionUsage } from "./chevron-position-usage/App";
+export { App as Usage } from "./usage/App";
