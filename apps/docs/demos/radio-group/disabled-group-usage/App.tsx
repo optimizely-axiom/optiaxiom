9f74fe4940ee1d0ceefdbf284b24efda7e60c03e
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-25 15:58:38 -0400

    ensure radio groups have unique names in demos

--- apps/docs/demos/radio-group/disabled-group-usage/App.tsx
+++ apps/docs/demos/radio-group/disabled-group-usage/App.tsx
@@ -2,7 +2,7 @@ import { RadioGroup, RadioGroupItem } from "@optiaxiom/react";
 
 export function App() {
   return (
-    <RadioGroup disabled value="one">
+    <RadioGroup disabled name="disabled-group-usage" value="one">
       <RadioGroupItem value="one">Option One</RadioGroupItem>
       <RadioGroupItem value="two">Option Two</RadioGroupItem>
     </RadioGroup>
--- apps/docs/demos/radio-group/label-usage/App.tsx
+++ apps/docs/demos/radio-group/label-usage/App.tsx
@@ -2,7 +2,7 @@ import { RadioGroup, RadioGroupItem } from "@optiaxiom/react";
 
 export function App() {
   return (
-    <RadioGroup>
+    <RadioGroup name="label-usage">
       <RadioGroupItem value="one" />
       <RadioGroupItem value="two">Option Two</RadioGroupItem>
     </RadioGroup>
