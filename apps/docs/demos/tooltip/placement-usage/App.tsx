04f9522b5013445f93631d8639c511a02bd229a2
Rafi Adnan <rafi.adnan@optimizely.com>
2024-10-28 15:01:58 -0400

    add tooltip docs

---
+++ apps/docs/demos/tooltip/placement-usage/App.tsx
@@ -0,0 +1,47 @@
+import { Button, Grid, Tooltip } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <Grid gap="xs" gridTemplateColumns="3">
+      <Tooltip align="start" content="Top Start" side="top">
+        <Button py="20">Top Start</Button>
+      </Tooltip>
+      <Tooltip align="center" content="Top Center" side="top">
+        <Button py="20">Top Center</Button>
+      </Tooltip>
+      <Tooltip align="end" content="Top End" side="top">
+        <Button py="20">Top End</Button>
+      </Tooltip>
+
+      <Tooltip align="start" content="Right Start" side="right">
+        <Button py="20">Right Start</Button>
+      </Tooltip>
+      <Tooltip align="center" content="Right Center" side="right">
+        <Button py="20">Right Center</Button>
+      </Tooltip>
+      <Tooltip align="end" content="Right End" side="right">
+        <Button py="20">Right End</Button>
+      </Tooltip>
+
+      <Tooltip align="start" content="bottom-start" side="bottom">
+        <Button py="20">Bottom Start</Button>
+      </Tooltip>
+      <Tooltip align="center" content="bottom-center" side="bottom">
+        <Button py="20">Bottom Center</Button>
+      </Tooltip>
+      <Tooltip align="end" content="bottom-end" side="bottom">
+        <Button py="20">Bottom End</Button>
+      </Tooltip>
+
+      <Tooltip align="start" content="Left Start" side="left">
+        <Button py="20">Left Start</Button>
+      </Tooltip>
+      <Tooltip align="center" content="Left Center" side="left">
+        <Button py="20">Left Center</Button>
+      </Tooltip>
+      <Tooltip align="end" content="Left End" side="left">
+        <Button py="20">Left End</Button>
+      </Tooltip>
+    </Grid>
+  );
+}
---
+++ apps/docs/demos/tooltip/usage/App.tsx
@@ -0,0 +1,9 @@
+import { Button, Tooltip } from "@optiaxiom/react";
+
+export function App() {
+  return (
+    <Tooltip content="Sample tooltip content">
+      <Button>Hover me</Button>
+    </Tooltip>
+  );
+}
