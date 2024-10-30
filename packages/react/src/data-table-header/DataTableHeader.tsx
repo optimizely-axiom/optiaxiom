21198356077c9a28c55b3a5f85457805ee3da8af
toha993 <mutasim.toha@optimizely.com>
2024-10-30 19:18:43 +0600

    Fix sort icon

--- packages/react/src/data-table-header/DataTableHeader.tsx
+++ packages/react/src/data-table-header/DataTableHeader.tsx
@@ -2,9 +2,11 @@ import type { ReactNode } from "react";
 
 import { type Column } from "@tanstack/react-table";
 
-import { Button } from "../button";
 import { Flex } from "../flex";
+import { Icon } from "../icon";
 import { IconSort } from "../icons/IconSort";
+import { IconSortDown } from "../icons/IconSortDown";
+import { IconSortUp } from "../icons/IconSortUp";
 import { Tooltip } from "../tooltip";
 import * as styles from "./DataTableHeader.css";
 
@@ -18,8 +20,15 @@ export const DataTableHeader = <TData,>({
   variant?: "number" | "text";
 }) => {
   if (!column) return null;
+
   return (
-    <Flex {...styles.header({ variant })}>
+    <Flex
+      {...styles.header({
+        sortable: column.columnDef.enableSorting,
+        variant,
+      })}
+      onClick={() => column.columnDef.enableSorting && column.toggleSorting()}
+    >
       {children}
       {column.columnDef.enableSorting && (
         <Tooltip
@@ -33,15 +42,20 @@ export const DataTableHeader = <TData,>({
               : undefined
           }
         >
-          <Button
-            border="0"
-            icon={
-              <IconSort
-                sortDirection={column.getIsSorted() as "asc" | "desc" | false}
-              />
-            }
-            onClick={() => column.toggleSorting()}
-          />
+          <Icon
+            asChild
+            {...styles.icon({
+              sorted: column.getIsSorted() === false ? false : true,
+            })}
+          >
+            {column.getIsSorted() === false ? (
+              <IconSort />
+            ) : column.getIsSorted() === "asc" ? (
+              <IconSortUp />
+            ) : (
+              <IconSortDown />
+            )}
+          </Icon>
         </Tooltip>
       )}
     </Flex>
--- packages/react/src/icons/IconSort.tsx
+++ packages/react/src/icons/IconSort.tsx
@@ -1,35 +1,13 @@
-import React from "react";
-
 import { withIcon } from "./withIcon";
 
-interface SortIconProps {
-  sortDirection: "asc" | "desc" | false;
-}
-
-const SortIcon = ({ sortDirection }: SortIconProps) => (
-  <>
-    <path
-      d="M7.04245 9.28571H12.9576C13.4933 9.28571 13.7835 8.63839 13.3817 8.23661L10.4353 5.20089C10.3237 5.06696 10.1675 5 10.0112 5C9.85495 5 9.67638 5.06696 9.56477 5.20089L6.61834 8.23661C6.21656 8.63839 6.50674 9.28571 7.04245 9.28571Z"
-      fill="currentColor"
-      style={{ opacity: sortDirection === "asc" ? 1 : 0.3 }}
-    />
-    <path
-      d="M7.04245 10.7143H12.9576C13.4933 10.7143 13.7835 11.3839 13.3817 11.7857L10.4353 14.8214C10.3237 14.9554 10.1675 15 10.0112 15C9.83263 15 9.67638 14.9554 9.56477 14.8214L6.61834 11.7857C6.21656 11.3839 6.50674 10.7143 7.04245 10.7143Z"
-      fill="currentColor"
-      style={{ opacity: sortDirection === "desc" ? 1 : 0.3 }}
-    />
-  </>
+export const IconSort = withIcon(
+  {
+    height: 12,
+    name: "IconSort",
+    width: 14,
+  },
+  <path
+    d="M3.88274 5.25H10.0937C10.6562 5.25 10.9609 4.57031 10.539 4.14844L7.44524 0.960938C7.32805 0.820312 7.16399 0.75 6.99993 0.75C6.83586 0.75 6.64836 0.820312 6.53118 0.960938L3.43743 4.14844C3.01555 4.57031 3.32024 5.25 3.88274 5.25ZM10.0937 6.75H3.88274C3.32024 6.75 3.01555 7.45312 3.43743 7.875L6.53118 11.0625C6.64836 11.2031 6.81243 11.25 6.99993 11.25C7.16399 11.25 7.32805 11.2031 7.44524 11.0625L10.539 7.875C10.9609 7.45312 10.6562 6.75 10.0937 6.75Z"
+    fill="currentColor"
+  />,
 );
-
-export const IconSort = ({ sortDirection }: SortIconProps) => {
-  const Icon = React.useMemo(
-    () =>
-      withIcon(
-        { height: 20, name: "IconSort", width: 20 },
-        <SortIcon sortDirection={sortDirection} />,
-      ),
-    [sortDirection],
-  );
-
-  return <Icon />;
-};
