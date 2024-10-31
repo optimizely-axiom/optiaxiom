e70c8b166fb44c3097c1d5a86e120eb44851e560
toha993 <mutasim.toha@optimizely.com>
2024-11-01 00:05:39 +0600

    Update icon

---
+++ packages/react/src/icons/CheckboxCheck.tsx
@@ -0,0 +1,17 @@
+import { withIcon } from "./withIcon";
+
+export const CheckboxCheck = withIcon(
+  {
+    fill: "none",
+    height: 8,
+    name: "CheckboxCheck",
+    width: 12,
+  },
+  <path
+    d="M1.5 3.47059L4.83333 7L10.5 1"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="1.5"
+  />,
+);
