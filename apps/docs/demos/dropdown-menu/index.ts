cd4fd6c7cfd5fa0e9c1e4a88b039541bb1e6ba2f
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-01 17:20:51 -0400

    remove dropdown trigger composition doc
    this is an escape hatch for rare use cases - should not be advertised

--- apps/docs/demos/dropdown-menu/index.ts
+++ apps/docs/demos/dropdown-menu/index.ts
@@ -6,6 +6,5 @@ export { App as DisabledUsage } from "./disabled-usage/App";
 export { App as ModalUsage } from "./modal-usage/App";
 export { App as PositionUsage } from "./position-usage/App";
 export { App as SelectUsage } from "./select-usage/App";
-export { App as TriggerCompositionUsage } from "./trigger-composition-usage/App";
 export { App as TriggerUsage } from "./trigger-usage/App";
 export { App as Usage } from "./usage/App";
