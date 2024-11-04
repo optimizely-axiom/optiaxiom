2618a6e784904858e3895a7e8e27b0069fd7d274
toha993 <mutasim.toha@optimizely.com>
2024-11-04 21:34:50 +0600

    Pass disabled prop to RadixTrigger

--- packages/react/src/dropdown-menu-trigger/DropdownMenuTrigger.tsx
+++ packages/react/src/dropdown-menu-trigger/DropdownMenuTrigger.tsx
@@ -3,21 +3,33 @@ import { type ComponentPropsWithoutRef, forwardRef } from "react";
 
 import { Button } from "../button";
 import { MenuButton } from "../menu-button";
-import { extractSprinkles } from "../sprinkles";
 
 type MenuTriggerProps = ComponentPropsWithoutRef<typeof Button>;
 
 export const DropdownMenuTrigger = forwardRef<
   HTMLButtonElement,
   MenuTriggerProps
->(({ asChild, children, ...props }, ref) => {
-  const { restProps, sprinkleProps } = extractSprinkles(props);
-
-  return (
-    <RadixMenu.Trigger asChild ref={ref} {...sprinkleProps}>
-      {asChild ? children : <MenuButton {...restProps}>{children}</MenuButton>}
-    </RadixMenu.Trigger>
-  );
-});
+>(
+  (
+    { asChild, children, icon, iconPosition, justifyContent, ...props },
+    ref,
+  ) => {
+    return (
+      <RadixMenu.Trigger asChild ref={ref} {...props}>
+        {asChild ? (
+          children
+        ) : (
+          <MenuButton
+            icon={icon}
+            iconPosition={iconPosition}
+            justifyContent={justifyContent}
+          >
+            {children}
+          </MenuButton>
+        )}
+      </RadixMenu.Trigger>
+    );
+  },
+);
 
 DropdownMenuTrigger.displayName = "@optiaxiom/react/DropdownMenuTrigger";
