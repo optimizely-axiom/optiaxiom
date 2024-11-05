165f4ecff2ab9a388363f74c3532a38e63f47319
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-05 17:03:56 -0500

    expand button group selectors to support nested items
    so either the target itself is the direct child of the group or a descendant of
    the direct child

---
+++ .changeset/kind-fans-fail.md
@@ -0,0 +1,5 @@
+---
+"@optiaxiom/react": patch
+---
+
+fix button group styles to handle nested elements
--- packages/react/src/button-group/groupStyle.ts
+++ packages/react/src/button-group/groupStyle.ts
@@ -3,35 +3,54 @@ import * as styles from "./ButtonGroup.css";
 const group = styles.className;
 
 export const groupStyle = () => ({
-  [`${group}[data-orientation="horizontal"] &:not(:first-child):not(:last-child)`]:
-    {
-      borderInlineWidth: "0.5px",
-    },
-  [`${group}[data-orientation="horizontal"] &:not(:only-child):first-child`]: {
+  [[
+    `${group}[data-orientation="horizontal"] > &:not(:first-child):not(:last-child)`,
+    `${group}[data-orientation="horizontal"] > :not(:first-child):not(:last-child) &`,
+  ].join(",")]: {
+    borderInlineWidth: "0.5px",
+  },
+  [[
+    `${group}[data-orientation="horizontal"] > &:not(:only-child):first-child`,
+    `${group}[data-orientation="horizontal"] > :not(:only-child):first-child &`,
+  ].join(",")]: {
     borderBottomRightRadius: 0,
     borderRightWidth: "0.5px",
     borderTopRightRadius: 0,
   },
-  [`${group}[data-orientation="horizontal"] &:not(:only-child):last-child`]: {
+  [[
+    `${group}[data-orientation="horizontal"] > &:not(:only-child):last-child`,
+    `${group}[data-orientation="horizontal"] > :not(:only-child):last-child &`,
+  ].join(",")]: {
     borderBottomLeftRadius: 0,
     borderLeftWidth: "0.5px",
     borderTopLeftRadius: 0,
   },
-  [`${group}[data-orientation="vertical"] &:not(:first-child):not(:last-child)`]:
-    {
-      borderBlockWidth: "0.5px",
-    },
-  [`${group}[data-orientation="vertical"] &:not(:only-child):first-child`]: {
+  [[
+    `${group}[data-orientation="vertical"] > &:not(:first-child):not(:last-child)`,
+    `${group}[data-orientation="vertical"] > :not(:first-child):not(:last-child) &`,
+  ].join(",")]: {
+    borderBlockWidth: "0.5px",
+  },
+  [[
+    `${group}[data-orientation="vertical"] > &:not(:only-child):first-child`,
+    `${group}[data-orientation="vertical"] > :not(:only-child):first-child &`,
+  ].join(",")]: {
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
     borderBottomWidth: "0.5px",
   },
-  [`${group}[data-orientation="vertical"] &:not(:only-child):last-child`]: {
+  [[
+    `${group}[data-orientation="vertical"] > &:not(:only-child):last-child`,
+    `${group}[data-orientation="vertical"] > :not(:only-child):last-child &`,
+  ].join(",")]: {
     borderTopLeftRadius: 0,
     borderTopRightRadius: 0,
     borderTopWidth: "0.5px",
   },
-  [`${group}[data-orientation] &:not(:first-child):not(:last-child)`]: {
+  [[
+    `${group}[data-orientation] > &:not(:first-child):not(:last-child)`,
+    `${group}[data-orientation] > :not(:first-child):not(:last-child) &`,
+  ].join(",")]: {
     borderRadius: 0,
   },
 });
