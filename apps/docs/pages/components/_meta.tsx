91382b919aae3c25bf35a70e45ef56dd2cce2cd0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-05 09:23:30 -0500

    rename combobox component group
    to prevent clash with actual Combobox component

--- apps/docs/pages/components/_meta.tsx
+++ apps/docs/pages/components/_meta.tsx
@@ -36,8 +36,8 @@ export default {
   button: "Button",
   "toggle-button": "ToggleButton",
 
-  "-- Combobox": {
-    title: "Combobox",
+  "-- Comboboxes": {
+    title: "Comboboxes",
     type: "separator",
   },
   autocomplete: {
