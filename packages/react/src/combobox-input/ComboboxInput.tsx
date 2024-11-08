8e16c10a4acf1a8b900936fb0beaa2e0770c8fe9
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-08 17:27:29 -0500

    move combobox specific input handling to ComboboxInput

--- packages/react/src/combobox-input/ComboboxInput.tsx
+++ packages/react/src/combobox-input/ComboboxInput.tsx
@@ -1,5 +1,28 @@
+import { type ComponentPropsWithoutRef, forwardRef } from "react";
+
+import { useCommandContext } from "../command-context";
 import { CommandInput } from "../command-input";
 
-export const ComboboxInput = CommandInput;
+type ComboboxInputProps = ComponentPropsWithoutRef<typeof CommandInput>;
+
+export const ComboboxInput = forwardRef<HTMLInputElement, ComboboxInputProps>(
+  (props, ref) => {
+    const { downshift, highlightedItem } = useCommandContext("ComboboxInput");
+
+    return (
+      <CommandInput
+        m="4"
+        onKeyDown={(event) => {
+          if (event.key === " " && highlightedItem) {
+            event.preventDefault();
+            downshift.selectItem(highlightedItem);
+          }
+        }}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
 
 ComboboxInput.displayName = "@optiaxiom/react/ComboboxInput";
--- packages/react/src/command-input/CommandInput.tsx
+++ packages/react/src/command-input/CommandInput.tsx
@@ -9,23 +9,15 @@ type CommandInputProps = ComponentPropsWithoutRef<typeof Input>;
 export const CommandInput = forwardRef<HTMLInputElement, CommandInputProps>(
   (props, ref) => {
     const { restProps, sprinkleProps } = extractSprinkles(props);
-    const { downshift, highlightedItem, setInputValue } =
-      useCommandContext("CommandInput");
+    const { downshift, setInputValue } = useCommandContext("CommandInput");
 
     return (
       <Input
-        m="4"
         {...sprinkleProps}
         {...downshift.getInputProps({
           ...restProps,
           onChange: (event) => {
             setInputValue("value" in event.target ? event.target.value : "");
-          },
-          onKeyDown: (event) => {
-            if (event.key === " " && highlightedItem) {
-              event.preventDefault();
-              downshift.selectItem(highlightedItem);
-            }
           },
           ref,
         })}
