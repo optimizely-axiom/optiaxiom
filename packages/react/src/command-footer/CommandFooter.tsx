4b3265bce661c5a2b77a90e10256b506b4d6140a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-08 11:39:25 -0500

    fix html element used for command footer
    and inline sprinkle props

--- packages/react/src/command-footer/CommandFooter.tsx
+++ packages/react/src/command-footer/CommandFooter.tsx
@@ -1,15 +1,23 @@
 import { type ComponentPropsWithRef, forwardRef } from "react";
 
 import { Flex } from "../flex";
-import * as styles from "./CommandFooter.css";
 
 type CommandFooterProps = ComponentPropsWithRef<typeof Flex>;
 
 export const CommandFooter = forwardRef<HTMLDivElement, CommandFooterProps>(
-  ({ children, className, ...props }, ref) => {
+  ({ children, ...props }, ref) => {
     return (
-      <Flex asChild ref={ref} {...styles.footer({}, className)} {...props}>
-        <li>{children}</li>
+      <Flex
+        borderColor="border.secondary"
+        borderT="1"
+        flexDirection="row"
+        gap="md"
+        justifyContent="space-between"
+        p="sm"
+        ref={ref}
+        {...props}
+      >
+        {children}
       </Flex>
     );
   },
