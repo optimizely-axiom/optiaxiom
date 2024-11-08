59b884fe0668ab4410bc420592ef11d6ae686543
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-08 17:28:58 -0500

    fix forwarding props to button in dialog trigger

--- packages/react/src/dialog-close/DialogClose.tsx
+++ packages/react/src/dialog-close/DialogClose.tsx
@@ -2,17 +2,14 @@ import * as RadixDialog from "@radix-ui/react-dialog";
 import { forwardRef } from "react";
 
 import { Button, type ButtonProps } from "../button";
-import { extractSprinkles } from "../sprinkles";
 
 type DialogCloseProps = ButtonProps<typeof RadixDialog.Close>;
 
 export const DialogClose = forwardRef<HTMLButtonElement, DialogCloseProps>(
   ({ asChild, children, ...props }, ref) => {
-    const { restProps, sprinkleProps } = extractSprinkles(props);
-
     return (
-      <RadixDialog.Close asChild ref={ref} {...sprinkleProps}>
-        {asChild ? children : <Button {...restProps}>{children}</Button>}
+      <RadixDialog.Close asChild ref={ref} {...props}>
+        {asChild ? children : <Button>{children}</Button>}
       </RadixDialog.Close>
     );
   },
