a3d91c6ca7b2be7e0aefa6f99c536e9eadf35513
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-14 16:08:40 -0500

    fix focus guard for SelectContent
    by re-using radix focus-guards and dismissable-layer packages

---
+++ packages/react/src/select-content/SelectContentImpl.tsx
@@ -0,0 +1,45 @@
+import { DismissableLayer } from "@radix-ui/react-dismissable-layer";
+import { useFocusGuards } from "@radix-ui/react-focus-guards";
+import { PopperContent } from "@radix-ui/react-popper";
+import { forwardRef } from "react";
+
+import { Box, type BoxProps } from "../box";
+import * as styles from "./SelectContent.css";
+
+type SelectContentImplProps = BoxProps<typeof PopperContent>;
+
+export const SelectContentImpl = forwardRef<
+  HTMLDivElement,
+  SelectContentImplProps
+>(
+  (
+    {
+      align = "center",
+      children,
+      className,
+      side = "bottom",
+      sideOffset = 5,
+      ...props
+    },
+    ref,
+  ) => {
+    useFocusGuards();
+
+    return (
+      <DismissableLayer asChild>
+        <Box asChild ref={ref} {...styles.content({}, className)} {...props}>
+          <PopperContent
+            align={align}
+            asChild
+            side={side}
+            sideOffset={sideOffset}
+          >
+            {children}
+          </PopperContent>
+        </Box>
+      </DismissableLayer>
+    );
+  },
+);
+
+SelectContentImpl.displayName = "@optiaxiom/react/SelectContentImpl";
