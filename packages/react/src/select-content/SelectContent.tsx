c85dfda1953bce5b5ac93e3d4f7e4afbc522ebe7
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-14 16:08:40 -0500

    fix focus guard for SelectContent
    by re-using radix focus-guards and dismissable-layer packages

--- packages/react/src/select-content/SelectContent.tsx
+++ packages/react/src/select-content/SelectContent.tsx
@@ -1,56 +1,37 @@
-import { PopperContent } from "@radix-ui/react-popper";
 import { forwardRef } from "react";
 
 import { Box, type BoxProps } from "../box";
 import { ListboxBase } from "../listbox-base";
 import { useSelectContext } from "../select-context";
 import { Spinner } from "../spinner";
-import * as styles from "./SelectContent.css";
+import { SelectContentImpl } from "./SelectContentImpl";
 
 type SelectContentProps = BoxProps<
-  typeof PopperContent,
+  typeof SelectContentImpl,
   {
     loading?: boolean;
   }
 >;
 
 export const SelectContent = forwardRef<HTMLDivElement, SelectContentProps>(
-  (
-    {
-      align = "center",
-      children,
-      className,
-      loading,
-      side = "bottom",
-      sideOffset = 5,
-      ...props
-    },
-    ref,
-  ) => {
+  ({ children, loading, ...props }, ref) => {
     const { downshift, isOpen } = useSelectContext("SelectContent");
 
     return (
       <ListboxBase open={isOpen} provider="popper">
-        <Box asChild ref={ref} {...styles.content({}, className)} {...props}>
-          <PopperContent
-            align={align}
-            asChild
-            side={side}
-            sideOffset={sideOffset}
-          >
-            <ul {...downshift.getMenuProps({}, { suppressRefError: true })}>
-              {loading ? (
-                <Box asChild display="flex" justifyContent="center" p="md">
-                  <li>
-                    <Spinner />
-                  </li>
-                </Box>
-              ) : (
-                children
-              )}
-            </ul>
-          </PopperContent>
-        </Box>
+        <SelectContentImpl ref={ref} {...props}>
+          <ul {...downshift.getMenuProps({}, { suppressRefError: true })}>
+            {loading ? (
+              <Box asChild display="flex" justifyContent="center" p="md">
+                <li>
+                  <Spinner />
+                </li>
+              </Box>
+            ) : (
+              children
+            )}
+          </ul>
+        </SelectContentImpl>
       </ListboxBase>
     );
   },
