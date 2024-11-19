696629876d4b87169abba141ad396fc18390465a
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-19 11:26:30 -0500

    use single sheet to manage local shadow dom styles
    to prevent needing to manage the different shadow roots

--- packages/web-components/src/styles.ts
+++ packages/web-components/src/styles.ts
@@ -1,13 +1,12 @@
-const shadowRoots = new Set<ShadowRoot>();
-const sheets: CSSStyleSheet[] = [];
-
-const sheet = new CSSStyleSheet();
-sheet.insertRule(`slot[name]::slotted(*) {
+const styles: string[] = [
+  `slot[name]::slotted(*) {
   display: inline-flex;
   height: 100%;
   width: 100%;
-}`);
-sheets.push(sheet);
+}`,
+];
+
+export const styleSheet = new CSSStyleSheet();
 
 const uuid = "ax" + crypto.getRandomValues(new Uint32Array(1))[0].toString(36);
 
@@ -30,22 +29,8 @@ export function injectGlobalStyle(text: string) {
 }
 
 export function injectLocalStyle(text: string) {
-  const sheet = new CSSStyleSheet();
-  void sheet.replace(
+  styles.push(
     text.replaceAll(/Fira Code Variable|InterVariable/g, (m) => `${uuid} ${m}`),
   );
-  sheets.push(sheet);
-
-  for (const shadowRoot of shadowRoots) {
-    shadowRoot.adoptedStyleSheets = sheets;
-  }
-}
-
-export function registerShadowRoot(shadowRoot: ShadowRoot) {
-  shadowRoots.add(shadowRoot);
-  shadowRoot.adoptedStyleSheets = sheets;
-}
-
-export function unregisterShadowRoot(shadowRoot: ShadowRoot) {
-  shadowRoots.delete(shadowRoot);
+  void styleSheet.replace(styles.join("\n"));
 }
