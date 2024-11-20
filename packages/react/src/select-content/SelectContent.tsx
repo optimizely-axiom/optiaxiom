66709530c02516f5274e502b11a6d0c4df1b86c6
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-20 14:47:13 -0500

    remove ul from select
    and use div everywhere

--- packages/react/src/select-content/SelectContent.tsx
+++ packages/react/src/select-content/SelectContent.tsx
@@ -17,24 +17,20 @@ type SelectContentProps = BoxProps<
 
 export const SelectContent = forwardRef<HTMLDivElement, SelectContentProps>(
   ({ children, loading, ...props }, ref) => {
-    const { downshift, isOpen } = useSelectContext("SelectContent");
+    const { isOpen } = useSelectContext("SelectContent");
 
     return (
       isOpen && (
         <Portal asChild>
           <MenuListbox asChild provider="popper">
             <SelectContentImpl ref={ref} {...props}>
-              <ul {...downshift.getMenuProps({}, { suppressRefError: true })}>
-                {loading ? (
-                  <Box asChild display="flex" justifyContent="center" p="md">
-                    <li>
-                      <Spinner />
-                    </li>
-                  </Box>
-                ) : (
-                  children
-                )}
-              </ul>
+              {loading ? (
+                <Box display="flex" justifyContent="center" p="md">
+                  <Spinner />
+                </Box>
+              ) : (
+                children
+              )}
             </SelectContentImpl>
           </MenuListbox>
         </Portal>
