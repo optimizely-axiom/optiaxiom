50d8727d32513213e322f6589af591be56541dc1
Rafi Adnan <rafi.adnan@optimizely.com>
2024-11-20 15:19:35 -0500

    announce loading state or number of options in autocomplete

--- packages/react/src/autocomplete-content/AutocompleteContent.tsx
+++ packages/react/src/autocomplete-content/AutocompleteContent.tsx
@@ -1,4 +1,5 @@
 import { Portal } from "@radix-ui/react-portal";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { forwardRef } from "react";
 
 import { useAutocompleteContext } from "../autocomplete-context";
@@ -18,13 +19,21 @@ export const AutocompleteContent = forwardRef<
   HTMLDivElement,
   AutocompleteContentProps
 >(({ children, loading, ...props }, ref) => {
-  const { isOpen } = useAutocompleteContext("AutocompleteContent");
+  const { isOpen, items } = useAutocompleteContext("AutocompleteContent");
 
   return (
     isOpen && (
       <Portal asChild>
         <MenuListbox asChild minW="trigger" provider="popper" {...props}>
           <AutocompleteContentImpl ref={ref}>
+            <VisuallyHidden>
+              <Box role="status">
+                {loading
+                  ? "Loading"
+                  : `${items.length} option${items.length === 1 ? "" : "s"} available`}
+              </Box>
+            </VisuallyHidden>
+
             {loading ? (
               <Box display="flex" justifyContent="center" p="md">
                 <Spinner />
