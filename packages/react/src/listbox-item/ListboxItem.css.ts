701f9a2ab92bc81fef2ad6af66c1b95f212cfad1
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-09 15:18:46 -0500

    update listbox text/bg colors for danger items

--- packages/react/src/listbox-item/ListboxItem.css.ts
+++ packages/react/src/listbox-item/ListboxItem.css.ts
@@ -2,14 +2,17 @@ import { theme } from "@optiaxiom/globals";
 
 import {
   createVar,
+  fallbackVar,
   recipe,
   type RecipeVariants,
   style,
 } from "../vanilla-extract";
 
-const bgColorVar = createVar();
-const bgColorPressedVar = createVar();
-const accentColorVar = createVar();
+const hoveredBgColorVar = createVar();
+const pressedBgColorVar = createVar();
+const hoveredTextColorVar = createVar();
+const pressedTextColorVar = createVar();
+const textColorVar = createVar();
 
 export const item = recipe({
   base: [
@@ -32,14 +35,16 @@ export const item = recipe({
           outline: "2px solid transparent",
         },
         "&:not([data-disabled])": {
-          color: accentColorVar,
+          color: textColorVar,
           cursor: "pointer",
         },
         "&:not([data-disabled])[data-highlighted]": {
-          backgroundColor: bgColorVar,
+          backgroundColor: hoveredBgColorVar,
+          color: fallbackVar(hoveredTextColorVar, textColorVar),
         },
         "&:not([data-disabled])[data-highlighted]:active": {
-          backgroundColor: bgColorPressedVar,
+          backgroundColor: pressedBgColorVar,
+          color: fallbackVar(pressedTextColorVar, textColorVar),
         },
         "&:not([data-disabled])[data-highlighted][data-focus-visible]": {
           outlineOffset: "-2px",
@@ -55,16 +60,18 @@ export const item = recipe({
     intent: {
       danger: style({
         vars: {
-          [accentColorVar]: theme.colors["fg.error"],
-          [bgColorPressedVar]: theme.colors["bg.error.subtle"],
-          [bgColorVar]: theme.colors["bg.error.subtlest"],
+          [hoveredBgColorVar]: theme.colors["bg.error.subtlest"],
+          [hoveredTextColorVar]: theme.colors["fg.error.hovered"],
+          [pressedBgColorVar]: theme.colors["bg.error.subtle"],
+          [pressedTextColorVar]: theme.colors["fg.error.strong"],
+          [textColorVar]: theme.colors["fg.error"],
         },
       }),
       neutral: style({
         vars: {
-          [accentColorVar]: theme.colors["fg.default"],
-          [bgColorPressedVar]: theme.colors["bg.secondary.hovered"],
-          [bgColorVar]: theme.colors["bg.secondary"],
+          [hoveredBgColorVar]: theme.colors["bg.default.hovered"],
+          [pressedBgColorVar]: theme.colors["bg.default.pressed"],
+          [textColorVar]: theme.colors["fg.default"],
         },
       }),
     },
