512857fbc287847b892b32e1c5fda6fa0c395a1c
toha993 <mutasim.toha@optimizely.com>
2024-12-13 03:22:48 +0600

    extract FieldLabel component

---
+++ packages/react/src/field-label/FieldLabel.tsx
@@ -0,0 +1,92 @@
+import * as RadixLabel from "@radix-ui/react-label";
+import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
+import { useId } from "@reach/auto-id";
+import { forwardRef, type ReactNode } from "react";
+
+import { type BoxProps } from "../box";
+import { Button } from "../button";
+import { Flex } from "../flex";
+import { HoverCard } from "../hover-card";
+import { HoverCardContent } from "../hover-card-content";
+import { HoverCardTrigger } from "../hover-card-trigger";
+import { IconCircleQuestion } from "../icons/IconCircleQuestion";
+import { Text } from "../text";
+
+type FieldLabelProps = BoxProps<
+  "div",
+  {
+    info?: ReactNode;
+    inputId?: string;
+    labelId?: string;
+    required?: boolean;
+  }
+>;
+
+export const FieldLabel = forwardRef<HTMLDivElement, FieldLabelProps>(
+  (
+    {
+      children,
+      info,
+      inputId: inputIdProp,
+      labelId: labelIdProp,
+      required,
+      ...props
+    },
+    ref,
+  ) => {
+    const inputId = useId(inputIdProp);
+    const labelId = useId(labelIdProp);
+
+    return (
+      <Flex flexDirection="row" gap="4" ref={ref} {...props}>
+        <Text
+          alignItems="center"
+          asChild
+          color="fg.secondary"
+          display="flex"
+          fontWeight="400"
+          gap="2"
+        >
+          <RadixLabel.Root htmlFor={inputId} id={labelId}>
+            {children}
+
+            {required && (
+              <>
+                <Text
+                  aria-hidden="true"
+                  asChild
+                  color="fg.error"
+                  fontWeight="400"
+                >
+                  <span>*</span>
+                </Text>
+                <VisuallyHidden>Required</VisuallyHidden>
+              </>
+            )}
+          </RadixLabel.Root>
+        </Text>
+
+        {info && (
+          <HoverCard keepOpenOnActivation>
+            <HoverCardTrigger asChild>
+              <Button
+                aria-label="Information hover"
+                border="0"
+                color="fg.secondary"
+                h="2xs"
+                icon={<IconCircleQuestion />}
+                p="0"
+                w="2xs"
+              />
+            </HoverCardTrigger>
+            <HoverCardContent side="top" sideOffset={5}>
+              {info}
+            </HoverCardContent>
+          </HoverCard>
+        )}
+      </Flex>
+    );
+  },
+);
+
+FieldLabel.displayName = "@optiaxiom/react/FieldLabel";
--- packages/react/src/field/Field.tsx
+++ packages/react/src/field/Field.tsx
@@ -1,18 +1,11 @@
-import * as RadixLabel from "@radix-ui/react-label";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import { useId } from "@reach/auto-id";
 import { forwardRef, type ReactNode } from "react";
 
 import { Box, type BoxProps } from "../box";
-import { Button } from "../button";
 import { FieldContext } from "../field-context";
+import { FieldLabel } from "../field-label";
 import { Flex } from "../flex";
-import { HoverCard } from "../hover-card";
-import { HoverCardContent } from "../hover-card-content";
-import { HoverCardTrigger } from "../hover-card-trigger";
-import { IconCircleQuestion } from "../icons/IconCircleQuestion";
 import { IconTriangleExclamation } from "../icons/IconTriangleExclamation";
-import { Text } from "../text";
 import { fallbackSpan } from "../utils";
 
 type FieldProps = BoxProps<
@@ -50,53 +43,14 @@ export const Field = forwardRef<HTMLDivElement, FieldProps>(
     return (
       <Flex flexDirection="column" gap="4" ref={ref} {...props}>
         {label && (
-          <Flex flexDirection="row" gap="4">
-            <Text
-              alignItems="center"
-              asChild
-              color="fg.secondary"
-              display="flex"
-              fontWeight="400"
-              gap="2"
-            >
-              <RadixLabel.Root htmlFor={inputId} id={labelId}>
-                {label}
-
-                {required && (
-                  <>
-                    <Text
-                      aria-hidden="true"
-                      asChild
-                      color="fg.error"
-                      fontWeight="400"
-                    >
-                      <span>*</span>
-                    </Text>
-                    <VisuallyHidden>Required</VisuallyHidden>
-                  </>
-                )}
-              </RadixLabel.Root>
-            </Text>
-
-            {info && (
-              <HoverCard keepOpenOnActivation>
-                <HoverCardTrigger asChild>
-                  <Button
-                    aria-label="Information hover"
-                    border="0"
-                    color="fg.secondary"
-                    h="2xs"
-                    icon={<IconCircleQuestion />}
-                    p="0"
-                    w="2xs"
-                  />
-                </HoverCardTrigger>
-                <HoverCardContent side="top" sideOffset={5}>
-                  {info}
-                </HoverCardContent>
-              </HoverCard>
-            )}
-          </Flex>
+          <FieldLabel
+            info={info}
+            inputId={inputId}
+            labelId={labelId}
+            required={required}
+          >
+            {label}
+          </FieldLabel>
         )}
 
         <FieldContext.Provider
