92c8336d98602d132169754cd254f826c55008af
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-16 13:40:59 -0500

    add basic table docs

---
+++ apps/docs/demos/table/usage/data.ts
@@ -0,0 +1,30 @@
+export const data = [
+  {
+    address: "123 Broad St.",
+    company: "Acme, Inc.",
+    firstName: "Fred",
+    id: "1",
+    lastName: "Jackson",
+  },
+  {
+    address: "123 Broad St.",
+    company: "Acme, Inc.",
+    firstName: "Sara",
+    id: "2",
+    lastName: "James",
+  },
+  {
+    address: "456 Main St.",
+    company: "XYZ, Inc.",
+    firstName: "Ralph",
+    id: "3",
+    lastName: "Jefferson",
+  },
+  {
+    address: "456 Main St.",
+    company: "XYZ, Inc.",
+    firstName: "Nancy",
+    id: "4",
+    lastName: "Jensen",
+  },
+];
