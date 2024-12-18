a1ea58350564484781e17eddc43e5ef78eea4958
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-18 15:04:19 -0500

    remove css patch that is no longer needed

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,42 +1,10 @@
 import type { AppProps } from "next/app";
 
 import { AxiomProvider } from "@optiaxiom/react";
-import { useEffect } from "react";
 
 import "./globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  /**
-   * Manually remove background-color rule from top navbar nextra theme.
-   * For some reason css isn't able to override the color.
-   */
-  useEffect(() => {
-    for (const sheet of document.styleSheets) {
-      for (const layerRule of sheet.cssRules) {
-        if (
-          layerRule instanceof CSSLayerBlockRule &&
-          layerRule.name === "optiaxiom.base"
-        ) {
-          for (const supportsRule of layerRule.cssRules) {
-            if (supportsRule instanceof CSSSupportsRule) {
-              for (const [index, cssRule] of Object.entries(
-                supportsRule.cssRules,
-              )) {
-                if (
-                  cssRule instanceof CSSStyleRule &&
-                  cssRule.selectorText.includes(".nextra-nav-container-blur") &&
-                  cssRule.selectorText.includes("dark")
-                ) {
-                  supportsRule.deleteRule(index as unknown as number);
-                }
-              }
-            }
-          }
-        }
-      }
-    }
-  }, []);
-
   return (
     <AxiomProvider>
       <Component {...pageProps} />
