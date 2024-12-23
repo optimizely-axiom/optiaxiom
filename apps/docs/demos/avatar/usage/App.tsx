ec9ce601df2f4966588cb84052078662793324f0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-23 13:37:50 -0500

    simplify avatar doc example
    and clarify how src and name are the primary props

--- apps/docs/demos/avatar/usage/App.tsx
+++ apps/docs/demos/avatar/usage/App.tsx
@@ -1,32 +1,10 @@
-import { Avatar, Flex } from "@optiaxiom/react";
+import { Avatar } from "@optiaxiom/react";
 
 export function App() {
   return (
-    <Flex flexDirection="row" gap="10">
-      <Avatar
-        name="John Snow"
-        size="3xl"
-        src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
-      >
-        JS
-      </Avatar>
-      <Avatar
-        name="Daenerys Targaryen"
-        size="xl"
-        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
-      >
-        DT
-      </Avatar>
-      <Avatar name="Khal Drogo" size="lg">
-        KD
-      </Avatar>
-      <Avatar name="Jamie Lannister" size="md">
-        JL
-      </Avatar>
-      <Avatar size="sm">+2</Avatar>
-      <Avatar name="Ned Stark" size="xs">
-        NS
-      </Avatar>
-    </Flex>
+    <Avatar
+      name="Jane Doe"
+      src="https://i.pravatar.cc/300?u=jane@doe.example"
+    />
   );
 }
