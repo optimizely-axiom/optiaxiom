e8dd4e075ce08749a12f36be58fb9c0ac234b370
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-26 12:20:12 -0500

    switch Tooltip to a semi virtual component
    since it does not render any dom element directly and only forwards props to
    tooltip trigger/content

--- packages/react/src/tooltip/Tooltip.tsx
+++ packages/react/src/tooltip/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { type ComponentPropsWithRef, forwardRef, type ReactNode } from "react";
+import { type ComponentPropsWithRef, type ReactNode } from "react";
 
 import { type BoxProps } from "../box";
 import { TooltipContent } from "../tooltip-content";
@@ -36,44 +36,37 @@ type TooltipProps = BoxProps<
   }
 >;
 
-export const Tooltip = forwardRef<HTMLButtonElement, TooltipProps>(
-  (
-    {
-      auto,
-      children,
-      content,
-      defaultOpen,
-      delayDuration,
-      disabled,
-      onOpenChange,
-      open,
-      ...props
-    },
-    ref,
-  ) => {
-    const empty = !content && typeof content !== "number";
+export function Tooltip({
+  auto,
+  children,
+  content,
+  defaultOpen,
+  delayDuration,
+  disabled,
+  onOpenChange,
+  open,
+  ...props
+}: TooltipProps) {
+  const empty = !content && typeof content !== "number";
 
-    return (
-      <TooltipRoot
-        auto={auto}
-        defaultOpen={defaultOpen}
-        delayDuration={delayDuration}
-        disableHoverableContent={disabled || empty}
-        onOpenChange={onOpenChange}
-        open={open}
-      >
-        <TooltipTrigger asChild ref={ref}>
-          {children}
-        </TooltipTrigger>
+  return (
+    <TooltipRoot
+      auto={auto}
+      defaultOpen={defaultOpen}
+      delayDuration={delayDuration}
+      disableHoverableContent={disabled || empty}
+      onOpenChange={onOpenChange}
+      open={open}
+    >
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
 
-        {!disabled && !empty && (
-          <TooltipContent maxW="xs" {...props}>
-            {content}
-          </TooltipContent>
-        )}
-      </TooltipRoot>
-    );
-  },
-);
+      {!disabled && !empty && (
+        <TooltipContent maxW="xs" {...props}>
+          {content}
+        </TooltipContent>
+      )}
+    </TooltipRoot>
+  );
+}
 
 Tooltip.displayName = "@optiaxiom/react/Tooltip";
