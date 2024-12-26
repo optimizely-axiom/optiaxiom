65cc406ec81ea7de5a90d20d3b1566e0a0309ed0
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-26 12:09:48 -0500

    merge back autocomplete and select content components
    now that ModalLayer is a separate component
    
    previously we needed to separate them to get the ref/mount event but now
    ModalLayer handles all of that internally

--- packages/react/src/select-content/SelectContent.tsx
+++ packages/react/src/select-content/SelectContent.tsx
@@ -1,3 +1,4 @@
+import { PopperContent } from "@radix-ui/react-popper";
 import { Portal } from "@radix-ui/react-portal";
 import { type ComponentPropsWithoutRef, forwardRef } from "react";
 
@@ -6,25 +7,44 @@ import { MenuListbox } from "../menu-listbox";
 import { ModalLayer } from "../modal-layer";
 import { useSelectContext } from "../select-context";
 import { Spinner } from "../spinner";
-import { SelectContentImpl } from "./SelectContentImpl";
+import { extractSprinkles } from "../sprinkles";
+import * as styles from "./SelectContent.css";
 
 type SelectContentProps = BoxProps<
-  typeof SelectContentImpl,
+  typeof PopperContent,
   Pick<ComponentPropsWithoutRef<typeof MenuListbox>, "minW"> & {
     loading?: boolean;
   }
 >;
 
 export const SelectContent = forwardRef<HTMLDivElement, SelectContentProps>(
-  ({ children, loading, ...props }, ref) => {
-    const { isOpen } = useSelectContext("SelectContent");
+  (
+    {
+      align = "start",
+      children,
+      className,
+      loading,
+      side = "bottom",
+      sideOffset = 5,
+      ...props
+    },
+    ref,
+  ) => {
+    const { restProps, sprinkleProps } = extractSprinkles(props);
+    const { downshift, isOpen } = useSelectContext("SelectContent");
 
     return (
       isOpen && (
         <Portal asChild>
           <ModalLayer asChild>
-            <MenuListbox asChild provider="popper">
-              <SelectContentImpl ref={ref} {...props}>
+            <MenuListbox
+              asChild
+              provider="popper"
+              {...styles.content({}, className)}
+              {...sprinkleProps}
+              {...downshift.getMenuProps({ ref, ...restProps })}
+            >
+              <PopperContent align={align} side={side} sideOffset={sideOffset}>
                 {loading ? (
                   <Box display="flex" justifyContent="center" p="16">
                     <Spinner />
@@ -32,7 +52,7 @@ export const SelectContent = forwardRef<HTMLDivElement, SelectContentProps>(
                 ) : (
                   children
                 )}
-              </SelectContentImpl>
+              </PopperContent>
             </MenuListbox>
           </ModalLayer>
         </Portal>
