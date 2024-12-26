3484302c9982b4e0f8db2765f21842b9a35491e3
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-26 12:44:43 -0500

    render our own code component in docs
    but exclude code that is inside <pre> tags which is typically multi-line
    
    and switch to inline display for code (so works inside <a> tags)
    
    and bump up font-size a bit to 0.9em

--- packages/react/src/code/Code.css.ts
+++ packages/react/src/code/Code.css.ts
@@ -4,12 +4,12 @@ export const code = recipe({
   base: [
     {
       bg: "bg.secondary",
-      display: "inline-block",
+      display: "inline",
       px: "4",
       rounded: "sm",
     },
     style({
-      fontSize: "0.875em",
+      fontSize: "0.9em",
       WebkitFontSmoothing: "auto",
     }),
   ],
