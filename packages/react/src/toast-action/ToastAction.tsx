fe27990d0b03dae7857250ea78f40951c5479f5c
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-27 13:01:21 -0500

    use default props to show in props table in docs

--- packages/react/src/toast-action/ToastAction.tsx
+++ packages/react/src/toast-action/ToastAction.tsx
@@ -7,12 +7,15 @@ import { Separator } from "../separator";
 type ToastProps = ButtonProps<typeof RadixToast.Action>;
 
 export const ToastAction = forwardRef<HTMLButtonElement, ToastProps>(
-  ({ altText, children, ...props }, ref) => {
+  (
+    { altText, appearance = "inverse", children, size = "sm", ...props },
+    ref,
+  ) => {
     return (
       <>
         <Separator alignSelf="center" h="sm" ml="12" orientation="vertical" />
 
-        <Button appearance="inverse" asChild size="sm" {...props}>
+        <Button appearance={appearance} asChild size={size} {...props}>
           <RadixToast.Action altText={altText} ref={ref}>
             {children}
           </RadixToast.Action>
