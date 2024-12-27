475af6910f8bc3f448ee9aca15f6b94eb9ba4bd7
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-27 14:53:23 -0500

    wrap highlight in box
    to prevent children from inheriting flex or other parent layouts

--- packages/react/src/highlight/Highlight.tsx
+++ packages/react/src/highlight/Highlight.tsx
@@ -1,4 +1,4 @@
-import { Fragment, type ReactElement, type ReactNode } from "react";
+import { forwardRef, Fragment, type ReactElement, type ReactNode } from "react";
 
 import { Box, type BoxProps } from "../box";
 import * as styles from "./Highlight.css";
@@ -13,29 +13,26 @@ type HighlightProps = BoxProps<
   }
 >;
 
-export function Highlight({
-  children = (chunk) => chunk,
-  content,
-  query,
-  ...props
-}: HighlightProps) {
-  const chunks = useHighlightedChunks(content || "", query);
+export const Highlight = forwardRef<HTMLDivElement, HighlightProps>(
+  ({ children = (chunk) => chunk, content, query, ...props }, ref) => {
+    const chunks = useHighlightedChunks(content || "", query);
 
-  return (
-    <Box {...props}>
-      {chunks.map(({ chunk, highlighted }, index) => (
-        <Fragment key={index}>
-          {highlighted
-            ? children(
-                <Box asChild {...styles.mark()}>
-                  <mark>{chunk}</mark>
-                </Box>,
-              )
-            : chunk}
-        </Fragment>
-      ))}
-    </Box>
-  );
-}
+    return (
+      <Box ref={ref} {...props}>
+        {chunks.map(({ chunk, highlighted }, index) => (
+          <Fragment key={index}>
+            {highlighted
+              ? children(
+                  <Box asChild {...styles.mark()}>
+                    <mark>{chunk}</mark>
+                  </Box>,
+                )
+              : chunk}
+          </Fragment>
+        ))}
+      </Box>
+    );
+  },
+);
 
 Highlight.displayName = "@optiaxiom/react/Highlight";
