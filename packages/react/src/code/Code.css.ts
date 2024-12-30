113bffd27d92f279f1c06da328939243ab190698
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-30 16:25:36 -0500

    use a code style that stands out more
    instead of using subtle bg color and padding we use font-weight and surround
    text with backticks to make code snippets stand out more.
    
    but these do not look good inside links so we remove them from existing links
    and some headings

--- packages/react/src/code/Code.css.ts
+++ packages/react/src/code/Code.css.ts
@@ -3,16 +3,18 @@ import { recipe, style } from "../vanilla-extract";
 export const code = recipe({
   base: [
     {
-      bg: "bg.secondary",
-      display: "inline",
-      rounded: "sm",
+      fontWeight: "600",
     },
     style({
       fontSize: "0.9em",
       fontVariantLigatures: "none",
-      paddingBlock: "0.1em",
-      paddingInline: "0.25em",
       WebkitFontSmoothing: "auto",
+
+      selectors: {
+        "&::after, &::before": {
+          content: "`",
+        },
+      },
     }),
   ],
 });
