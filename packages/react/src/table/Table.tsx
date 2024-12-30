bbb4e4ffae4656ef170331f1e8aefb37d6882d48
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-30 18:02:25 -0500

    set default table layout mode to auto

--- packages/react/src/table/Table.tsx
+++ packages/react/src/table/Table.tsx
@@ -6,7 +6,7 @@ import * as styles from "./Table.css";
 type TableProps = BoxProps<"table", styles.TableVariants>;
 
 export const Table = forwardRef<HTMLTableElement, TableProps>(
-  ({ children, className, layout, ...props }, ref) => (
+  ({ children, className, layout = "auto", ...props }, ref) => (
     <Box {...styles.wrapper({}, className)} ref={ref} {...props}>
       <Box asChild {...styles.table({ layout })}>
         <table ref={ref}>{children}</table>
