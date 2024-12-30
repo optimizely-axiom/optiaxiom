a3079ecd500ec55236d402eadfd2526ddbf36d09
Rafi Adnan <rafi.adnan@optimizely.com>
2024-12-30 15:15:54 -0500

    document how to use alert dialog inside dropdown menu

---
+++ apps/docs/demos/dropdown-menu/alert-dialog-usage/App.tsx
@@ -0,0 +1,49 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogBody,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@optiaxiom/react";
+import { IconPencil, IconTrash } from "@tabler/icons-react";
+
+export function App() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+
+      <DropdownMenuContent>
+        <DropdownMenuItem icon={<IconPencil />}>Edit</DropdownMenuItem>
+        <AlertDialog>
+          <AlertDialogTrigger asChild>
+            <DropdownMenuItem
+              icon={<IconTrash />}
+              intent="danger"
+              onSelect={(event) => event.preventDefault()}
+            >
+              Delete
+            </DropdownMenuItem>
+          </AlertDialogTrigger>
+
+          <AlertDialogContent>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogBody>
+              The task and all content will be deleted.
+            </AlertDialogBody>
+            <AlertDialogFooter>
+              <AlertDialogCancel />
+              <AlertDialogAction>Yes, delete</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
