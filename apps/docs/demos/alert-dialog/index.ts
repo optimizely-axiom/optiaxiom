9128ad13f94dcaf326da2f6de60a25c1a0260fc2
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-06 13:46:46 -0500

    add separate controlled dialog demos
    so first usage/demo is the simplest form

--- apps/docs/demos/alert-dialog/index.ts
+++ apps/docs/demos/alert-dialog/index.ts
@@ -1,5 +1,6 @@
 export { App as AsyncUsage } from "./async-usage/App";
 export { App as ConfirmationUsage } from "./confirmation-usage/App";
+export { App as ControlledUsage } from "./controlled-usage/App";
 export { App as SizeUsage } from "./size-usage/App";
 export { App as TriggerUsage } from "./trigger-usage/App";
 export { App as Usage } from "./usage/App";
