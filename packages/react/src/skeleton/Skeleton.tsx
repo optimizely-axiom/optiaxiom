de12748e0f749739aa56ef2631522e5c7ae6267a
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-06 14:01:31 -0500

    actually use size prop when needed
    instead of mangling h prop to do two things
    
    or just wrap avatar with skeleton when needing to show avatar loading status

--- packages/react/src/skeleton/Skeleton.tsx
+++ packages/react/src/skeleton/Skeleton.tsx
@@ -1,8 +1,6 @@
 import { Slot } from "@radix-ui/react-slot";
 import { forwardRef, type ReactElement } from "react";
 
-import type { Sprinkles } from "../sprinkles";
-
 import { Box, type BoxProps } from "../box";
 import * as styles from "./Skeleton.css";
 
@@ -11,27 +9,18 @@ type SkeletonProps = BoxProps<
   {
     children?: ReactElement;
     /**
-     * Render skeleton as a circle if `true`. Will set the width to be the same as the height.
+     * Render skeleton as a circle if `true`.
      */
     circle?: boolean;
-    /**
-     * Control the height of the skeleton - or both width and height if `circle` is also set to true.
-     */
-    h?: Sprinkles["h"];
   }
 >;
 
 export const Skeleton = forwardRef<HTMLSpanElement, SkeletonProps>(
-  (
-    { asChild = true, children, circle, className, h, rounded, w, ...props },
-    ref,
-  ) => {
+  ({ asChild = true, children, circle, className, rounded, ...props }, ref) => {
     return (
       <Box
         asChild={asChild}
-        h={h}
         rounded={circle || rounded === "full" ? "full" : (rounded ?? "sm")}
-        w={w ?? (circle || rounded === "full" ? h : undefined)}
         {...styles.skeleton({}, className)}
         {...props}
       >
