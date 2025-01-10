f50650cd2dadbe21d704aa6ce764e8e1fb18f166
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-10 16:43:43 -0500

    fix dialog cancel button default appearance
    and set it to subtle

---
+++ .changeset/tall-nails-chew.md
@@ -0,0 +1,5 @@
+---
+"@optiaxiom/react": patch
+---
+
+fix dialog cancel button default appearance
--- packages/react/src/dialog-close/DialogClose.tsx
+++ packages/react/src/dialog-close/DialogClose.tsx
@@ -6,10 +6,19 @@ import { Button, type ButtonProps } from "../button";
 type DialogCloseProps = ButtonProps<typeof RadixDialog.Close>;
 
 export const DialogClose = forwardRef<HTMLButtonElement, DialogCloseProps>(
-  ({ asChild, children, size = "lg", ...props }, ref) => {
+  (
+    { appearance = "subtle", asChild, children, size = "lg", ...props },
+    ref,
+  ) => {
     return (
       <RadixDialog.Close asChild ref={ref} {...props}>
-        {asChild ? children : <Button size={size}>{children}</Button>}
+        {asChild ? (
+          children
+        ) : (
+          <Button appearance={appearance} size={size}>
+            {children}
+          </Button>
+        )}
       </RadixDialog.Close>
     );
   },
