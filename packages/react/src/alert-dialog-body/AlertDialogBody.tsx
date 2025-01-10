0ce4ad337567c525039bbed3c73053137644a47c
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-10 15:57:28 -0500

    inline sprinkles in AlertDialogBody
    since we're not using complex styles or selectors

--- packages/react/src/alert-dialog-body/AlertDialogBody.tsx
+++ packages/react/src/alert-dialog-body/AlertDialogBody.tsx
@@ -2,16 +2,24 @@ import * as RadixAlertDialog from "@radix-ui/react-alert-dialog";
 import { forwardRef } from "react";
 
 import { Box, type BoxProps } from "../box";
-import * as styles from "./AlertDialogBody.css";
 
 type AlertDialogBodyProps = BoxProps<typeof RadixAlertDialog.Description>;
 
 export const AlertDialogBody = forwardRef<
   HTMLParagraphElement,
   AlertDialogBodyProps
->(({ children, className, ...props }, ref) => {
+>(({ children, ...props }, ref) => {
   return (
-    <Box asChild {...styles.body({}, className)} ref={ref} {...props}>
+    <Box
+      asChild
+      flex="1"
+      fontSize="md"
+      overflow="auto"
+      px="24"
+      py="16"
+      ref={ref}
+      {...props}
+    >
       <RadixAlertDialog.Description>{children}</RadixAlertDialog.Description>
     </Box>
   );
