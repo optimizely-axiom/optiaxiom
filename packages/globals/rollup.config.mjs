c2750875decd182672e7dd4c052f0c0acba78052
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-17 15:17:03 -0500

    fix chunking of client and server chunks for localdev

--- packages/globals/rollup.config.mjs
+++ packages/globals/rollup.config.mjs
@@ -47,7 +47,7 @@ export default defineConfig([
               if (bannerFilter(id)) {
                 return "client";
               }
-              return null;
+              return "server";
             },
       preserveModules: env === "production",
     },
