c377dd10f9c53101ffc5718de90ba4d9f0d46cbb
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-23 16:36:23 -0500

    refactor Scale component
    - show actual theme variable/css custom property when possible (with comment of
      token/resolved value)
    - remove pixels and previews columns
    - colorize styles to feel like css output

--- apps/docs/components/scale/ScaleValue.tsx
+++ apps/docs/components/scale/ScaleValue.tsx
@@ -7,25 +7,21 @@ import { useEffect, useState } from "react";
 
 import { Td } from "../table";
 
-const px = (_name: string, rem: string) =>
+const px = (rem: string) =>
   rem.endsWith("rem")
     ? `${parseFloat((parseFloat(rem.slice(0, -3)) * 16).toFixed(0))}px`
     : rem;
 
 export const ScaleValue = ({
-  hidePixels,
-  hidePreview,
   mode,
   name,
-  pixelTemplate,
+  themeKey,
   type,
   value,
 }: {
-  hidePixels?: boolean;
-  hidePreview?: boolean;
   mode?: "color";
   name: string;
-  pixelTemplate?: string;
+  themeKey?: keyof typeof theme;
   type: "selector" | "value";
   value: Record<string, string> | string;
 }) => {
@@ -33,92 +29,125 @@ export const ScaleValue = ({
   const [renderedTheme, setRenderedTheme] = useState("");
   useEffect(() => setRenderedTheme(resolvedTheme), [resolvedTheme]);
 
-  const [size, setSize] = useState(type === "value" ? value : undefined);
+  const [resolvedValue, setResolvedValue] = useState(
+    type === "value" ? value : undefined,
+  );
   useEffect(() => {
     if (type === "selector" && typeof value === "string") {
-      setSize(getStyleValues(value));
+      setResolvedValue(getStyleValues(value));
     }
   }, [renderedTheme, type, value]);
 
   return (
-    size !== undefined && (
+    resolvedValue !== undefined && (
       <>
         <Td
           display={
-            mode === "color" && isColorType(size)
+            mode === "color" && isColorType(resolvedValue)
               ? ["none", "table-cell"]
               : undefined
           }
-          valign={mode === "color" && isColorType(size) ? "middle" : undefined}
+          valign={
+            mode === "color" && isColorType(resolvedValue)
+              ? "middle"
+              : undefined
+          }
           whiteSpace="nowrap"
         >
-          <Flex gap="8">
-            {(typeof size === "object"
-              ? Object.entries(size).map(([key, value]) => `${key}: ${value}`)
-              : [size]
-            ).map((value) => (
-              <Text
-                fontFamily="mono"
-                fontSize="sm"
-                fontWeight="500"
-                key={value}
-                style={{ color: "var(--shiki-token-function)" }}
-              >
-                {value}
+          <Flex gap="0">
+            {(typeof resolvedValue === "object"
+              ? Object.entries(resolvedValue)
+              : resolvedValue
+                ? [[null, resolvedValue]]
+                : []
+            ).map(([key, value]) => (
+              <Text asChild fontFamily="mono" key={key ?? value}>
+                <code>
+                  {key && (
+                    <>
+                      <span
+                        style={{ color: "var(--shiki-token-css-constant)" }}
+                      >
+                        {key}
+                      </span>
+                      <span style={{ color: "var(--shiki-token-keyword)" }}>
+                        :{" "}
+                      </span>
+                    </>
+                  )}
+                  <span style={{ color: "var(--shiki-token-constant)" }}>
+                    {addShiki(
+                      themeKey && name in theme[themeKey]
+                        ? key &&
+                          typeof theme[themeKey][name as never] === "object" &&
+                          toCamelCase(key) in theme[themeKey][name as never]
+                          ? theme[themeKey][name as never][toCamelCase(key)]
+                          : theme[themeKey][name as never]
+                        : value,
+                    )}
+                  </span>
+                  <span style={{ color: "var(--shiki-color-text)" }}>
+                    {key && ";"}
+                  </span>
+                  {themeKey &&
+                  name in theme[themeKey] &&
+                  !(type === "value" && value?.startsWith("var(")) ? (
+                    <span style={{ color: "var(--shiki-token-comment)" }}>
+                      {" /* "}
+                      {value}
+                      {value?.endsWith("rem") && <> ({px(value)})</>}
+                      {" */"}
+                    </span>
+                  ) : value?.endsWith("rem") ? (
+                    <span style={{ color: "var(--shiki-token-comment)" }}>
+                      {" /* "}
+                      {px(value)}
+                      {" */"}
+                    </span>
+                  ) : null}
+                </code>
               </Text>
             ))}
           </Flex>
         </Td>
-        {!hidePixels && (
-          <Td whiteSpace="nowrap">
-            <Flex gap="8">
-              {(typeof size === "object"
-                ? Object.entries<string>(size).map(
-                    ([key, value]) =>
-                      `${key}: ${transform(pixelTemplate, name, value, key)}`,
-                  )
-                : [transform(pixelTemplate, name, size, "")]
-              ).map((value) => (
-                <Text
-                  fontFamily="mono"
-                  fontSize="sm"
-                  fontWeight="500"
-                  key={value}
-                  style={{ color: "var(--shiki-token-function)" }}
-                >
-                  {value}
-                </Text>
-              ))}
-            </Flex>
-          </Td>
-        )}
-        {!hidePreview && typeof size !== "object" && (
-          <Td display={isColorType(size) ? undefined : ["none", "table-cell"]}>
-            {isColorType(size) ? (
-              <Box
-                rounded="sm"
-                style={{
-                  aspectRatio: 100 / 60,
-                  backgroundImage: [
-                    `linear-gradient(${size}, ${size})`,
-                    `linear-gradient(${theme.colors["bg.default"]}, ${theme.colors["bg.default"]})`,
-                    `repeating-conic-gradient(color-mix(in srgb, ${theme.colors["bg.default.inverse"]} 5%, transparent) 0% 25%, transparent 0% 50%)`,
-                  ].join(", "),
-                  backgroundRepeat: "no-repeat, no-repeat, repeat",
-                  backgroundSize: "100% 100%, 50% 100%, 16px 16px",
-                  boxShadow: `inset 0 0 0 1px color-mix(in srgb, ${theme.colors["bg.default.inverse"]} 20%, transparent)`,
-                }}
-                w="56"
-              />
-            ) : (
-              <Box bg="bg.avatar.purple" h="2xs" style={{ width: size }} />
-            )}
+        {mode === "color" && typeof resolvedValue !== "object" && (
+          <Td display={["none", "table-cell"]}>
+            <Box
+              rounded="sm"
+              style={{
+                aspectRatio: 100 / 60,
+                backgroundImage: [
+                  `linear-gradient(${resolvedValue}, ${resolvedValue})`,
+                  `linear-gradient(${theme.colors["bg.default"]}, ${theme.colors["bg.default"]})`,
+                  `repeating-conic-gradient(color-mix(in srgb, ${theme.colors["bg.default.inverse"]} 5%, transparent) 0% 25%, transparent 0% 50%)`,
+                ].join(", "),
+                backgroundRepeat: "no-repeat, no-repeat, repeat",
+                backgroundSize: "100% 100%, 50% 100%, 16px 16px",
+                boxShadow: `inset 0 0 0 1px color-mix(in srgb, ${theme.colors["bg.default.inverse"]} 20%, transparent)`,
+              }}
+              w="56"
+            />
           </Td>
         )}
       </>
     )
   );
 };
+
+const addShiki = (str: null | string) =>
+  str?.split(/(light-dark|px|rem|var|\(|\))/).map((part, index) =>
+    part === "light-dark" || part === "var" || part === "(" || part === ")" ? (
+      <span key={index} style={{ color: "var(--shiki-token-css-function)" }}>
+        {part}
+      </span>
+    ) : part === "px" || part === "rem" ? (
+      <span key={index} style={{ color: "var(--shiki-token-css-keyword)" }}>
+        {part}
+      </span>
+    ) : (
+      part
+    ),
+  );
 
 function getCssRuleText(
   node: CSSRule | CSSStyleSheet | Document,
@@ -148,42 +177,47 @@ function getCssRuleText(
 const getStyleValues = (selector: string) => {
   const style = selector.startsWith("var(")
     ? `property: ${selector}`
-    : getCssRuleText(document, `.${cssesc(selector, { isIdentifier: true })}`);
+    : selector
+        .split(" ")
+        .map((part) =>
+          getCssRuleText(document, `.${cssesc(part, { isIdentifier: true })}`),
+        )
+        .join("\n");
   if (!style) {
     throw new Error("Could not parse style for selector");
   }
 
-  const styles = style
-    .split(";")
-    .filter((line) => line)
-    .map((line) => {
-      const [key, ...rest] = line.split(": ");
-      const value = rest.join(": ");
-      if (value.startsWith("var(")) {
-        const computedValue = window
-          .getComputedStyle(document.body)
-          .getPropertyValue(value.slice("var(".length, -")".length));
-        return [key, computedValue];
-      }
-      return [key, value];
-    });
-  return styles.length === 1 ? styles[0][1] : Object.fromEntries(styles);
+  const styles = Object.fromEntries(
+    style
+      .split(/;\s?/)
+      .filter((line) => line)
+      .map((line) => {
+        const [key, ...rest] = line.split(": ");
+        const value = rest.join(": ");
+        if (value.startsWith("var(")) {
+          const computedValue = window
+            .getComputedStyle(document.body)
+            .getPropertyValue(value.slice("var(".length, -")".length));
+          return [key, computedValue];
+        }
+        return [key, value];
+      }),
+  );
+
+  if ("border-top-width" in styles) {
+    return { border: styles["border-top-width"] };
+  } else if ("margin-top" in styles) {
+    return { margin: styles["margin-top"] };
+  } else if ("padding-top" in styles) {
+    return { padding: styles["padding-top"] };
+  }
+
+  return selector.startsWith("var(") ? styles["property"] : styles;
 };
 
 const isColorType = (value: unknown) =>
   typeof value === "string" &&
   (value.startsWith("#") || value.startsWith("light-dark("));
 
-const transform = (
-  template: string | undefined,
-  name: string,
-  value: string,
-  key: string,
-) => {
-  return template
-    ? template
-        .replace("{name}", name)
-        .replace("{value}", value)
-        .replace("{key}", key)
-    : px(name, value);
-};
+const toCamelCase = <T extends null | string>(str: T) =>
+  str?.replace(/-(\w)/g, (_, c) => c.toUpperCase()) as T;
