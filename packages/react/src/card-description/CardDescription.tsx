ccbdaf8ddbf515b3df60a548a38c376b58fe3e52
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-27 17:12:52 -0500

    misc cleanup
    - add client directive
    - fix ref type
    - inline sprinkle only props

--- packages/react/src/card-description/CardDescription.tsx
+++ packages/react/src/card-description/CardDescription.tsx
@@ -2,29 +2,29 @@ import { type ComponentPropsWithRef, forwardRef } from "react";
 
 import { useCardContext } from "../card-context";
 import { Text } from "../text";
-import { Tooltip } from "../tooltip";
-import * as styles from "./CardDescription.css";
 
 type CardDescriptionProps = ComponentPropsWithRef<typeof Text>;
 
-export const CardDescription = forwardRef<HTMLDivElement, CardDescriptionProps>(
-  ({ children, className, ...props }, ref) => {
-    const { descriptionId } = useCardContext("CardDescription");
+export const CardDescription = forwardRef<
+  HTMLParagraphElement,
+  CardDescriptionProps
+>(({ children, ...props }, ref) => {
+  const { descriptionId } = useCardContext("CardDescription");
 
-    return (
-      <Tooltip auto content={children}>
-        <Text
-          id={descriptionId}
-          ref={ref}
-          truncate
-          {...styles.description({}, className)}
-          {...props}
-        >
-          {children}
-        </Text>
-      </Tooltip>
-    );
-  },
-);
+  return (
+    <Text
+      color="fg.tertiary"
+      flex="none"
+      fontSize="sm"
+      id={descriptionId}
+      ref={ref}
+      truncate
+      w="full"
+      {...props}
+    >
+      {children}
+    </Text>
+  );
+});
 
 CardDescription.displayName = "@optiaxiom/react/CardDescription";
--- packages/react/src/card-title/CardTitle.tsx
+++ packages/react/src/card-title/CardTitle.tsx
@@ -1,23 +1,29 @@
+import { Slot } from "@radix-ui/react-slot";
 import { forwardRef } from "react";
 
-import { type BoxProps } from "../box";
 import { useCardContext } from "../card-context";
-import { Flex } from "../flex";
-import { Heading } from "../heading";
-import * as styles from "./CardTitle.css";
+import { Heading, type HeadingProps } from "../heading";
 
-type CardTitleProps = BoxProps<"div">;
+type CardTitleProps = HeadingProps<"h2">;
 
 export const CardTitle = forwardRef<HTMLDivElement, CardTitleProps>(
-  ({ children, className, ...props }, ref) => {
+  ({ asChild, children, ...props }, ref) => {
+    const Comp = asChild ? Slot : "h2";
     const { labelId } = useCardContext("CardTitle");
 
     return (
-      <Flex id={labelId} ref={ref} {...styles.title({}, className)} {...props}>
-        <Heading fontWeight="500" level="6">
-          {children}
-        </Heading>
-      </Flex>
+      <Heading
+        asChild
+        fontWeight="500"
+        id={labelId}
+        level="6"
+        pt="12"
+        ref={ref}
+        w="full"
+        {...props}
+      >
+        <Comp>{children}</Comp>
+      </Heading>
     );
   },
 );
