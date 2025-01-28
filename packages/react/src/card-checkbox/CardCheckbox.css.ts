79f4315ff6f88ee45c9df08586c25677434351dd
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-28 13:17:37 -0500

    hide card checkbox by default
    and show when hovering or interacting with card

--- packages/react/src/card-checkbox/CardCheckbox.css.ts
+++ packages/react/src/card-checkbox/CardCheckbox.css.ts
@@ -1,5 +1,6 @@
 import { theme } from "@optiaxiom/globals";
 
+import * as styles from "../card/Card.css";
 import { recipe, style } from "../vanilla-extract";
 
 const marker = style({});
@@ -9,12 +10,27 @@ export const root = recipe({
     marker,
     {
       rounded: "inherit",
+      transition: "opacity",
     },
     style({
       inset: "0",
+      opacity: "0",
       position: "absolute",
 
+      "@media": {
+        "(hover: hover)": {
+          selectors: {
+            [`${styles.className}:hover &`]: {
+              opacity: "1",
+            },
+          },
+        },
+      },
+
       selectors: {
+        "&:has(:checked, :focus-visible)": {
+          opacity: "1",
+        },
         "&:has(:focus-visible)": {
           outline: `2px solid ${theme.colors["border.focus"]}`,
           outlineOffset: "1px",
--- packages/react/src/card/Card.css.ts
+++ packages/react/src/card/Card.css.ts
@@ -3,8 +3,11 @@ import { theme } from "@optiaxiom/globals";
 import * as styles from "../link/Link.css";
 import { recipe, style } from "../vanilla-extract";
 
+export const className = style({});
+
 export const card = recipe({
   base: [
+    className,
     {
       border: "1",
       color: "fg.default",
