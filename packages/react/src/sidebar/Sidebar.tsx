0c1c29dcb71060f38b95ebc9f555f19200f20fa1
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-29 13:49:53 -0500

    stretch sidebar children
    rather than applying height on each navbar manually

--- packages/react/src/sidebar/Sidebar.tsx
+++ packages/react/src/sidebar/Sidebar.tsx
@@ -1,59 +1,74 @@
 import { useControllableState } from "@radix-ui/react-use-controllable-state";
-import { type ReactNode, useEffect, useRef, useState } from "react";
+import { forwardRef, useEffect, useRef, useState } from "react";
 
-import { Flex } from "../flex";
+import { Box, type BoxProps } from "../box";
 import { SidebarContextProvider } from "../sidebar-context";
 
-type SidebarProps = {
-  children?: ReactNode;
-  /**
-   * The initial expanded state in uncontrolled mode.
-   */
-  defaultExpanded?: boolean;
-  /**
-   * The expanded state in controlled mode.
-   */
-  expanded?: boolean;
-  /**
-   * Handler that is called when the expanded state changes.
-   */
-  onExpandedChange?: (expanded: boolean) => void;
-};
+type SidebarProps = BoxProps<
+  "div",
+  {
+    /**
+     * The initial expanded state in uncontrolled mode.
+     */
+    defaultExpanded?: boolean;
+    /**
+     * The expanded state in controlled mode.
+     */
+    expanded?: boolean;
+    /**
+     * Handler that is called when the expanded state changes.
+     */
+    onExpandedChange?: (expanded: boolean) => void;
+  }
+>;
 
-export const Sidebar = ({
-  children,
-  defaultExpanded,
-  expanded: expandedProp,
-  onExpandedChange,
-}: SidebarProps) => {
-  const [expanded, setExpanded] = useControllableState({
-    defaultProp: defaultExpanded,
-    onChange: onExpandedChange,
-    prop: expandedProp,
-  });
+export const Sidebar = forwardRef<HTMLDivElement, SidebarProps>(
+  (
+    {
+      children,
+      defaultExpanded,
+      expanded: expandedProp,
+      onExpandedChange,
+      ...props
+    },
+    ref,
+  ) => {
+    const [expanded, setExpanded] = useControllableState({
+      defaultProp: defaultExpanded,
+      onChange: onExpandedChange,
+      prop: expandedProp,
+    });
 
-  const [animations, setAnimations] = useState(false);
-  const timerRef = useRef<number | undefined>();
-  useEffect(() => {
-    timerRef.current = window.setTimeout(() => setAnimations(false), 300);
-  }, [animations]);
+    const [animations, setAnimations] = useState(false);
+    const timerRef = useRef<number | undefined>();
+    useEffect(() => {
+      timerRef.current = window.setTimeout(() => setAnimations(false), 300);
+    }, [animations]);
 
-  return (
-    <SidebarContextProvider
-      animations={animations}
-      expanded={expanded}
-      onExpandedChange={(flag) => {
-        window.clearTimeout(timerRef.current);
-        setAnimations(true);
+    return (
+      <SidebarContextProvider
+        animations={animations}
+        expanded={expanded}
+        onExpandedChange={(flag) => {
+          window.clearTimeout(timerRef.current);
+          setAnimations(true);
 
-        setExpanded(flag);
-      }}
-    >
-      <Flex flexDirection="row" gap="0">
-        {children}
-      </Flex>
-    </SidebarContextProvider>
-  );
-};
+          setExpanded(flag);
+        }}
+      >
+        <Box
+          alignItems="stretch"
+          display="flex"
+          gap="0"
+          h="full"
+          ref={ref}
+          {...props}
+        >
+          {children}
+        </Box>
+      </SidebarContextProvider>
+    );
+  },
+);
 
 Sidebar.displayName = "@optiaxiom/react/Sidebar";
