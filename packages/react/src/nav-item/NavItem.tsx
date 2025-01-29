837560cd85387ea123c68f6c39c7e47eec0ea84d
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-29 16:38:05 -0500

    define nav item styles instead of re-using button
    since nav item has different specs from button and also makes it easier to
    animate

--- packages/react/src/nav-item/NavItem.tsx
+++ packages/react/src/nav-item/NavItem.tsx
@@ -1,58 +1,90 @@
-import { forwardRef } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { forwardRef, type ReactNode } from "react";
 
-import { Box } from "../box";
-import { Button, type ButtonProps } from "../button";
+import { Box, type BoxProps, extractBoxProps } from "../box";
 import { Flex } from "../flex";
+import { Icon } from "../icon";
 import { useSidebarContext } from "../sidebar-context";
+import { Text } from "../text";
 import { Tooltip } from "../tooltip";
-import { Transition } from "../transition";
-import { decorateChildren } from "../utils";
+import { decorateChildren, fallbackSpan } from "../utils";
 import * as styles from "./NavItem.css";
 
-export type NavItemProps = ButtonProps<
-  typeof Button,
+export type NavItemProps = BoxProps<
+  "button",
   {
     active?: boolean;
+    /**
+     * Display content inside the button after `children`.
+     */
+    addonAfter?: ReactNode;
     "aria-label": string;
+    /**
+     * Display an icon before button content.
+     */
+    icon?: ReactNode;
   }
 >;
 
 export const NavItem = forwardRef<HTMLButtonElement, NavItemProps>(
-  ({ active, addonAfter, asChild, children, className, ...props }, ref) => {
-    const { animations, expanded } = useSidebarContext("NavItem");
+  (
+    {
+      active,
+      addonAfter,
+      asChild,
+      children,
+      className,
+      disabled,
+      icon,
+      ...props
+    },
+    ref,
+  ) => {
+    const Comp = asChild ? Slot : "button";
+    const { boxProps, restProps } = extractBoxProps(props);
+
+    const { expanded } = useSidebarContext("NavItem");
 
     let tooltip = null;
     children = decorateChildren({ asChild, children }, (children) => {
       tooltip = children;
       return (
-        expanded && (
-          <Transition skipAnimations={!animations}>
-            <Box whiteSpace="nowrap">{children}</Box>
-          </Transition>
-        )
+        <>
+          <Flex flex="none" size="lg">
+            <Icon asChild>{icon}</Icon>
+          </Flex>
+
+          <Text truncate {...styles.label({ expanded: Boolean(expanded) })}>
+            {children}
+          </Text>
+
+          {addonAfter && expanded && (
+            <Box asChild display="flex" flex="none">
+              {fallbackSpan(addonAfter)}
+            </Box>
+          )}
+        </>
       );
     });
 
     return (
       <Flex role="listitem">
         <Tooltip
-          content={!expanded && (props["aria-label"] || tooltip)}
+          content={props["aria-label"] || tooltip}
+          disabled={expanded}
           side="right"
         >
-          <Button
-            addonAfter={expanded && addonAfter}
-            appearance="subtle"
-            asChild={asChild}
+          <Box
+            asChild
+            data-disabled={disabled ? "" : undefined}
             data-state={active ? "active" : undefined}
-            justifyContent={expanded ? "start" : "center"}
-            ref={ref}
-            size="lg"
-            transition={animations ? "colors" : undefined}
             {...styles.item({}, className)}
-            {...props}
+            {...boxProps}
           >
-            {children}
-          </Button>
+            <Comp disabled={disabled} ref={ref} {...restProps}>
+              {children}
+            </Comp>
+          </Box>
         </Tooltip>
       </Flex>
     );
