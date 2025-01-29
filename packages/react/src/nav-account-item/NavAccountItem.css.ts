13ed316b44fb62ad7a49da5d4bd1ca18c49bb8d6
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-29 16:44:22 -0500

    directly use css transition
    instead of component to simplify logic and remove need for skipAnimations

---
+++ packages/react/src/nav-account-item/NavAccountItem.css.ts
@@ -0,0 +1,22 @@
+import { recipe, style } from "../vanilla-extract";
+
+export const item = recipe({
+  base: {
+    flex: "1",
+    flexDirection: "row",
+    gap: "8",
+    overflowX: "hidden",
+    transition: "opacity",
+  },
+
+  variants: {
+    expanded: {
+      false: style({
+        opacity: "0",
+      }),
+      true: style({
+        opacity: "1",
+      }),
+    },
+  },
+});
