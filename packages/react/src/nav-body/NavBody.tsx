607f6d81a622058366b587ca804adcfca1de7f42
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-30 11:27:35 -0500

    add stable gutter to nav body
    since we will typically collapse/expand groups within navbar which will cause
    layout shift if scrollbar appears/disappears

--- packages/react/src/nav-body/NavBody.tsx
+++ packages/react/src/nav-body/NavBody.tsx
@@ -2,24 +2,14 @@ import { forwardRef } from "react";
 
 import { type BoxProps } from "../box";
 import { Flex } from "../flex";
+import * as styles from "./NavBody.css";
 
 type NavBodyProps = BoxProps<"div">;
 
 export const NavBody = forwardRef<HTMLDivElement, NavBodyProps>(
-  ({ children, ...props }, ref) => {
+  ({ children, className, ...props }, ref) => {
     return (
-      <Flex
-        flex="1"
-        gap="4"
-        justifyContent="start"
-        overflowX="hidden"
-        overflowY="auto"
-        py="8"
-        ref={ref}
-        role="list"
-        w="full"
-        {...props}
-      >
+      <Flex ref={ref} role="list" {...styles.body({}, className)} {...props}>
         {children}
       </Flex>
     );
