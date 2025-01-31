3d6dbee6cbfc1df6b1ae3be45b240fead5455485
Rafi Adnan <rafi.adnan@optimizely.com>
2025-01-31 15:10:03 -0500

    add missing a11y addon annotations to vitest setup

--- apps/storybook/.storybook/vitest.setup.ts
+++ apps/storybook/.storybook/vitest.setup.ts
@@ -1,8 +1,12 @@
+import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import { setProjectAnnotations } from "@storybook/react";
 import { beforeAll } from "vitest";
 
 import * as previewAnnotations from "./preview";
 
-const annotations = setProjectAnnotations([previewAnnotations]);
+const annotations = setProjectAnnotations([
+  previewAnnotations,
+  a11yAddonAnnotations,
+]);
 
 beforeAll(annotations.beforeAll);
