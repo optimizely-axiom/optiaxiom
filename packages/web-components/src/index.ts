1ff1b4b37ed9b5ba4ae0c54574260a57300e942b
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-03 16:32:56 -0500

    move html factory to separate file

--- packages/web-components/src/index.ts
+++ packages/web-components/src/index.ts
@@ -1,57 +1,8 @@
+import { factory } from "./factory";
 import { mapping } from "./mapping";
 
 for (const [name, component] of Object.entries(mapping)) {
   if (!customElements.get(name)) {
-    customElements.define(
-      name,
-      class extends HTMLElement {
-        #connected = false;
-        #disconnected = false;
-
-        #internal?: {
-          connectedCallback: () => void;
-          disconnectedCallback: () => void;
-        };
-        #loader: Promise<void>;
-
-        constructor() {
-          super();
-          this.#loader = import(`./components/${component}.js`).then((mod) => {
-            this.#internal = mod.default(this);
-          });
-        }
-
-        connectedCallback() {
-          if (this.#disconnected) {
-            return;
-          }
-
-          this.style.visibility = "hidden";
-          void this.#loader.then(() => {
-            if (this.#disconnected) {
-              return;
-            }
-
-            this.style.visibility = "";
-
-            this.#internal?.connectedCallback();
-            this.#connected = true;
-
-            this.classList.add("hydrated");
-          });
-        }
-
-        disconnectedCallback() {
-          if (!this.#connected) {
-            this.#disconnected = true;
-            return;
-          }
-
-          void this.#loader.then(() => {
-            this.#internal?.disconnectedCallback();
-          });
-        }
-      },
-    );
+    customElements.define(name, factory(component));
   }
 }
