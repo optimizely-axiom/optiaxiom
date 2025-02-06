056b16cd256d806a490dbb25284b4d1fe292363a
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-06 14:10:08 -0500

    dynamically generate index file
    so we can build export list of all components

--- packages/web-components/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { factory } from "./factory";
-import { mapping } from "./mapping";
-
-for (const [name, component] of Object.entries(mapping)) {
-  if (!customElements.get(name)) {
-    customElements.define(name, factory(name, component));
-  }
-}
