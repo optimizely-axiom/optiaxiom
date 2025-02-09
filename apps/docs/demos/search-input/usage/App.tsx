87f1b50d8055fe52e9b349f33513d59586638098
toha993 <mutasim.toha@optimizely.com>
2025-02-09 16:23:26 +0600

    Update search-input documentation

--- apps/docs/demos/search-input/usage/App.tsx
+++ apps/docs/demos/search-input/usage/App.tsx
@@ -1,5 +1,26 @@
-import { SearchInput } from "@optiaxiom/react";
+"use client";
 
-export function App() {
-  return <SearchInput placeholder="Enter text..." w="224" />;
+import type { ComponentPropsWithoutRef } from "react";
+
+import { Field, SearchInput } from "@optiaxiom/react";
+
+export function App({
+  description,
+  error,
+  label = "Search label",
+  required = false,
+}: Pick<
+  ComponentPropsWithoutRef<typeof Field>,
+  "description" | "error" | "label" | "required"
+>) {
+  return (
+    <Field
+      description={description}
+      error={error}
+      label={label}
+      required={required}
+    >
+      <SearchInput placeholder="Enter text..." w="224" />
+    </Field>
+  );
 }
