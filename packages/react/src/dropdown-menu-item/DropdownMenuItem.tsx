14ff7079a3cacef8f18a6562952e98783fb76481
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-12 14:34:34 -0500

    prevent interactions in dropdown while closing
    since we animate the closing of dropdowns there is brief duration when dropdown
    is actually meant to be closed and we should be ignoring interactions within
    the dropdown
    
    but seems radix does not have this check and will try to handle mousemove and
    focus items/trigger if we quickly move the mouse while exit animation is
    playing
    
    so we prevent these mouse events from propagating to radix while dropdown is
    closed

--- packages/react/src/dropdown-menu-item/DropdownMenuItem.tsx
+++ packages/react/src/dropdown-menu-item/DropdownMenuItem.tsx
@@ -1,6 +1,7 @@
 import * as RadixMenu from "@radix-ui/react-dropdown-menu";
 import { forwardRef } from "react";
 
+import { useDropdownMenuContext } from "../dropdown-menu-context";
 import { ListboxItem, type ListboxItemProps } from "../listbox-item";
 
 type DropdownMenuItemProps = ListboxItemProps<typeof RadixMenu.Item>;
@@ -9,9 +10,24 @@ export const DropdownMenuItem = forwardRef<
   HTMLDivElement,
   DropdownMenuItemProps
 >(({ asChild, children, onSelect, ...props }, ref) => {
+  const { open } = useDropdownMenuContext("DropdownMenuItem");
+
   return (
     <ListboxItem asChild ref={ref} {...props}>
-      <RadixMenu.Item asChild={asChild} onSelect={onSelect}>
+      <RadixMenu.Item
+        asChild={asChild}
+        onPointerLeave={(event) => {
+          if (!open) {
+            event.preventDefault();
+          }
+        }}
+        onPointerMove={(event) => {
+          if (!open) {
+            event.preventDefault();
+          }
+        }}
+        onSelect={onSelect}
+      >
         {children}
       </RadixMenu.Item>
     </ListboxItem>
