051b697e1781947fa226ad76403126764b54096f
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-21 18:29:08 -0500

    document table row action usage

---
+++ apps/docs/demos/data-table/row-action-usage/data.ts
@@ -0,0 +1,10 @@
+import { faker } from "@faker-js/faker";
+
+faker.seed(123);
+
+export const data = Array.from({ length: 5 }, (_, index) => ({
+  amount: faker.finance.amount(),
+  firstName: faker.person.firstName(),
+  id: index + 1,
+  lastName: faker.person.lastName(),
+}));
