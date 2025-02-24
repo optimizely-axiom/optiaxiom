6e292f9938114af30e7406eeff30b497f3f09f37
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-24 09:56:00 -0500

    add width constraint for combobox popover
    by setting min-width to maximum width as content changes

---
+++ .changeset/nasty-pets-fold.md
@@ -0,0 +1,5 @@
+---
+"@optiaxiom/react": patch
+---
+
+add width constraint for combobox popover
--- packages/react/src/combobox-input/ComboboxInput.tsx
+++ packages/react/src/combobox-input/ComboboxInput.tsx
@@ -1,5 +1,13 @@
-import { type ComponentPropsWithoutRef, forwardRef } from "react";
+import {
+  type ComponentPropsWithoutRef,
+  forwardRef,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
+import { Box } from "../box";
+import { useComboboxContext } from "../combobox-context";
 import { useCommandContext } from "../command-context";
 import { CommandInput } from "../command-input";
 
@@ -8,20 +16,36 @@ type ComboboxInputProps = ComponentPropsWithoutRef<typeof CommandInput>;
 export const ComboboxInput = forwardRef<HTMLInputElement, ComboboxInputProps>(
   (props, ref) => {
     const { downshift, highlightedItem } = useCommandContext("ComboboxInput");
+    const { open } = useComboboxContext("ComboboxInput");
+
+    const [minWidth, setMinWidth] = useState(160);
+    const containerRef = useRef<HTMLDivElement>(null);
+    useEffect(() => {
+      if (!containerRef.current) {
+        return;
+      }
+
+      const rect = containerRef.current.getBoundingClientRect();
+      if (open && rect.width > minWidth) {
+        setMinWidth(rect.width);
+      }
+    }, [downshift.inputValue, minWidth, open]);
 
     return (
-      <CommandInput
-        htmlSize={1}
-        m="4"
-        onKeyDown={(event) => {
-          if (event.key === " " && highlightedItem) {
-            event.preventDefault();
-            downshift.selectItem(highlightedItem);
-          }
-        }}
-        ref={ref}
-        {...props}
-      />
+      <Box ref={containerRef} style={{ minWidth }}>
+        <CommandInput
+          htmlSize={1}
+          m="4"
+          onKeyDown={(event) => {
+            if (event.key === " " && highlightedItem) {
+              event.preventDefault();
+              downshift.selectItem(highlightedItem);
+            }
+          }}
+          ref={ref}
+          {...props}
+        />
+      </Box>
     );
   },
 );
