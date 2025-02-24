13d39b04f409c05e4449a54a6aa67ea0a6ff5431
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-24 09:57:58 -0500

    include styles prop in box props
    since both className and styles should apply to same element

--- packages/react/src/box/extractBoxProps.ts
+++ packages/react/src/box/extractBoxProps.ts
@@ -1,11 +1,18 @@
 import { type Sprinkles, sprinkles } from "../sprinkles";
 
 export function extractBoxProps<S extends Record<string, unknown>>(props: S) {
-  const boxProps = {} as Pick<S, ("className" | keyof Sprinkles) & keyof S>;
-  const restProps = {} as Omit<S, "className" | keyof Sprinkles>;
+  const boxProps = {} as Pick<
+    S,
+    ("className" | "styles" | keyof Sprinkles) & keyof S
+  >;
+  const restProps = {} as Omit<S, "className" | "styles" | keyof Sprinkles>;
 
   for (const [name, value] of Object.entries(props)) {
-    if (name === "className" || sprinkles.properties.has(name as never)) {
+    if (
+      name === "className" ||
+      name === "styles" ||
+      sprinkles.properties.has(name as never)
+    ) {
       if (value !== null && value !== undefined) {
         // @ts-expect-error -- too complex
         boxProps[name] = value;
