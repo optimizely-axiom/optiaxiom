0ba2ad8495b8db0c1495045a49132aa80ce6674d
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-26 12:09:14 -0500

    fix nested ModalLayer usage
    by forwarding the outer ref as a shard ref to the inner modal layers

--- packages/react/src/modal-layer/ModalLayer.tsx
+++ packages/react/src/modal-layer/ModalLayer.tsx
@@ -1,3 +1,4 @@
+import { useComposedRefs } from "@radix-ui/react-compose-refs";
 import { DismissableLayer } from "@radix-ui/react-dismissable-layer";
 import { FocusGuards } from "@radix-ui/react-focus-guards";
 import { Slot } from "@radix-ui/react-slot";
@@ -5,25 +6,29 @@ import {
   type ComponentPropsWithoutRef,
   forwardRef,
   type ReactNode,
+  useRef,
   useState,
 } from "react";
 import { RemoveScroll as ReactRemoveScroll } from "react-remove-scroll";
 
 import type { Box } from "../box";
 
-import { useModalContext } from "../modal-context";
+import { ModalContextProvider, useModalContext } from "../modal-context";
 
 type ModalLayerProps = Pick<ComponentPropsWithoutRef<typeof Box>, "asChild"> & {
   children?: ReactNode;
 };
 
 export const ModalLayer = forwardRef<HTMLDivElement, ModalLayerProps>(
-  ({ asChild, children, ...props }, ref) => {
+  ({ asChild, children, ...props }, outerRef) => {
     const { shardRef } = useModalContext("ModalLayer");
     const [locked] = useState(() => document.body.dataset.scrollLocked);
     const [guards] = useState(() =>
       document.querySelector("[data-radix-focus-guard]"),
     );
+
+    const innerRef = useRef<HTMLDivElement>(null);
+    const ref = useComposedRefs(innerRef, outerRef);
 
     if (!shardRef.current) {
       return asChild ? (
@@ -53,7 +58,9 @@ export const ModalLayer = forwardRef<HTMLDivElement, ModalLayerProps>(
       result = <FocusGuards>{result}</FocusGuards>;
     }
 
-    return result;
+    return (
+      <ModalContextProvider shardRef={innerRef}>{result}</ModalContextProvider>
+    );
   },
 );
 
