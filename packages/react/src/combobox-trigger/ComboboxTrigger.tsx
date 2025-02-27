c72c0a450d715ac87ec51d7a147a88b4422eee71
Rafi Adnan <rafi.adnan@optimizely.com>
2025-02-27 15:28:04 -0500

    fix aria-labelledby overriding in SelectTrigger
    and ComboboxTrigger to allow consuming aria-labelledby prop or fallback to
    field context

--- packages/react/src/combobox-trigger/ComboboxTrigger.tsx
+++ packages/react/src/combobox-trigger/ComboboxTrigger.tsx
@@ -16,47 +16,52 @@ type ComboboxTriggerProps = ComponentPropsWithoutRef<typeof PopoverTrigger>;
 export const ComboboxTrigger = forwardRef<
   HTMLButtonElement,
   ComboboxTriggerProps
->(({ asChild, children, ...props }, outerRef) => {
-  const { components, setOpen } = useComboboxContext("ComboboxTrigger");
+>(
+  (
+    { "aria-labelledby": ariaLabelledBy, asChild, children, ...props },
+    outerRef,
+  ) => {
+    const { components, setOpen } = useComboboxContext("ComboboxTrigger");
 
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const ref = useComposedRefs(outerRef, buttonRef);
+    const buttonRef = useRef<HTMLButtonElement>(null);
+    const ref = useComposedRefs(outerRef, buttonRef);
 
-  const { labelId } = useFieldContext();
-  useEffect(() => {
-    if (!labelId || !buttonRef.current) {
-      return;
-    }
+    const { labelId = ariaLabelledBy } = useFieldContext();
+    useEffect(() => {
+      if (!labelId || !buttonRef.current) {
+        return;
+      }
 
-    const button = buttonRef.current;
-    const label = document.getElementById(labelId);
-    if (!label) {
-      return;
-    }
+      const button = buttonRef.current;
+      const label = document.getElementById(labelId);
+      if (!label) {
+        return;
+      }
 
-    const onLabelClick = () => button.focus();
-    label.addEventListener("click", onLabelClick);
-    return () => label.removeEventListener("click", onLabelClick);
-  }, [labelId]);
+      const onLabelClick = () => button.focus();
+      label.addEventListener("click", onLabelClick);
+      return () => label.removeEventListener("click", onLabelClick);
+    }, [labelId]);
 
-  return (
-    <components.Trigger
-      aria-labelledby={labelId}
-      asChild
-      onKeyDown={(event) => {
-        switch (event.key) {
-          case "ArrowDown":
-          case "ArrowUp":
-            setOpen(true);
-        }
-      }}
-      ref={ref}
-      role="combobox"
-      {...props}
-    >
-      {asChild ? children : <AngleMenuButton>{children}</AngleMenuButton>}
-    </components.Trigger>
-  );
-});
+    return (
+      <components.Trigger
+        aria-labelledby={labelId}
+        asChild
+        onKeyDown={(event) => {
+          switch (event.key) {
+            case "ArrowDown":
+            case "ArrowUp":
+              setOpen(true);
+          }
+        }}
+        ref={ref}
+        role="combobox"
+        {...props}
+      >
+        {asChild ? children : <AngleMenuButton>{children}</AngleMenuButton>}
+      </components.Trigger>
+    );
+  },
+);
 
 ComboboxTrigger.displayName = "@optiaxiom/react/ComboboxTrigger";
