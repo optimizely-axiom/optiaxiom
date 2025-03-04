8ca15a1de99fe3ad5df8e7411cbefd127255a80e
Rafi Adnan <rafi.adnan@optimizely.com>
2025-03-04 10:33:32 -0500

    add re-usable useFieldLabelTrigger hook
    to easily attach click listeners to button trigger labels
    
    clicking the label will focus the associated button

---
+++ packages/react/src/use-field-label-trigger/useFieldLabelTrigger.ts
@@ -0,0 +1,27 @@
+import { type RefObject, useEffect } from "react";
+
+import { useFieldContext } from "../field-context";
+
+export const useFieldLabelTrigger = (
+  ref: RefObject<HTMLButtonElement>,
+  ariaLabelledBy: string | undefined,
+) => {
+  const { labelId = ariaLabelledBy } = useFieldContext();
+  useEffect(() => {
+    if (!labelId || !ref.current) {
+      return;
+    }
+
+    const button = ref.current;
+    const label = document.getElementById(labelId);
+    if (!label) {
+      return;
+    }
+
+    const onLabelClick = () => button.focus();
+    label.addEventListener("click", onLabelClick);
+    return () => label.removeEventListener("click", onLabelClick);
+  }, [labelId, ref]);
+
+  return labelId;
+};
