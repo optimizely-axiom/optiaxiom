6dd8143490597d704b2093facee9ad115e79c2b8
Rafi Adnan <rafi.adnan@optimizely.com>
2025-03-05 17:14:05 -0500

    stretch table to fill container
    by ensuring table flex container has align-items: stretch and adding a filler
    table header and data cell to each row that has `width: 0` but `flex: 1` to
    fill up any empty space

--- packages/react/src/table-header-cell/TableHeaderCell.tsx
+++ packages/react/src/table-header-cell/TableHeaderCell.tsx
@@ -8,12 +8,19 @@ type TableHeaderCellProps = BoxProps<"th">;
 export const TableHeaderCell = forwardRef<
   HTMLTableCellElement,
   TableHeaderCellProps
->(({ children, className, colSpan, ...props }, ref) => (
-  <Box asChild {...styles.cell({}, className)} {...props}>
-    <th colSpan={colSpan} ref={ref}>
-      <Box {...styles.content()}>{children}</Box>
-    </th>
-  </Box>
-));
+>(
+  (
+    { children, className, colSpan, p, px = "16", py = "12", ...props },
+    ref,
+  ) => (
+    <Box asChild {...styles.cell({}, className)} {...props}>
+      <th colSpan={colSpan} ref={ref}>
+        <Box {...styles.content()} px={p ?? px} py={p ?? py}>
+          {children}
+        </Box>
+      </th>
+    </Box>
+  ),
+);
 
 TableHeaderCell.displayName = "@optiaxiom/react/TableHeaderCell";
