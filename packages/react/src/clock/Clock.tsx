a5963c6075cb03d02879fff92f0d29c8b2eebaff
Rafi Adnan <rafi.adnan@optimizely.com>
2025-03-13 16:43:57 -0400

    use updated design for time controls

--- packages/react/src/clock/Clock.tsx
+++ packages/react/src/clock/Clock.tsx
@@ -1,12 +1,13 @@
 import { useControllableState } from "@radix-ui/react-use-controllable-state";
-import { forwardRef, useEffect, useRef } from "react";
+import { forwardRef } from "react";
 
 import type { BoxProps } from "../box";
 
 import { Flex } from "../flex";
-import { SegmentedControl } from "../segmented-control";
-import { SegmentedControlItem } from "../segmented-control-item";
-import * as styles from "./Clock.css";
+import { Select } from "../select";
+import { SelectContent } from "../select-content";
+import { SelectRadioItem } from "../select-radio-item";
+import { SelectTrigger } from "../select-trigger";
 import { format, parse, range } from "./utils";
 
 type ClockProps = BoxProps<
@@ -31,138 +32,72 @@ export const Clock = forwardRef<HTMLDivElement, ClockProps>(
     });
     const parsed = parse(value, parseInt(step) / 60);
 
-    const hourRef = useRef<HTMLButtonElement>(null);
-    const minuteRef = useRef<HTMLButtonElement>(null);
-    useEffect(() => {
-      if (hourRef.current?.parentElement) {
-        hourRef.current.parentElement.style.overflow = "hidden";
-        hourRef.current?.scrollIntoView();
-        hourRef.current.parentElement.style.overflow = "";
-      }
-
-      if (minuteRef.current?.parentElement) {
-        minuteRef.current.parentElement.style.overflow = "hidden";
-        minuteRef.current?.scrollIntoView();
-        minuteRef.current.parentElement.style.overflow = "";
-      }
-    }, []);
+    const hours = range(1, 12);
+    const minutes = range(0, 59, parseInt(step) / 60).map((minute) =>
+      minute.padStart(2, "0"),
+    );
+    const periods = ["AM" as const, "PM" as const];
 
     return (
-      <Flex
-        alignItems="stretch"
-        flexDirection="row"
-        gap="0"
-        onKeyDown={(event) => {
-          if (!(event.target instanceof HTMLButtonElement)) {
-            return;
-          }
-
-          const parent = event.target.parentElement;
-          if (!parent) {
-            return;
-          }
-
-          const grandParent = parent.parentElement;
-          if (!grandParent) {
-            return;
-          }
-
-          const sibling =
-            event.key === "ArrowRight"
-              ? (parent.nextElementSibling ?? grandParent.firstElementChild)
-              : event.key === "ArrowLeft"
-                ? (parent.previousElementSibling ??
-                  grandParent.lastElementChild)
-                : undefined;
-          if (!sibling) {
-            return;
-          }
-
-          const focusable =
-            sibling.querySelector('[tabindex="0"]') ??
-            sibling.querySelector('[aria-checked="true"]');
-          if (focusable instanceof HTMLButtonElement) {
-            focusable.focus();
-          }
-        }}
-        ref={ref}
-        {...props}
-      >
-        <SegmentedControl
-          aria-label="Select hour"
-          loop
-          onValueChange={(hour: string) =>
+      <Flex flexDirection="row" gap="4" ref={ref} {...props}>
+        <Select
+          items={hours}
+          onValueChange={(hour) =>
             hour && setValue(format({ ...parsed, hour }))
           }
-          orientation="vertical"
-          role="radiogroup"
           value={parsed.hour}
-          {...styles.wheel()}
         >
-          {range(1, 12).map((hour) => (
-            <SegmentedControlItem
-              appearance="subtle"
-              flex="none"
-              key={hour}
-              ref={hour === parsed.hour ? hourRef : undefined}
-              square
-              value={hour}
-            >
-              {hour}
-            </SegmentedControlItem>
-          ))}
-        </SegmentedControl>
+          <SelectTrigger aria-label="Select hour" flex="1" placeholder="HH" />
+          <SelectContent>
+            {hours.map((hour) => (
+              <SelectRadioItem item={hour} key={hour}>
+                {hour}
+              </SelectRadioItem>
+            ))}
+          </SelectContent>
+        </Select>
 
-        <SegmentedControl
-          aria-label="Select minute"
-          loop
-          onValueChange={(minute: string) =>
+        <Select
+          items={minutes}
+          onValueChange={(minute) =>
             minute && setValue(format({ ...parsed, minute }))
           }
-          orientation="vertical"
-          role="radiogroup"
           value={parsed.minute}
-          {...styles.wheel()}
         >
-          {range(0, 59, parseInt(step) / 60)
-            .map((minute) => minute.padStart(2, "0"))
-            .map((minute) => (
-              <SegmentedControlItem
-                appearance="subtle"
+          <SelectTrigger aria-label="Select minute" flex="1" placeholder="MM" />
+          <SelectContent>
+            {minutes.map((minute) => (
+              <SelectRadioItem
                 aria-label={`${parseInt(minute)}`}
-                flex="none"
+                item={minute}
                 key={minute}
-                ref={minute === parsed.minute ? minuteRef : undefined}
-                square
-                value={minute}
               >
                 {minute}
-              </SegmentedControlItem>
+              </SelectRadioItem>
             ))}
-        </SegmentedControl>
+          </SelectContent>
+        </Select>
 
-        <SegmentedControl
-          aria-label="Select AM or PM"
-          onValueChange={(meridiem: "" | "AM" | "PM") =>
+        <Select
+          items={periods}
+          onValueChange={(meridiem) =>
             meridiem && setValue(format({ ...parsed, meridiem }))
           }
-          orientation="vertical"
-          role="radiogroup"
           value={parsed.meridiem}
-          {...styles.wheel()}
         >
-          {["AM", "PM"].map((meridiem) => (
-            <SegmentedControlItem
-              appearance="subtle"
-              flex="none"
-              key={meridiem}
-              square
-              value={meridiem}
-            >
-              {meridiem}
-            </SegmentedControlItem>
-          ))}
-        </SegmentedControl>
+          <SelectTrigger
+            aria-label="Select AM or PM"
+            flex="1"
+            placeholder="--"
+          />
+          <SelectContent>
+            {periods.map((meridiem) => (
+              <SelectRadioItem item={meridiem} key={meridiem}>
+                {meridiem}
+              </SelectRadioItem>
+            ))}
+          </SelectContent>
+        </Select>
       </Flex>
     );
   },
