2c106037b28d8ada6f9f151e4ed90c5e53abd1ca
Rafi Adnan <rafi.adnan@optimizely.com>
2025-03-14 10:33:33 -0400

    sync calendar design with figma
    - match spacing between day cells
    - match weekend styles
    - add pressed state

---
+++ packages/react/src/calendar/CalendarWeek.tsx
@@ -0,0 +1,17 @@
+import type { ComponentPropsWithoutRef } from "react";
+
+import { Week } from "react-day-picker";
+
+import { Box } from "../box";
+
+type CalendarWeekProps = ComponentPropsWithoutRef<typeof Week>;
+
+export function CalendarWeek({ children, ...props }: CalendarWeekProps) {
+  return (
+    <Box asChild display="flex" gap="2">
+      <Week {...props}>{children}</Week>
+    </Box>
+  );
+}
+
+CalendarWeek.displayName = "@optiaxiom/react/CalendarWeek";
