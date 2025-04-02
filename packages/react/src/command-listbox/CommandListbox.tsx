05fe0e552ccdc15bb686b89d37d6c611e3357efc
Rafi Adnan <rafi.adnan@optimizely.com>
2025-04-02 11:03:15 -0400

    incorporate virtualization into CommandListbox
    rather than in each Listbox implementation

--- packages/react/src/command-listbox/CommandListbox.tsx
+++ packages/react/src/command-listbox/CommandListbox.tsx
@@ -1,24 +1,66 @@
-import { forwardRef, useEffect } from "react";
+import { forwardRef, Fragment, type ReactNode, useEffect } from "react";
 
 import { type BoxProps } from "../box";
 import { useCommandContext } from "../command-context";
 import { Listbox } from "../listbox";
+import { ListboxEmpty } from "../listbox-empty";
+import { ListboxVirtualized } from "../listbox-virtualized";
 
-type CommandListboxProps = BoxProps;
+const VIRTUALIZE_THRESHOLD = 50;
+
+type CommandListboxProps = BoxProps<
+  "div",
+  | {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      children: ((item: any) => ReactNode) | ReactNode;
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      items?: any[];
+    }
+  | {
+      children?: ReactNode;
+      items?: never;
+    }
+>;
 
 export const CommandListbox = forwardRef<HTMLDivElement, CommandListboxProps>(
-  ({ children, size, ...props }, ref) => {
-    const { downshift, placed, setPlaced } = useCommandContext(
-      "@optiaxiom/react/CommandListbox",
-    );
+  ({ children, items: itemsProp, size, ...props }, ref) => {
+    const { downshift, highlightedItem, items, placed, setPlaced } =
+      useCommandContext("@optiaxiom/react/CommandListbox");
     useEffect(() => {
       requestAnimationFrame(() => setPlaced(true));
       return () => setPlaced(false);
     }, [setPlaced]);
 
     return (
-      <Listbox size={size} {...downshift.getMenuProps({ ref, ...props })}>
-        {children}
+      <Listbox
+        asChild={
+          typeof children === "function" &&
+          items.length > VIRTUALIZE_THRESHOLD &&
+          placed
+        }
+        size={size}
+        {...downshift.getMenuProps({ ref, ...props })}
+      >
+        {typeof children === "function" ? (
+          items.length > VIRTUALIZE_THRESHOLD ? (
+            placed && (
+              <ListboxVirtualized
+                highlightedItem={highlightedItem}
+                items={itemsProp ?? items}
+              >
+                {children}
+              </ListboxVirtualized>
+            )
+          ) : items.length > 0 ? (
+            items.map((item, index) => (
+              <Fragment key={index}>{children(item)}</Fragment>
+            ))
+          ) : (
+            <ListboxEmpty />
+          )
+        ) : (
+          children
+        )}
       </Listbox>
     );
   },
