539c278c5ca513fdbb61ead63e9a266881fc8a7f
Rafi Adnan <rafi.adnan@optimizely.com>
2025-04-02 11:03:15 -0400

    incorporate virtualization into CommandListbox
    rather than in each Listbox implementation

--- packages/react/src/combobox-listbox/ComboboxListbox.tsx
+++ packages/react/src/combobox-listbox/ComboboxListbox.tsx
@@ -1,11 +1,9 @@
-import { forwardRef, type ReactNode } from "react";
+import { forwardRef } from "react";
 
 import { Box, type BoxProps } from "../box";
-import { ComboboxEmpty } from "../combobox-empty";
 import { ComboboxRadioItem } from "../combobox-radio-item";
 import { useCommandContext } from "../command-context";
 import { CommandListbox } from "../command-listbox";
-import { ListboxVirtualized } from "../listbox-virtualized";
 import { Spinner } from "../spinner";
 
 type ComboboxListboxProps = BoxProps<
@@ -15,63 +13,28 @@ type ComboboxListboxProps = BoxProps<
      * Whether to show loading spinner inside the menu.
      */
     loading?: boolean;
-  } & (
-    | {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        children: ((item: any) => ReactNode) | ReactNode;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        items?: any[];
-      }
-    | {
-        children?: ReactNode;
-        items?: never;
-      }
-  )
+  }
 >;
 
 export const ComboboxListbox = forwardRef<HTMLDivElement, ComboboxListboxProps>(
-  ({ children, items: itemsProp, loading, ...props }, ref) => {
-    const { highlightedItem, items, itemToLabel } = useCommandContext(
+  ({ children, loading, ...props }, ref) => {
+    const { itemToLabel } = useCommandContext(
       "@optiaxiom/react/ComboboxListbox",
     );
 
     return (
-      <CommandListbox
-        asChild={!loading && typeof children === "function"}
-        ref={ref}
-        tabIndex={-1}
-        {...props}
-      >
+      <CommandListbox ref={ref} tabIndex={-1} {...props}>
         {loading ? (
           <Box display="flex" justifyContent="center" p="16">
             <Spinner />
           </Box>
-        ) : children ? (
-          typeof children === "function" ? (
-            items.length > 0 ? (
-              <ListboxVirtualized
-                highlightedItem={highlightedItem}
-                items={itemsProp ?? items}
-              >
-                {children}
-              </ListboxVirtualized>
-            ) : (
-              <ComboboxEmpty />
-            )
-          ) : (
-            children
-          )
-        ) : items.length > 0 ? (
-          items.map((item, index) => (
-            <ComboboxRadioItem
-              item={item}
-              key={typeof item === "string" ? item : index}
-            >
+        ) : (
+          (children ??
+          ((item) => (
+            <ComboboxRadioItem item={item}>
               {itemToLabel(item)}
             </ComboboxRadioItem>
-          ))
-        ) : (
-          <ComboboxEmpty />
+          )))
         )}
       </CommandListbox>
     );
--- packages/react/src/dropdown-menu-combobox-listbox/DropdownMenuComboboxListbox.tsx
+++ packages/react/src/dropdown-menu-combobox-listbox/DropdownMenuComboboxListbox.tsx
@@ -1,10 +1,9 @@
-import { forwardRef, type ReactNode } from "react";
+import { forwardRef } from "react";
 
 import { Box, type BoxProps } from "../box";
 import { useCommandContext } from "../command-context";
 import { CommandListbox } from "../command-listbox";
 import { DropdownMenuAutocompleteItem } from "../dropdown-menu-autocomplete-item";
-import { ListboxVirtualized } from "../listbox-virtualized";
 import { Spinner } from "../spinner";
 
 type DropdownMenuAutocompleteListboxProps = BoxProps<
@@ -14,55 +13,30 @@ type DropdownMenuAutocompleteListboxProps = BoxProps<
      * Whether to show loading spinner inside the menu.
      */
     loading?: boolean;
-  } & (
-    | {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        children: (item: any) => ReactNode;
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        items?: any[];
-      }
-    | {
-        children?: ReactNode;
-        items?: never;
-      }
-  )
+  }
 >;
 
 export const DropdownMenuComboboxListbox = forwardRef<
   HTMLDivElement,
   DropdownMenuAutocompleteListboxProps
->(({ children, items: itemsProp, loading, ...props }, ref) => {
-  const { highlightedItem, items, itemToLabel } = useCommandContext(
+>(({ children, loading, ...props }, ref) => {
+  const { items, itemToLabel } = useCommandContext(
     "@optiaxiom/react/DropdownMenuComboboxListbox",
   );
 
   return (
-    <CommandListbox
-      asChild={!loading && typeof children === "function"}
-      ref={ref}
-      {...props}
-    >
+    <CommandListbox ref={ref} {...props}>
       {loading ? (
         <Box display="flex" justifyContent="center" p="16">
           <Spinner />
         </Box>
-      ) : children ? (
-        typeof children === "function" ? (
-          <ListboxVirtualized
-            highlightedItem={highlightedItem}
-            items={itemsProp ?? items}
-          >
-            {children}
-          </ListboxVirtualized>
-        ) : (
-          children
-        )
       ) : (
+        (children ??
         items.map((item) => (
           <DropdownMenuAutocompleteItem item={item} key={item}>
             {itemToLabel(item)}
           </DropdownMenuAutocompleteItem>
-        ))
+        )))
       )}
     </CommandListbox>
   );
