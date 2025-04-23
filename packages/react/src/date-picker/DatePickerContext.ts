7bc280efbe29801c010893c1ac183e24f6bf5094
Rafi Adnan <rafi.adnan@optimizely.com>
2025-04-23 13:07:12 -0400

    auto close date picker after selection

--- packages/react/src/date-picker/DatePickerContext.ts
+++ packages/react/src/date-picker/DatePickerContext.ts
@@ -4,6 +4,7 @@ import { createContext } from "@radix-ui/react-context";
 
 export const [DatePickerProvider, useDatePickerContext] = createContext<{
   disabled: boolean | undefined;
+  setOpen: (open: boolean) => void;
   setValue: (date: Date | undefined) => void;
   step: string;
   type: "date" | "datetime-local";
