2cec970a41716d251acb63b5cb734b40ab4e98c8
Rafi Adnan <rafi.adnan@optimizely.com>
2025-04-30 14:49:39 -0400

    add keyboard interaction doc sections
    - improve Kbd styles by using sans font and other style upgrades (so cmd/shift
      keys look much nicer)
    - add aria-label for Kbd shortcuts
    - add changelog for moving Select out of Alpha

--- packages/react/src/kbd/Kbd.css.ts
+++ packages/react/src/kbd/Kbd.css.ts
@@ -1,36 +1,32 @@
 import { recipe, type RecipeVariants, style } from "../vanilla-extract";
 
 export const kbd = recipe({
-  base: [
-    {
-      alignItems: "center",
-      bg: "bg.secondary",
-      display: "inline-flex",
-      flexDirection: "row",
-      px: "4",
-      rounded: "sm",
-      whiteSpace: "nowrap",
-    },
-    style({
-      fontSize: "0.9em",
-    }),
-  ],
+  base: {
+    alignItems: "center",
+    color: "fg.secondary",
+    display: "inline-flex",
+    flexDirection: "row",
+    fontFamily: "sans",
+    gap: "2",
+    whiteSpace: "nowrap",
+  },
 
   variants: {
     variant: {
-      outline: {
-        border: "1",
-        borderB: "2",
-        borderColor: "border.tertiary",
-        gap: "4",
-      },
-      subtle: {
-        bg: "transparent",
-        color: "fg.secondary",
-        fontFamily: "sans",
-        gap: "2",
-        px: "0",
-      },
+      outline: [
+        {
+          bg: "bg.secondary",
+          border: "1",
+          borderB: "2",
+          borderColor: "border.secondary",
+          px: "6",
+          rounded: "sm",
+        },
+        style({
+          fontSize: "0.8em",
+        }),
+      ],
+      subtle: {},
     },
   },
 });
@@ -39,15 +35,6 @@ export const keys = recipe({
   base: style({
     textDecoration: "none",
   }),
-
-  variants: {
-    variant: {
-      outline: style({
-        fontSize: "1.2em",
-      }),
-      subtle: {},
-    },
-  },
 });
 
 export type KdbVariants = RecipeVariants<typeof kbd>;
