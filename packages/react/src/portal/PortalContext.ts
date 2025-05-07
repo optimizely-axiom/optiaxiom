d15c590e967f8e5ccfd9431e53ed3b2ed4ae32ef
Rafi Adnan <rafi.adnan@optimizely.com>
2025-05-07 17:09:09 -0400

    make portal context optional

--- packages/react/src/portal/PortalContext.ts
+++ packages/react/src/portal/PortalContext.ts
@@ -4,4 +4,4 @@ import { createContext } from "@radix-ui/react-context";
 
 export const [PortalProvider, usePortalContext] = createContext<{
   container: ShadowRoot | undefined;
-}>("@optiaxiom/react/Portal");
+}>("@optiaxiom/react/Portal", { container: undefined });
