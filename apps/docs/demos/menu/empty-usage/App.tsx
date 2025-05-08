ce78d73da1b03c5a222d934f8d14986a312399b2
Rafi Adnan <rafi.adnan@optimizely.com>
2025-05-08 17:39:25 -0400

    improve demos
    so demo is clearer about what it is doing

--- apps/docs/demos/menu/empty-usage/App.tsx
+++ apps/docs/demos/menu/empty-usage/App.tsx
@@ -2,18 +2,9 @@
 
 import { Menu, MenuContent, MenuTrigger } from "@optiaxiom/react/unstable";
 
-const colors = [
-  { label: "Ocean" },
-  { label: "Blue" },
-  { label: "Purple" },
-  { label: "Red" },
-  { label: "Orange" },
-  { label: "Yellow" },
-];
-
 export function App() {
   return (
-    <Menu empty="No colors matched." initialInputVisible options={colors}>
+    <Menu empty="No colors available." initialInputVisible options={[]}>
       <MenuTrigger>Select color</MenuTrigger>
       <MenuContent />
     </Menu>
