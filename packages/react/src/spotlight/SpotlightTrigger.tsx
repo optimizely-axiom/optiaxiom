d8c9cbfc6ce43deef4424026cd18e84971a89c26
Rafi Adnan <rafi.adnan@optimizely.com>
2025-05-23 17:01:44 -0400

    remove ability to customize spotlight hotkey

--- packages/react/src/spotlight/SpotlightTrigger.tsx
+++ packages/react/src/spotlight/SpotlightTrigger.tsx
@@ -7,22 +7,17 @@ import { IconMagnifyingGlass } from "../icons/IconMagnifyingGlass";
 import { MenuTrigger } from "../menu";
 import { useMenuContext } from "../menu/internals";
 
-export type SpotlightTriggerProps = ButtonProps<
-  typeof MenuTrigger,
-  {
-    hotkey?: string;
-  }
->;
+export type SpotlightTriggerProps = ButtonProps<typeof MenuTrigger>;
 
 const DEFAULT_HOTKEY = "mod+K";
 
 export const SpotlightTrigger = forwardRef<
   HTMLButtonElement,
   SpotlightTriggerProps
->(({ children, hotkey = DEFAULT_HOTKEY, ...props }, ref) => {
+>(({ children, ...props }, ref) => {
   const { open, setOpen } = useMenuContext("@optiaxiom/react/SpotlightTrigger");
 
-  useHotkeys([[hotkey, () => setOpen(!open)]]);
+  useHotkeys([[DEFAULT_HOTKEY, () => setOpen(!open)]]);
 
   return (
     <MenuTrigger asChild ref={ref} {...props}>
