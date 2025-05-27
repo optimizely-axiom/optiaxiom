2d3b787a966f3d03f905ea7a9f28fd1a4763f48a
Rafi Adnan <rafi.adnan@optimizely.com>
2025-05-27 15:42:33 -0400

    optimize submenu rendering performance
    by only using a single popover per listbox and swapping the content when user
    moves to a new menu with suboptions or open/close as usual

--- packages/react/src/menu/MenuSubContext.ts
+++ packages/react/src/menu/MenuSubContext.ts
@@ -1,11 +1,13 @@
 "use client";
 
-import type { RefObject } from "react";
+import type { MutableRefObject, RefObject } from "react";
 
 import { createContext } from "@radix-ui/react-context";
 
 export const [MenuSubProvider, useMenuSubContext] = createContext<{
+  contentRef: RefObject<HTMLDivElement>;
   inputRef: RefObject<HTMLInputElement>;
   open: boolean;
   setOpen: (open: boolean) => void;
+  triggerRef: MutableRefObject<HTMLDivElement | null>;
 }>("@optiaxiom/react/MenuSub");
--- packages/react/src/menu/MenuSubTrigger.tsx
+++ packages/react/src/menu/MenuSubTrigger.tsx
@@ -1,115 +1,127 @@
-import { forwardRef, type RefObject, useEffect, useState } from "react";
+import { useComposedRefs } from "@radix-ui/react-compose-refs";
+import { forwardRef, useEffect, useRef } from "react";
 
-import type { BoxProps } from "../box";
-
+import { type BoxProps } from "../box";
 import { useCommandContext } from "../command/internals";
-import { FilteredSlot } from "../filtered-slot";
 import { IconAngleRight } from "../icons/IconAngleRight";
-import { PopoverTrigger } from "../popover";
 import { usePopoverContext } from "../popover/internals";
 import { MenuItem } from "./MenuItem";
+import { useMenuSubContext } from "./MenuSubContext";
 
-export type MenuSubTriggerProps = BoxProps<
-  typeof MenuItem,
-  {
-    /**
-     * A ref pointing to the popover content element. Used to determine if user's pointer is moving towards the sub menu.
-     */
-    contentRef: RefObject<HTMLDivElement>;
-  }
->;
+export type MenuSubTriggerProps = BoxProps<typeof MenuItem>;
 
 export const MenuSubTrigger = forwardRef<HTMLDivElement, MenuSubTriggerProps>(
   (
     {
       children,
-      contentRef,
       item,
       onPointerDown,
       onPointerEnter,
       onPointerLeave,
+      onPointerMove,
       ...props
     },
-    ref,
+    outerRef,
   ) => {
     const { downshift, highlightedItem, pauseInteractionRef } =
       useCommandContext("@optiaxiom/react/MenuSubTrigger");
     const { open } = usePopoverContext("@optiaxiom/react/MenuSubTrigger");
+    const { contentRef, triggerRef } = useMenuSubContext(
+      "@optiaxiom/react/MenuSubTrigger",
+    );
 
-    const [isMounted, setIsMounted] = useState(false);
+    const pointerEnterRef = useRef(false);
+
+    const innerRef = useRef();
+    const ref = useComposedRefs(innerRef, outerRef);
     useEffect(() => {
-      setIsMounted(true);
-    }, []);
+      if (open && highlightedItem === item && innerRef.current) {
+        triggerRef.current = innerRef.current;
+      }
+    }, [highlightedItem, item, open, triggerRef]);
 
     return (
-      <PopoverTrigger asChild>
-        <FilteredSlot exclude="type">
-          <MenuItem
-            addonAfter={<IconAngleRight />}
-            item={item}
-            onPointerDown={(event) => {
-              onPointerDown?.(event);
-              if (open) {
-                event.preventDefault();
-              }
-            }}
-            onPointerEnter={(event) => {
-              onPointerEnter?.(event);
-              if (!isMounted || highlightedItem !== item) {
-                return;
-              }
+      <MenuItem
+        addonAfter={<IconAngleRight />}
+        item={item}
+        onPointerDown={(event) => {
+          onPointerDown?.(event);
+          if (open) {
+            event.preventDefault();
+          }
+        }}
+        onPointerEnter={(event) => {
+          onPointerEnter?.(event);
+          if (event.defaultPrevented) {
+            return;
+          }
 
-              downshift.selectItem(item);
-            }}
-            onPointerLeave={(event) => {
-              onPointerLeave?.(event);
-              const box = contentRef.current?.getBoundingClientRect();
-              if (!box) {
-                return;
-              }
+          if (highlightedItem === item) {
+            pointerEnterRef.current = true;
+          }
+        }}
+        onPointerLeave={(event) => {
+          onPointerLeave?.(event);
+          pointerEnterRef.current = false;
+          const box = contentRef.current?.getBoundingClientRect();
+          if (!box) {
+            return;
+          }
+          if (pauseInteractionRef.current.isInsideTriangle) {
+            return;
+          }
 
-              const bottom = {
-                x: box.left,
-                y: box.top + box.height,
-              };
-              const side = {
-                x: event.clientX + 10 * (box.left > event.clientX ? -1 : 1),
-                y: event.clientY,
-              };
-              const top = {
-                x: box.left,
-                y: box.top,
-              };
-              pauseInteractionRef.current.isInsideTriangle = (target) => {
-                const b0 =
-                    (top.x - side.x) * (bottom.y - side.y) -
-                    (bottom.x - side.x) * (top.y - side.y),
-                  b1 =
-                    ((top.x - target.x) * (bottom.y - target.y) -
-                      (bottom.x - target.x) * (top.y - target.y)) /
-                    b0,
-                  b2 =
-                    ((bottom.x - target.x) * (side.y - target.y) -
-                      (side.x - target.x) * (bottom.y - target.y)) /
-                    b0,
-                  b3 =
-                    ((side.x - target.x) * (top.y - target.y) -
-                      (top.x - target.x) * (side.y - target.y)) /
-                    b0;
+          const bottom = {
+            x: box.left,
+            y: box.top + box.height,
+          };
+          const side = {
+            x: event.clientX + 10 * (box.left > event.clientX ? -1 : 1),
+            y: event.clientY,
+          };
+          const top = {
+            x: box.left,
+            y: box.top,
+          };
+          pauseInteractionRef.current.isInsideTriangle = (target) => {
+            const b0 =
+                (top.x - side.x) * (bottom.y - side.y) -
+                (bottom.x - side.x) * (top.y - side.y),
+              b1 =
+                ((top.x - target.x) * (bottom.y - target.y) -
+                  (bottom.x - target.x) * (top.y - target.y)) /
+                b0,
+              b2 =
+                ((bottom.x - target.x) * (side.y - target.y) -
+                  (side.x - target.x) * (bottom.y - target.y)) /
+                b0,
+              b3 =
+                ((side.x - target.x) * (top.y - target.y) -
+                  (top.x - target.x) * (side.y - target.y)) /
+                b0;
 
-                return b1 > 0 && b2 > 0 && b3 > 0;
-              };
-              pauseInteractionRef.current.timer = window.setTimeout(() => {
-                pauseInteractionRef.current.isInsideTriangle = null;
-              }, 20);
-            }}
-            ref={ref}
-            {...props}
-          >
-            {children}
-          </MenuItem>
-        </FilteredSlot>
-      </PopoverTrigger>
+            return b1 > 0 && b2 > 0 && b3 > 0;
+          };
+          pauseInteractionRef.current.timer = window.setTimeout(() => {
+            pauseInteractionRef.current.isInsideTriangle = null;
+          }, 20);
+        }}
+        onPointerMove={(event) => {
+          onPointerMove?.(event);
+          if (event.defaultPrevented) {
+            return;
+          }
+
+          if (pointerEnterRef.current) {
+            downshift.selectItem(item);
+          }
+          pointerEnterRef.current = false;
+        }}
+        ref={ref}
+        {...props}
+      >
+        {children}
+      </MenuItem>
     );
   },
 );
