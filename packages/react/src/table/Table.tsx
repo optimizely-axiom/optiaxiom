5a84da407eeb73f89069787668f3caa035ce2ec5
Rafi Adnan <rafi.adnan@optimizely.com>
2025-05-29 15:20:23 -0400

    also forward style prop to outer div

--- packages/react/src/table/Table.tsx
+++ packages/react/src/table/Table.tsx
@@ -6,11 +6,16 @@ import * as styles from "./Table.css";
 export type TableProps = BoxProps<"table", styles.TableVariants>;
 
 export const Table = forwardRef<HTMLDivElement, TableProps>(
-  ({ children, className, layout = "auto", ...props }, ref) => {
+  ({ children, className, layout = "auto", style, ...props }, ref) => {
     const { boxProps, restProps } = extractBoxProps(props);
 
     return (
-      <Box {...styles.wrapper({}, className)} ref={ref} {...boxProps}>
+      <Box
+        ref={ref}
+        style={style}
+        {...styles.wrapper({}, className)}
+        {...boxProps}
+      >
         <Box asChild {...styles.table({ layout })} {...restProps}>
           <table>{children}</table>
         </Box>
