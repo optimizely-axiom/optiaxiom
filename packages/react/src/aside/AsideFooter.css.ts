46e120d55e8a79263130a796b458e91986aaaa24
Abu Jafar Saifullah <AbuJafar.Saifullah@optimizely.com>
2025-06-12 12:04:01 +0600

    add aside component structure

---
+++ packages/react/src/aside/AsideFooter.css.ts
@@ -0,0 +1,15 @@
+import { recipe } from "../vanilla-extract";
+
+export const footer = recipe({
+  base: [
+    {
+      borderColor: "border.secondary",
+      borderT: "1",
+      flexDirection: "row",
+      gap: "16",
+      justifyContent: "flex-end",
+      px: "24",
+      py: "20",
+    },
+  ],
+});
