5bdbc83caac3a7cabd12155bb46891f3dd6598a0
Rafi Adnan <rafi.adnan@optimizely.com>
2025-06-18 12:02:35 -0400

    apply autoprefixer postcss plugin
    to apply vendor prefixes for properties that need it

---
+++ .changeset/every-badgers-hammer.md
@@ -0,0 +1,6 @@
+---
+"@optiaxiom/react": patch
+"@optiaxiom/web-components": patch
+---
+
+apply autoprefixer postcss plugin
--- packages/react/rollup.config.mjs
+++ packages/react/rollup.config.mjs
@@ -1,10 +1,16 @@
 import hash from "@emotion/hash";
 import json from "@rollup/plugin-json";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import {
+  getSourceFromVirtualCssFile,
+  virtualCssFileFilter,
+} from "@vanilla-extract/integration";
 import { vanillaExtractPlugin } from "@vanilla-extract/rollup-plugin";
+import autoprefixer from "autoprefixer";
 import { readFileSync } from "node:fs";
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
+import postcss from "postcss";
 import { defineConfig } from "rollup";
 import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
@@ -102,6 +108,7 @@ export default defineConfig([
         target: "esnext",
       }),
       json(),
+      env === "production" && stylePlugin(),
       vanillaExtractPlugin(
         env === "production"
           ? {
@@ -150,4 +157,28 @@ export default defineConfig([
 
 function normalizeIdentifier(identifier) {
   return identifier.match(/^[0-9]/) ? "_".concat(identifier) : identifier;
+}
+
+/** @returns {import('rollup').Plugin} */
+function stylePlugin() {
+  return {
+    name: "rollup-plugin-style",
+    async resolveId(id) {
+      if (!virtualCssFileFilter.test(id)) {
+        return null;
+      }
+
+      const { fileName, source } = await getSourceFromVirtualCssFile(id);
+      const result = await postcss([autoprefixer]).process(source, {
+        from: fileName,
+      });
+      return {
+        external: true,
+        id: fileName,
+        meta: {
+          css: result.css,
+        },
+      };
+    },
+  };
 }
