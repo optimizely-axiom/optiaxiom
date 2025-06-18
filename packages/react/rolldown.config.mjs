c1c46abfb9c30869d094d2c6b97b738f47d02ea6
Rafi Adnan <rafi.adnan@optimizely.com>
2025-06-18 14:22:01 -0400

    replace rollup with rolldown

--- packages/react/rolldown.config.mjs
+++ packages/react/rolldown.config.mjs
@@ -1,6 +1,4 @@
 import hash from "@emotion/hash";
-import json from "@rollup/plugin-json";
-import { nodeResolve } from "@rollup/plugin-node-resolve";
 import {
   getSourceFromVirtualCssFile,
   virtualCssFileFilter,
@@ -11,9 +9,8 @@ import { readFileSync } from "node:fs";
 import { readFile } from "node:fs/promises";
 import { resolve } from "node:path";
 import postcss from "postcss";
-import { defineConfig } from "rollup";
-import dts from "rollup-plugin-dts";
-import esbuild from "rollup-plugin-esbuild";
+import { defineConfig } from "rolldown";
+import { dts } from "rolldown-plugin-dts";
 
 const env = process.env.NODE_ENV ?? "development";
 const pkg = JSON.parse(readFileSync("./package.json"));
@@ -60,7 +57,7 @@ export default defineConfig([
         name: "preserve-directives",
         renderChunk(code, chunk) {
           const directives = chunk.moduleIds.some(
-            (id) => this.getModuleInfo(id).meta.preserveDirectives,
+            (id) => this.getModuleInfo(id)?.meta.preserveDirectives,
           );
           return directives ? '"use client";\n' + code : code;
         },
@@ -98,16 +95,6 @@ export default defineConfig([
           return code.replace(search, replace);
         },
       },
-      nodeResolve({
-        preferBuiltins: false,
-      }),
-      esbuild({
-        define: {
-          "process.env.NODE_ENV": JSON.stringify(env),
-        },
-        target: "esnext",
-      }),
-      json(),
       env === "production" && stylePlugin(),
       vanillaExtractPlugin(
         env === "production"
@@ -135,6 +122,12 @@ export default defineConfig([
         name: "optimize-generate-bundle",
       },
     ],
+    transform: {
+      define: {
+        "process.env.NODE_ENV": JSON.stringify(env),
+      },
+      target: "esnext",
+    },
   },
   {
     external,
@@ -148,7 +141,8 @@ export default defineConfig([
     },
     plugins: [
       dts({
-        respectExternal: true,
+        emitDtsOnly: true,
+        sourcemap: false,
         tsconfig: "tsconfig.build.json",
       }),
     ],
@@ -159,10 +153,10 @@ function normalizeIdentifier(identifier) {
   return identifier.match(/^[0-9]/) ? "_".concat(identifier) : identifier;
 }
 
-/** @returns {import('rollup').Plugin} */
+/** @returns {import('rolldown').Plugin} */
 function stylePlugin() {
   return {
-    name: "rollup-plugin-style",
+    name: "rolldown-plugin-style",
     async resolveId(id) {
       if (!virtualCssFileFilter.test(id)) {
         return null;
