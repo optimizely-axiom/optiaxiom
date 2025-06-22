4f679db8db3c8a04e18f49a57747a2553bcb37c2
TashfiaJ <Tashfia.Jannath@optimizely.com>
2025-06-23 01:37:51 +0600

    Use Input from DL and wrap it with VisuallyHidden component

--- packages/react/src/file-upload/FileUploadDropzone.tsx
+++ packages/react/src/file-upload/FileUploadDropzone.tsx
@@ -1,17 +1,24 @@
 import { Box } from "../box";
 import { Flex } from "../flex";
 import { IconDragAndDrop } from "../icons/IconDragAndDrop";
+import { Input } from "../input";
 import { Text } from "../text";
+import { VisuallyHidden } from "../visually-hidden";
 import * as styles from "./FileUpload.css";
 import { useFileUploadContext } from "./FileUploadContext";
 
 export const FileUploadDropzone: React.FC = () => {
-  const { getInputProps, getRootProps } = useFileUploadContext();
+  const { getInputProps, getRootProps } = useFileUploadContext("@optiaxiom/react/FileUploadDropzone");
+
+  const inputProps = getInputProps();
+  const { color: _color, size: _size, ...filteredProps } = inputProps;
 
   return (
     <Box {...getRootProps()}>
       <Flex {...styles.fileUpload({})}>
-        <input type="file" {...(getInputProps() as React.InputHTMLAttributes<HTMLInputElement>)} />
+        <VisuallyHidden>
+          <Input {...filteredProps} />
+        </VisuallyHidden>
         <IconDragAndDrop/>
         <Text>Drag and drop or click to upload</Text>
       </Flex>
