09be15459b0ea4d288cb2a98c6332a7024621b26
TashfiaJ <Tashfia.Jannath@optimizely.com>
2025-06-22 10:15:00 +0600

    create FileUploadDropzone with react-dropzone integration and styled UI

---
+++ packages/react/src/file-upload/FileUploadDropzone.tsx
@@ -0,0 +1,21 @@
+import { Flex } from "../flex";
+import { IconDragAndDrop } from "../icons/IconDragAndDrop";
+import { Text } from "../text";
+import * as styles from "./FileUpload.css";
+import { useFileUploadContext } from "./FileUploadContext";
+
+export const FileUploadDropzone: React.FC = () => {
+  const { getInputProps, getRootProps } = useFileUploadContext();
+
+  return (
+    <div {...getRootProps()}>
+      <Flex {...styles.fileUpload({})}>
+        <input type="file" {...(getInputProps() as React.InputHTMLAttributes<HTMLInputElement>)} />
+        <IconDragAndDrop/>
+        <Text>Drag and drop or click to upload</Text>
+      </Flex>
+    </div>
+  );
+};
+
+FileUploadDropzone.displayName = '@optiaxiom/react/FileUploadDropzone';
