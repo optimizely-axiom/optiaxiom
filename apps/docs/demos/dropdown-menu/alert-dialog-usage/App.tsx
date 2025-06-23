343931f1a217a9cebe93b089764048a53b2f97f4
Rafi Adnan <rafi.adnan@optimizely.com>
2025-06-23 13:09:43 -0400

    use controlled dialog from dropdown menu
    instead of composing trigger with item
    
    we should not have a dropdown menu open while interacting with something else
    plus this will make it easier to replace dropdown menu with menu for consumers
    or if we ever decide to switch out dialog from radix with something else
    entirely

--- apps/docs/demos/dropdown-menu/alert-dialog-usage/App.tsx
+++ apps/docs/demos/dropdown-menu/alert-dialog-usage/App.tsx
@@ -8,44 +8,44 @@ import {
   AlertDialogContent,
   AlertDialogFooter,
   AlertDialogHeader,
-  AlertDialogTrigger,
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@optiaxiom/react";
 import { IconPencil, IconTrash } from "@tabler/icons-react";
+import { useState } from "react";
 
 export function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger>Open</DropdownMenuTrigger>
 
       <DropdownMenuContent>
         <DropdownMenuItem icon={<IconPencil />}>Edit</DropdownMenuItem>
-        <AlertDialog>
-          <AlertDialogTrigger asChild>
-            <DropdownMenuItem
-              icon={<IconTrash />}
-              intent="danger"
-              onSelect={(event) => event.preventDefault()}
-            >
-              Delete
-            </DropdownMenuItem>
-          </AlertDialogTrigger>
-
-          <AlertDialogContent>
-            <AlertDialogHeader>Are you sure?</AlertDialogHeader>
-            <AlertDialogBody>
-              The task and all content will be deleted.
-            </AlertDialogBody>
-            <AlertDialogFooter>
-              <AlertDialogCancel />
-              <AlertDialogAction>Yes, delete</AlertDialogAction>
-            </AlertDialogFooter>
-          </AlertDialogContent>
-        </AlertDialog>
+        <DropdownMenuItem
+          icon={<IconTrash />}
+          intent="danger"
+          onSelect={() => setOpen(true)}
+        >
+          Delete
+        </DropdownMenuItem>
       </DropdownMenuContent>
+
+      <AlertDialog onOpenChange={setOpen} open={open}>
+        <AlertDialogContent>
+          <AlertDialogHeader>Are you sure?</AlertDialogHeader>
+          <AlertDialogBody>
+            The task and all content will be deleted.
+          </AlertDialogBody>
+          <AlertDialogFooter>
+            <AlertDialogCancel />
+            <AlertDialogAction>Yes, delete</AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </DropdownMenu>
   );
 }
