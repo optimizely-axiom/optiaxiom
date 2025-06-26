a692582f1e3481a350ad4127ca7396a802d2161a
Rafi Adnan <rafi.adnan@optimizely.com>
2025-06-26 10:21:54 -0400

    update vitest to latest version

--- packages/react/vitest.rtl.ts
+++ packages/react/vitest.rtl.ts
@@ -1,18 +1,13 @@
 import * as _ from "@testing-library/dom";
 import { render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { createElement } from "react";
 import { vi } from "vitest";
 
 import { AxiomProvider } from "./src";
 
-const GlobalTestWrapper = ({ children }: { children?: React.ReactNode }) => {
-  return createElement(AxiomProvider, undefined, children);
-};
-
 const customRender = (...args: Parameters<typeof render>) => ({
   ...render(args[0], {
-    wrapper: GlobalTestWrapper,
+    wrapper: AxiomProvider,
     ...args[1],
   }),
   user: userEvent.setup({
@@ -20,10 +15,5 @@ const customRender = (...args: Parameters<typeof render>) => ({
   }),
 });
 
-/**
- * There's some bug in vitest <3.1.2 that causes render from
- * "@testing-library/react" to overwrite our custom render.
- * So we explicitly list out the re-exported modules.
- */
-export { screen, waitFor } from "@testing-library/react";
+export * from "@testing-library/react";
 export { customRender as render };
