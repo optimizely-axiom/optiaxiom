1449b83c62ef032e230e1ae8dc7ff5a6f3f0d6c7
Rafi Adnan <rafi.adnan@optimizely.com>
2025-07-25 16:05:01 -0400

    sticky dropzone overlay position

---
+++ .changeset/angry-mangos-smoke.md
@@ -0,0 +1,5 @@
+---
+"@optiaxiom/react": patch
+---
+
+sticky dropzone overlay position
--- packages/react/src/file-upload/FileUploadContext.ts
+++ packages/react/src/file-upload/FileUploadContext.ts
@@ -1,8 +1,11 @@
 "use client";
 
+import type { RefObject } from "react";
+
 import { createContext } from "@radix-ui/react-context";
 import { type DropzoneState } from "react-dropzone";
 
 export const [FileUploadProvider, useFileUploadContext] = createContext<{
   dropzone: DropzoneState;
+  rootRef: RefObject<HTMLDivElement>;
 }>("@optiaxiom/react/FileUpload");
