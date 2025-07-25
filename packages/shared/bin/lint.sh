2c57b38c19f85db4346518bf591ddcd80b254bb9
Rafi Adnan <rafi.adnan@optimizely.com>
2025-07-25 10:56:28 -0400

    upgrade pnpm to v10

--- packages/shared/bin/lint.sh
+++ packages/shared/bin/lint.sh
@@ -5,5 +5,5 @@ e_flag="--fix" p_flag="--log-level silent -w"
 [ "$1" = "--no-fix" ] && e_flag= p_flag="-c"
 if [ -f eslint.config.js ]; then
   prettier --cache $p_flag .
-  eslint --cache --quiet $e_flag .
+  packages/shared/node_modules/.bin/eslint --cache --quiet $e_flag .
 fi
