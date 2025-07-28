51cb025a35e081b9ffc1bee6373a3e1049524771
Rafi Adnan <rafi.adnan@optimizely.com>
2025-07-28 17:13:44 -0400

    parse custom opal json
    and return an external resource object

--- packages/react/src/file-upload/FileUploadContext.ts
+++ packages/react/src/file-upload/FileUploadContext.ts
@@ -4,10 +4,16 @@ import type { RefObject } from "react";
 
 import { createContext } from "@radix-ui/react-context";
 
+export type RemoteFile = {
+  name: string;
+  type: string;
+  url: string;
+};
+
 export const [FileUploadProvider, useFileUploadContext] = createContext<{
   accept: string;
   disabled: boolean | undefined;
   inputRef: RefObject<HTMLInputElement>;
-  onFilesDrop: ((files: Array<Blob | File>) => void) | undefined;
+  onFilesDrop: ((files: Array<File | RemoteFile>) => void) | undefined;
   rootRef: RefObject<HTMLDivElement>;
 }>("@optiaxiom/react/FileUpload");
--- packages/react/src/file-upload/index.ts
+++ packages/react/src/file-upload/index.ts
@@ -1,4 +1,5 @@
 export * from "./FileUpload";
+export { type RemoteFile as OpalFile } from "./FileUploadContext";
 export * from "./FileUploadDropzone";
 export * from "./FileUploadTrigger";
 export * from "./useFileUploadTrigger";
