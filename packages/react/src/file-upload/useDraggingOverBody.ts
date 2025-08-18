166b18ef3318e9562ba6f04b9cdbe5829686479e
Rafi Adnan <rafi.adnan@optimizely.com>
2025-08-18 13:48:36 -0400

    better detection of valid drag sources

--- packages/react/src/file-upload/useDraggingOverBody.ts
+++ packages/react/src/file-upload/useDraggingOverBody.ts
@@ -3,21 +3,47 @@ import { useEffect, useRef, useState } from "react";
 import { useFileUploadContext } from "./FileUploadContext";
 
 export const useDraggingOverBody = () => {
-  const { disabled } = useFileUploadContext(
+  const { disabled, rootRef } = useFileUploadContext(
     "@optiaxiom/react/FileUploadDropzone",
   );
   const [isDragging, setIsDragging] = useState(false);
-  const dropRef = useRef<number>();
+  const dragRef = useRef({
+    source: null as "file" | Node | null,
+    timer: 0,
+  });
 
   useEffect(() => {
     if (disabled) {
       return;
     }
 
-    const over = () => {
-      setIsDragging(true);
-      window.clearTimeout(dropRef.current);
-      dropRef.current = window.setTimeout(() => {
+    const over = (event: DragEvent) => {
+      if (!dragRef.current.source) {
+        dragRef.current.source = event.dataTransfer?.items
+          ? Array.from(event.dataTransfer.items).some(
+              (item) => item.kind === "file",
+            )
+            ? "file"
+            : Array.from(event.dataTransfer.items).some(
+                  (item) =>
+                    item.type === "opal-chat-dnd-data" ||
+                    item.type === "opal-host-dnd-data",
+                )
+              ? event.target instanceof Node
+                ? event.target
+                : null
+              : null
+          : "file";
+      }
+      setIsDragging(
+        dragRef.current.source !== null &&
+          (dragRef.current.source === "file" ||
+            !rootRef.current?.contains(dragRef.current.source)),
+      );
+
+      window.clearTimeout(dragRef.current.timer);
+      dragRef.current.timer = window.setTimeout(() => {
+        dragRef.current.source = null;
         setIsDragging(false);
       }, 100);
     };
@@ -26,7 +52,7 @@ export const useDraggingOverBody = () => {
     return () => {
       document.body.removeEventListener("dragover", over);
     };
-  }, [disabled]);
+  }, [disabled, rootRef]);
 
   return isDragging;
 };
