556ace98eb69b993891c081487075346e65a472e
Rafi Adnan <rafi.adnan@optimizely.com>
2025-08-20 12:51:06 -0400

    use default export for preview annotations
    and remove beforeAll call from vitest setup

--- apps/storybook/.storybook/preview.tsx
+++ apps/storybook/.storybook/preview.tsx
@@ -7,35 +7,35 @@ import "./preview.css";
 
 TransitionGlobalConfig.skipAnimations = isChromatic();
 
-export const loaders = isChromatic()
-  ? [
-      async () => ({
-        fonts: await document.fonts.load("1rem InterVariable"),
-      }),
-    ]
-  : [];
-
-export const parameters = {
-  a11y: {
-    test: "error",
-  },
-  controls: { sort: "requiredFirst" },
-  layout: "centered",
-  options: {
-    storySort: {
-      method: "alphabetical",
-    },
-  },
-  useAxiomProvider: true,
-} satisfies Preview["parameters"];
-
-export const decorators = [
-  (Story, context) =>
-    context.parameters.useAxiomProvider ? (
-      <AxiomProvider>
+export default {
+  decorators: [
+    (Story, context) =>
+      context.parameters.useAxiomProvider ? (
+        <AxiomProvider>
+          <Story />
+        </AxiomProvider>
+      ) : (
         <Story />
-      </AxiomProvider>
-    ) : (
-      <Story />
-    ),
-] satisfies Preview["decorators"];
+      ),
+  ],
+  loaders: isChromatic()
+    ? [
+        async () => ({
+          fonts: await document.fonts.load("1rem InterVariable"),
+        }),
+      ]
+    : [],
+  parameters: {
+    a11y: {
+      test: "error",
+    },
+    controls: { sort: "requiredFirst" },
+    layout: "centered",
+    options: {
+      storySort: {
+        method: "alphabetical",
+      },
+    },
+    useAxiomProvider: true,
+  },
+} satisfies Preview;
