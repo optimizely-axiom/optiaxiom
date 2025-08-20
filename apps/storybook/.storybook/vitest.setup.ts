c3857a012fcdea6314a57e6b6de16f73ebcb7bd2
Rafi Adnan <rafi.adnan@optimizely.com>
2025-08-20 12:51:06 -0400

    use default export for preview annotations
    and remove beforeAll call from vitest setup

--- apps/storybook/.storybook/vitest.setup.ts
+++ apps/storybook/.storybook/vitest.setup.ts
@@ -1,12 +1,6 @@
 import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 import { setProjectAnnotations } from "@storybook/react-vite";
-import { beforeAll } from "vitest";
 
-import * as previewAnnotations from "./preview";
+import * as projectAnnotations from "./preview";
 
-const annotations = setProjectAnnotations([
-  a11yAddonAnnotations,
-  previewAnnotations,
-]);
-
-beforeAll(annotations.beforeAll);
+setProjectAnnotations([a11yAddonAnnotations, projectAnnotations]);
