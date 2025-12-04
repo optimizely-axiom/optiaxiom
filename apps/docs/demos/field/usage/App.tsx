0c6cbf2694d6f44575ce8dcc0175035d7bb2efee
Rafi Adnan <rafi.adnan@optimizely.com>
2025-12-04 16:22:45 -0500

    expand field docs

--- apps/docs/demos/field/usage/App.tsx
+++ apps/docs/demos/field/usage/App.tsx
@@ -1,8 +1,25 @@
+"use client";
+
+import type { ComponentPropsWithoutRef } from "react";
+
 import { Field, Input } from "@optiaxiom/react";
 
-export function App() {
+export function App({
+  description,
+  error,
+  label = "Input label",
+  required = false,
+}: Pick<
+  ComponentPropsWithoutRef<typeof Field>,
+  "description" | "error" | "label" | "required"
+>) {
   return (
-    <Field label="Input label">
+    <Field
+      description={description}
+      error={error}
+      label={label}
+      required={required}
+    >
       <Input placeholder="Enter text..." />
     </Field>
   );
