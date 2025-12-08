b0ce103d7ae3cd580941c95b8c3bef0a1c65a9d2
Rafi Adnan <rafi.adnan@optimizely.com>
2025-12-08 15:25:00 -0500

    add mcp banner to every component page

--- apps/docs/components/links/Links.tsx
+++ apps/docs/components/links/Links.tsx
@@ -1,17 +1,27 @@
-import { Button, Group } from "@optiaxiom/react";
-import Link from "next/link";
+import { Alert, Button, Group, Text } from "@optiaxiom/react";
+import NextLink from "next/link";
+import { Link } from "nextra-theme-docs";
 import { GitHubIcon } from "nextra/icons";
 
 export const Links = ({ component }: { component: string }) => (
-  <Group gap="12" mt="16">
-    <Button asChild icon={<GitHubIcon />}>
-      <Link
-        href={`https://github.com/optimizely-axiom/optiaxiom/tree/main/packages/react/src/${component}/`}
-        rel="noreferrer"
-        target="_blank"
-      >
-        Source
-      </Link>
-    </Button>
+  <Group alignItems="start" flexDirection="column" gap="16">
+    <Group gap="12" mt="16">
+      <Button asChild icon={<GitHubIcon />}>
+        <NextLink
+          href={`https://github.com/optimizely-axiom/optiaxiom/tree/main/packages/react/src/${component}/`}
+          rel="noreferrer"
+          target="_blank"
+        >
+          Source
+        </NextLink>
+      </Button>
+    </Group>
+
+    <Alert intent="information">
+      <Text>
+        Want to skip the docs? Try our{" "}
+        <Link href="/guides/mcp/">MCP Server</Link>
+      </Text>
+    </Alert>
   </Group>
 );
