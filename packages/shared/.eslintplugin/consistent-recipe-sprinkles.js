c68d97ed30d9043b7ee8ba8a2fd4a1cf51b14e33
Rafi Adnan <rafi.adnan@optimizely.com>
2025-12-11 15:53:13 -0500

    replace type aware lint rule with simpler checks
    since oxlint does not support type aware js rules yet

--- packages/shared/.eslintplugin/consistent-recipe-sprinkles.js
+++ packages/shared/.eslintplugin/consistent-recipe-sprinkles.js
@@ -1,7 +1,133 @@
-import { ESLintUtils } from "@typescript-eslint/utils";
+/**
+ * Static list of all sprinkle properties from properties.css.ts
+ * This list should be kept in sync with the actual properties defined in properties.css.ts
+ */
+const SPRINKLE_PROPERTIES = new Set([
+  "alignItems",
+  "alignSelf",
+  "animation",
+  "backgroundColor",
+  "backgroundImage",
+  "bg",
+  "border",
+  "borderB",
+  "borderBottomWidth",
+  "borderColor",
+  "borderL",
+  "borderLeftWidth",
+  "borderR",
+  "borderRadius",
+  "borderRightWidth",
+  "borderT",
+  "borderTopWidth",
+  "boxShadow",
+  "color",
+  "cursor",
+  "display",
+  "flex",
+  "flexDirection",
+  "flexWrap",
+  "fontFamily",
+  "fontSize",
+  "fontWeight",
+  "gap",
+  "gridColumn",
+  "gridTemplateColumns",
+  "h",
+  "height",
+  "justifyContent",
+  "justifyItems",
+  "m",
+  "marginBottom",
+  "marginLeft",
+  "marginRight",
+  "marginTop",
+  "maxH",
+  "maxHeight",
+  "maxW",
+  "maxWidth",
+  "mb",
+  "ml",
+  "mr",
+  "mt",
+  "mx",
+  "my",
+  "objectFit",
+  "overflow",
+  "overflowX",
+  "overflowY",
+  "p",
+  "paddingBottom",
+  "paddingLeft",
+  "paddingRight",
+  "paddingTop",
+  "pb",
+  "pl",
+  "placeItems",
+  "pointerEvents",
+  "pr",
+  "pt",
+  "px",
+  "py",
+  "rounded",
+  "shadow",
+  "size",
+  "textAlign",
+  "textTransform",
+  "transition",
+  "w",
+  "whiteSpace",
+  "width",
+  "z",
+  "zIndex",
+]);
 
-export default ESLintUtils.RuleCreator.withoutDocs({
+/** @type {import('eslint').Rule.RuleModule} */
+export default {
   create(context) {
+    // Check if we're linting properties.css.ts
+    if (context.filename.endsWith("sprinkles/properties.css.ts")) {
+      return {
+        /**
+         * Check that all properties in defineProperties match our static list
+         * @type {import('eslint').Rule.RuleListener['Property']}
+         */
+        "CallExpression[callee.name=defineProperties] > ObjectExpression > Property[key.name=properties] > ObjectExpression > Property":
+          (node) => {
+            if (node.key.type !== "Identifier") {
+              return;
+            }
+            const propName = node.key.name;
+            if (!SPRINKLE_PROPERTIES.has(propName)) {
+              context.report({
+                data: { property: propName },
+                messageId: "sync",
+                node: node.key,
+              });
+            }
+          },
+        /**
+         * Check that all shorthands in defineProperties match our static list
+         * @type {import('eslint').Rule.RuleListener['Property']}
+         */
+        "CallExpression[callee.name=defineProperties] > ObjectExpression > Property[key.name=shorthands] > ObjectExpression > Property":
+          (node) => {
+            if (node.key.type !== "Identifier") {
+              return;
+            }
+            const propName = node.key.name;
+            if (!SPRINKLE_PROPERTIES.has(propName)) {
+              context.report({
+                data: { property: propName },
+                messageId: "sync",
+                node: node.key,
+              });
+            }
+          },
+      };
+    }
+
+    // Check TSX files for inline sprinkle props when using recipes
     return {
       /**
        * @type {import('@typescript-eslint/utils').TSESLint.RuleListener['JSXSpreadAttribute']}
@@ -10,15 +136,6 @@ export default ESLintUtils.RuleCreator.withoutDocs({
         parent,
       }) => {
         if (parent.type !== "JSXOpeningElement") {
-          return;
-        }
-
-        const parserServices = ESLintUtils.getParserServices(context);
-        const component = parserServices.getTypeAtLocation(parent.name);
-        const props = component
-          .getCallSignatures()[0]
-          ?.getTypeParameterAtPosition(0);
-        if (!props) {
           return;
         }
 
@@ -40,11 +157,8 @@ export default ESLintUtils.RuleCreator.withoutDocs({
             continue;
           }
 
-          const source = props
-            .getProperty(attribute.name.name)
-            ?.getDeclarations()?.[0]
-            .getSourceFile().fileName;
-          if (source?.endsWith("sprinkles/properties.css.ts")) {
+          // Check if this attribute is a sprinkle property
+          if (SPRINKLE_PROPERTIES.has(attribute.name.name)) {
             context.report({
               messageId: "expected",
               node: attribute.name,
@@ -55,15 +169,14 @@ export default ESLintUtils.RuleCreator.withoutDocs({
     };
   },
 
-  defaultOptions: [],
-
   meta: {
     fixable: "code",
     messages: {
       expected:
         "Please avoid inlining sprinkle props when using recipes and move them inside the recipe base instead.",
+      sync: 'Property "{{property}}" is not in the static SPRINKLE_PROPERTIES list. Please update the list in consistent-recipe-sprinkles.js',
     },
     schema: [],
     type: "suggestion",
   },
-});
+};
--- packages/shared/.eslintplugin/missing-recipe-classname.js
+++ packages/shared/.eslintplugin/missing-recipe-classname.js
@@ -1,7 +1,50 @@
-import { ESLintUtils } from "@typescript-eslint/utils";
-
-export default ESLintUtils.RuleCreator.withoutDocs({
+/** @type {import('@typescript-eslint/utils').TSESLint.RuleModule<string>} */
+export default {
   create(context) {
+    /**
+     * Find the parent function/arrow function of a node
+     * @param {import('@typescript-eslint/utils').TSESTree.Node} node
+     * @returns {(import('@typescript-eslint/utils').TSESTree.FunctionDeclaration | import('@typescript-eslint/utils').TSESTree.FunctionExpression | import('@typescript-eslint/utils').TSESTree.ArrowFunctionExpression) | null}
+     */
+    function findParentFunction(node) {
+      /** @type {import('@typescript-eslint/utils').TSESTree.Node | undefined} */
+      let current = node;
+      while (current) {
+        if (
+          current.type === "FunctionDeclaration" ||
+          current.type === "FunctionExpression" ||
+          current.type === "ArrowFunctionExpression"
+        ) {
+          return current;
+        }
+        current = current.parent;
+      }
+      return null;
+    }
+
+    /**
+     * Check if a function parameter destructures className
+     * @param {(import('@typescript-eslint/utils').TSESTree.FunctionDeclaration | import('@typescript-eslint/utils').TSESTree.FunctionExpression | import('@typescript-eslint/utils').TSESTree.ArrowFunctionExpression) | null} funcNode
+     * @returns {boolean}
+     */
+    function hasClassNameInParams(funcNode) {
+      if (!funcNode || !funcNode.params) {
+        return false;
+      }
+
+      return funcNode.params.some((param) => {
+        if (param.type === "ObjectPattern") {
+          return param.properties.some(
+            (prop) =>
+              prop.type === "Property" &&
+              prop.key.type === "Identifier" &&
+              prop.key.name === "className",
+          );
+        }
+        return false;
+      });
+    }
+
     return {
       /**
        * @type {import('@typescript-eslint/utils').TSESLint.RuleListener['CallExpression']}
@@ -16,36 +59,41 @@ export default ESLintUtils.RuleCreator.withoutDocs({
           return;
         }
 
-        const parserServices = ESLintUtils.getParserServices(context);
         const hasClassName = node.arguments.length === 2;
         const hasSpreadPropsWithClassName = node.parent.parent.attributes.find(
           (attr) =>
             attr.type === "JSXSpreadAttribute" &&
-            attr.argument.type === "Identifier" &&
-            parserServices
-              .getTypeAtLocation(attr.argument)
-              .getProperty("className"),
+            attr.argument.type === "Identifier",
         );
+
         if (hasSpreadPropsWithClassName && !hasClassName) {
-          const children = context.sourceCode
-            .getScope(node)
-            .variables.find((variable) => variable.name === "children")
-            ?.defs[0].name;
-          context.report({
-            fix: (fixer) => [
-              node.arguments.length === 1
-                ? fixer.insertTextAfter(node.arguments[0], ", className")
-                : fixer.insertTextAfterRange(
-                    [node.range[0], node.range[1] - 1],
-                    "{}, className",
-                  ),
-              ...(children
-                ? [fixer.insertTextAfter(children, ", className")]
-                : []),
-            ],
-            messageId: "recipe",
-            node,
-          });
+          // Check if the component function destructures className
+          const destructuresClassName = hasClassNameInParams(
+            findParentFunction(node),
+          );
+
+          // If className is NOT destructured, it's still in props and will override the recipe
+          if (!destructuresClassName) {
+            const children = context.sourceCode
+              .getScope(node)
+              .variables.find((variable) => variable.name === "children")
+              ?.defs[0].name;
+            context.report({
+              fix: (fixer) => [
+                node.arguments.length === 1
+                  ? fixer.insertTextAfter(node.arguments[0], ", className")
+                  : fixer.insertTextAfterRange(
+                      [node.range[0], node.range[1] - 1],
+                      "{}, className",
+                    ),
+                ...(children
+                  ? [fixer.insertTextAfter(children, ", className")]
+                  : []),
+              ],
+              messageId: "recipe",
+              node,
+            });
+          }
         }
       },
     };
@@ -61,4 +109,4 @@ export default ESLintUtils.RuleCreator.withoutDocs({
     schema: [],
     type: "suggestion",
   },
-});
+};
