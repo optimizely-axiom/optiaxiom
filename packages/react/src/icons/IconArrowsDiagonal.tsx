c90f758f0b412c8956e75e528ea147900e4ecf27
Rafi Adnan <rafi.adnan@optimizely.com>
2026-03-05 14:44:14 -0500

    add copy/download actions to proteus images

---
+++ packages/react/src/icons/IconArrowsDiagonal.tsx
@@ -0,0 +1,16 @@
+import { withIcon } from "./withIcon";
+
+export const IconArrowsDiagonal = withIcon(
+  {
+    fill: "none",
+    height: 24,
+    name: "IconArrowsDiagonal",
+    width: 24,
+  },
+  <>
+    <path d="M16 4l4 0l0 4" stroke="currentColor" />
+    <path d="M14 10l6 -6" stroke="currentColor" />
+    <path d="M8 20l-4 0l0 -4" stroke="currentColor" />
+    <path d="M4 20l6 -6" stroke="currentColor" />
+  </>,
+);
---
+++ packages/react/src/icons/IconDownload.tsx
@@ -0,0 +1,18 @@
+import { withIcon } from "./withIcon";
+
+export const IconDownload = withIcon(
+  {
+    fill: "none",
+    height: 24,
+    name: "IconDownload",
+    width: 24,
+  },
+  <>
+    <path
+      d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2"
+      stroke="currentColor"
+    />
+    <path d="M7 11l5 5l5 -5" stroke="currentColor" />
+    <path d="M12 4l0 12" stroke="currentColor" />
+  </>,
+);
