d3b436cafeaa4bd05c4087b9f2a9b1eeedf57e7e
Rafi Adnan <rafi.adnan@optimizely.com>
2026-03-06 11:32:29 -0500

    report unused directives with oxlint

--- packages/shared/bin/lint.sh
+++ packages/shared/bin/lint.sh
@@ -5,5 +5,5 @@ p_flag="--write" ox_flag="--fix"
 [ "$1" = "--no-fix" ] && p_flag="--check" ox_flag=
 if [ -f .oxlintrc.json ]; then
   oxfmt $p_flag .
-  oxlint --quiet --type-aware $ox_flag .
+  oxlint --report-unused-disable-directives-severity error --quiet --type-aware $ox_flag .
 fi
