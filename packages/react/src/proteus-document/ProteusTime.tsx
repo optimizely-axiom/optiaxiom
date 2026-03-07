d00d3b3b97492eecd7a830ba6c3c33ed839d2b9a
Rafi Adnan <rafi.adnan@optimizely.com>
2026-03-07 10:35:10 -0500

    fix resolving props for time component

--- packages/react/src/proteus-document/ProteusTime.tsx
+++ packages/react/src/proteus-document/ProteusTime.tsx
@@ -1,8 +1,9 @@
 import { Time, type TimeProps } from "../time";
+import { useResolvedProteusProps } from "./useResolvedProteusProps";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function ProteusTime(props: Record<string, any>) {
-  return <Time {...(props as TimeProps)} />;
+  return <Time {...(useResolvedProteusProps(props) as TimeProps)} />;
 }
 
 ProteusTime.displayName = "@optiaxiom/react/ProteusTime";
