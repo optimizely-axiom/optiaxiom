223399c710474859b81ab68a6e9ab0cad83f3908
Rafi Adnan <rafi.adnan@optimizely.com>
2026-03-30 17:02:08 -0400

    temporary a11y fix

--- packages/react/src/listbox/ListboxItem.css.ts
+++ packages/react/src/listbox/ListboxItem.css.ts
@@ -122,7 +122,7 @@ export const description = recipe({
       fontSize: "sm",
     },
     style({
-      color: theme.colors["fg.tertiary"],
+      color: theme.colors["fg.secondary"],
     }),
   ],
 });
