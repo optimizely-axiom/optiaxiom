c1af0d0ce09ce158e7ae2073715878c172c616a8
toha993 <mutasim.toha@optimizely.com>
2026-04-02 18:44:16 +0600

    support autoFocus prop for proteus Input

--- packages/react/src/input/Input.tsx
+++ packages/react/src/input/Input.tsx
@@ -24,6 +24,10 @@ export type InputProps = InputControlProps<
        */
       addonAfter?: ReactNode;
       /**
+       * Whether the input should be focused on mount.
+       */
+      autoFocus?: boolean;
+      /**
        * Display content inside the input at the start.
        */
       addonBefore?: ReactNode;
