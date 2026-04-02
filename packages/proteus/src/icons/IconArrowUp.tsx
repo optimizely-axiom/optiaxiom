5560628816bfcf130dcbd0eda07099bbe3f1d85d
toha993 <mutasim.toha@optimizely.com>
2026-04-02 18:32:11 +0600

    add arrow up and close icons

---
+++ packages/proteus/src/icons/IconArrowUp.tsx
@@ -0,0 +1,14 @@
+import { withIcon } from "./withIcon";
+
+export const IconArrowUp = withIcon(
+  {
+    fill: "none",
+    name: "IconArrowUp",
+  },
+  <path
+    d="M10 14V2M10 2L5 7M10 2L15 7"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+  />,
+);
