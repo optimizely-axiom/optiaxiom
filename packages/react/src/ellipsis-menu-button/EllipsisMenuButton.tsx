041cd4ed1545b253365309e93816b22f27d32c08
Rafi Adnan <rafi.adnan@optimizely.com>
2026-04-16 14:21:20 -0400

    replace with material icons

--- packages/react/src/ellipsis-menu-button/EllipsisMenuButton.tsx
+++ packages/react/src/ellipsis-menu-button/EllipsisMenuButton.tsx
@@ -1,7 +1,7 @@
+import { IconMoreHoriz } from "@optiaxiom/icons";
 import { type ComponentPropsWithoutRef, forwardRef } from "react";
 
 import { Button } from "../button";
-import { IconEllipsisSolid } from "../icons/IconEllipsisSolid";
 
 export type EllipsisMenuButtonProps = ComponentPropsWithoutRef<typeof Button>;
 
@@ -21,7 +21,7 @@ export const EllipsisMenuButton = forwardRef<
 >(({ children, ...props }, ref) => {
   return (
     <Button
-      icon={<IconEllipsisSolid />}
+      icon={<IconMoreHoriz />}
       iconPosition="end"
       ref={ref}
       {...props}
