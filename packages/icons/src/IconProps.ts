6229c4c424f424e583d36faeeebcc8615ad3a246
Rafi Adnan <rafi.adnan@optimizely.com>
2026-04-16 14:32:36 -0400

    fix IconProps type
    so we can import this from other packages

--- packages/icons/src/IconProps.ts
+++ packages/icons/src/IconProps.ts
@@ -1,8 +1,19 @@
-import type { ForwardRefExoticComponent, RefAttributes } from "react";
-
-import type { MaterialIconProps } from "./MaterialIcon";
+import type {
+  ComponentPropsWithoutRef,
+  ForwardRefExoticComponent,
+  RefAttributes,
+} from "react";
 
 export type IconProps = ForwardRefExoticComponent<
-  Omit<MaterialIconProps, "filledPath" | "unfilledPath"> &
-    RefAttributes<SVGSVGElement>
+  Omit<ComponentPropsWithoutRef<"svg">, "height" | "width"> &
+    RefAttributes<SVGSVGElement> & {
+      /**
+       * Whether the icon should use filled or unfilled state.
+       */
+      filled?: boolean;
+      /**
+       * Set the icon size using design tokens (`sm`, `md`, `lg`, etc.), pixel string values (`"16"`, `"20"`), or numeric pixel values (`16`, `20`).
+       */
+      size?: number | string;
+    }
 >;
