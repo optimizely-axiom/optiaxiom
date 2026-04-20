6e4c8529672945cd999617f8f0b19db457772b64
Rafi Adnan <rafi.adnan@optimizely.com>
2026-04-20 14:28:00 -0400

    add scroll down marker in case content truncated

--- packages/proteus/src/proteus-document/ProteusDocumentShell.css.ts
+++ packages/proteus/src/proteus-document/ProteusDocumentShell.css.ts
@@ -1,11 +1,14 @@
 import { recipe, style } from "../vanilla-extract";
 
+const marker = style({});
+
 export const body = recipe({
   base: [
     {
       flexDirection: "column",
       gap: "16",
     },
+    marker,
   ],
   variants: {
     truncate: {
@@ -22,4 +25,37 @@ export const body = recipe({
       ],
     },
   },
+});
+
+export const scrollIndicator = recipe({
+  base: [
+    {
+      bg: "bg.secondary",
+      border: "1",
+      borderColor: "border.secondary",
+      color: "fg.secondary",
+      display: "grid",
+      flex: "none",
+      placeItems: "center",
+      pointerEvents: "none",
+      rounded: "full",
+      shadow: "sm",
+      size: "lg",
+      transition: "opacity",
+    },
+    style({
+      alignSelf: "center",
+      bottom: "8px",
+      marginTop: "-56px", // size (40px) + gap (16px) = 56px
+      opacity: 0,
+      position: "sticky",
+      zIndex: 10,
+
+      selectors: {
+        [`${marker}[data-can-scroll] &`]: {
+          opacity: 1,
+        },
+      },
+    }),
+  ],
 });
