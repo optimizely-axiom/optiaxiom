c2c846589d29b23aa7ba5a68a9f415f99ee4e89c
Niloy <Bholanath.Das@optimizely.com>
2026-04-23 01:34:03 +0600

    docs(proteus): remove SDK integration section

--- apps/docs/app/_meta.global.tsx
+++ apps/docs/app/_meta.global.tsx
@@ -44,8 +44,8 @@ export default {
       "css-layers": "CSS Layers",
       icons: "Icons",
       "module-federation": "Module Federation",
-      "react-select": "React Select",
       proteus: "Proteus",
+      "react-select": "React Select",
 
       "-- Migrations": {
         title: "Migrations",
