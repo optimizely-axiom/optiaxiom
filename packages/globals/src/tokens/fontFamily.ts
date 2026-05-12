fd43e492fd750ec6017f2b5d45f5c8dd03127993
Rafi Adnan <rafi.adnan@optimizely.com>
2026-05-12 13:56:38 -0400

    fixup! fixup! replace fonts

--- packages/globals/src/tokens/fontFamily.ts
+++ packages/globals/src/tokens/fontFamily.ts
@@ -1,6 +1,5 @@
 export const fontFamily = {
-  heading:
-    "Nudge Semicondensed, Roboto Condensed Variable, system-ui, sans-serif",
+  heading: "Nudge SemiNormal, Roboto Condensed Variable, system-ui, sans-serif",
   mono: "Roboto Mono Variable, ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace",
   sans: "DI Grotesk B, Roboto Variable, system-ui, sans-serif",
 } as const;
